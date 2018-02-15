dd95bb6befc3ee1d2418eabff077ef32225c6166
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-02-15 18:32:09 +0100

    Fixed external regexp issue for empty external arrays (#30)

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,9 @@ const external = Object.keys(pkg.peerDependencies || {})
 const allExternal = external.concat(Object.keys(pkg.dependencies || {}))
 
 const makeExternalPredicate = externalArr => {
+  if (externalArr.length === 0) {
+    return () => false
+  }
   const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`)
   return id => pattern.test(id)
 }
