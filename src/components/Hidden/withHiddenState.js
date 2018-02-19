d22ce63828a99584329f2c8c89042d0608843e8d
Haz <hazdiego@gmail.com>
2018-02-19 18:44:08 -0300

    Refactor on state enhancers (#38)

--- src/components/Hidden/withHiddenState.js
+++ src/components/Hidden/withHiddenState.js
@@ -1,30 +1,23 @@
-import PropTypes from 'prop-types'
-import { withState, withHandlers, setPropTypes } from 'recompose'
+import { withStateHandlers } from 'recompose'
 import namespace from '../../enhancers/namespace'
 
-const propTypes = {
-  visible: PropTypes.bool,
-}
+const toggle = state => () => ({ visible: !state.visible })
+const show = () => () => ({ visible: true })
+const hide = () => () => ({ visible: false })
+
+const handlers = { toggle, show, hide }
 
 const withHiddenState = namespace(
   'hidden',
   options => [
-    setPropTypes(propTypes),
-    withState(
-      'visible',
-      'setVisible',
-      props =>
-        typeof props.visible !== 'undefined'
-          ? props.visible
-          : !!options.visible,
+    withStateHandlers(
+      {
+        visible: !!options.visible,
+      },
+      handlers,
     ),
-    withHandlers({
-      toggle: ({ visible, setVisible }) => () => setVisible(!visible),
-      show: ({ setVisible }) => () => setVisible(true),
-      hide: ({ setVisible }) => () => setVisible(false),
-    }),
   ],
-  Object.keys(propTypes),
+  ['visible', ...Object.keys(handlers)],
 )
 
 export default withHiddenState
--- src/components/Step/withStepState.js
+++ src/components/Step/withStepState.js
@@ -1,129 +1,105 @@
-import PropTypes from 'prop-types'
-import { withState, withHandlers, withProps, setPropTypes } from 'recompose'
+import { withHandlers, withProps, withStateHandlers } from 'recompose'
 import namespace from '../../enhancers/namespace'
 
-const getCurrentId = props => () => props.ids[props.current]
+const getCurrentId = state => () => state.ids[state.current]
 
-const hasPrevious = props => () =>
-  props.ids.length > 1 && !!props.ids[props.current - 1]
+const hasPrevious = state => () =>
+  state.ids.length > 1 && !!state.ids[state.current - 1]
 
-const hasNext = props => () =>
-  props.ids.length > 1 && !!props.ids[props.current + 1]
+const hasNext = state => () =>
+  state.ids.length > 1 && !!state.ids[state.current + 1]
 
-const indexOf = props => idOrIndex =>
-  typeof idOrIndex === 'number' ? idOrIndex : props.ids.indexOf(idOrIndex)
+const indexOf = state => idOrIndex =>
+  typeof idOrIndex === 'number' ? idOrIndex : state.ids.indexOf(idOrIndex)
 
-const show = props => idOrIndex => props.setCurrent(indexOf(props)(idOrIndex))
+const isCurrent = state => idOrIndex =>
+  state.current >= 0 && state.current === indexOf(state)(idOrIndex)
 
-const hide = props => () => props.setCurrent(-1)
+const show = state => idOrIndex => ({ current: indexOf(state)(idOrIndex) })
 
-const isCurrent = props => idOrIndex =>
-  props.current >= 0 && props.current === indexOf(props)(idOrIndex)
+const hide = () => () => ({ current: -1 })
 
-const toggle = props => idOrIndex =>
-  isCurrent(props)(idOrIndex) ? hide(props)() : show(props)(idOrIndex)
+const toggle = state => idOrIndex =>
+  isCurrent(state)(idOrIndex) ? hide(state)() : show(state)(idOrIndex)
 
-const previous = props => () => {
-  props.setCurrent(current => {
-    if (hasPrevious({ ...props, current })()) {
-      return current - 1
-    } else if (props.loop) {
-      return props.ids.length - 1
-    }
-    return current
-  })
+const previous = (state, props) => () => {
+  if (hasPrevious(state)()) {
+    return { current: state.current - 1 }
+  } else if (props.loop) {
+    return { current: state.ids.length - 1 }
+  }
+  return state
 }
 
-const next = props => () => {
-  props.setCurrent(current => {
-    if (hasNext({ ...props, current })()) {
-      return current + 1
-    } else if (props.loop) {
-      return 0
-    }
-    return current
-  })
+const next = (state, props) => () => {
+  if (hasNext(state)()) {
+    return { current: state.current + 1 }
+  } else if (props.loop) {
+    return { current: 0 }
+  }
+  return state
 }
 
-const reorder = props => (id, order) => {
-  props.setOrdered(ordered => {
-    const nextOrdered = { ...ordered, [id]: order }
-    const current = isCurrent(props)(id)
-    props.setIds(
-      ids => ids.sort((a, b) => (nextOrdered[a] || 0) - (nextOrdered[b] || 0)),
-      () => current && show(props)(id),
-    )
-    return nextOrdered
-  })
-}
-
-const register = props => (id, order = 0) => {
-  props.setIds(
-    ids => {
-      if (ids.indexOf(id) >= 0) {
-        return ids
-      }
-      return [...ids, id]
-    },
-    () => reorder(props)(id, order),
-  )
-}
-
-const unregister = props => id => {
-  props.setIds(ids => {
-    const nextProps = { ...props, ids }
-    const index = indexOf(nextProps)(id)
-    if (index === -1) {
-      return ids
-    }
-
-    if (isCurrent(nextProps)(id) && !hasNext(nextProps)()) {
-      if (hasPrevious(nextProps)()) {
-        previous(nextProps)()
-      } else {
-        hide(nextProps)()
-      }
-    }
-    return [...ids.slice(0, index), ...ids.slice(index + 1)]
-  })
-}
-
-const update = props => (id, nextId, order = props.ordered[id]) => {
-  const idChanged = id !== nextId
-  const orderChanged = order !== props.ordered[id]
-
-  if (!idChanged && !orderChanged) return
-
-  const overridingId = idChanged && props.ids.indexOf(nextId) >= 0
-
-  if (overridingId) {
-    unregister(props)(id)
-    if (orderChanged) {
-      reorder(props)(nextId, order)
-    }
-  } else {
-    props.setIds(
-      ids => {
-        const index = indexOf({ ...props, ids })(id)
-        return [...ids.slice(0, index), nextId, ...ids.slice(index + 1)]
-      },
-      () => {
-        if (orderChanged) {
-          reorder(props)(nextId, order)
-        }
-      },
-    )
+const reorder = state => (id, order) => {
+  const ordered = { ...state.ordered, [id]: order }
+  const ids = state.ids
+    .slice()
+    .sort((a, b) => (ordered[a] || 0) - (ordered[b] || 0))
+  return {
+    ordered,
+    ids,
+    ...(isCurrent(state)(id) ? show({ ...state, ids })(id) : {}),
   }
 }
 
+const register = state => (id, order = 0) => {
+  const ids = state.ids.indexOf(id) >= 0 ? state.ids : [...state.ids, id]
+  return reorder({ ...state, ids })(id, order)
+}
+
+const unregister = state => id => {
+  const index = indexOf(state)(id)
+  if (index === -1) {
+    return state
+  }
+
+  const ids = [...state.ids.slice(0, index), ...state.ids.slice(index + 1)]
+
+  if (isCurrent(state)(id) && !hasNext(state)()) {
+    if (hasPrevious(state)()) {
+      return { ...previous(state)(), ids }
+    }
+    return { ...hide(state)(), ids }
+  }
+  return { ids }
+}
+
+const update = state => (id, nextId, order = state.ordered[id]) => {
+  const idChanged = id !== nextId
+  const orderChanged = order !== state.ordered[id]
+
+  if (!idChanged && !orderChanged) return state
+
+  const overridingId = idChanged && state.ids.indexOf(nextId) >= 0
+
+  if (overridingId) {
+    const nextState = orderChanged ? reorder(state)(nextId, order) : {}
+    return unregister({ ...state, ...nextState })(id)
+  }
+
+  const index = indexOf(state)(id)
+  const ids = [
+    ...state.ids.slice(0, index),
+    nextId,
+    ...state.ids.slice(index + 1),
+  ]
+  return reorder({ ...state, ids })(nextId, order)
+}
+
+const selectors = { getCurrentId, hasPrevious, hasNext, indexOf, isCurrent }
 const handlers = {
-  getCurrentId,
-  hasPrevious,
-  hasNext,
-  indexOf,
   show,
   hide,
-  isCurrent,
   toggle,
   previous,
   next,
@@ -133,30 +109,28 @@ const handlers = {
   update,
 }
 
-const propTypes = {
-  loop: PropTypes.bool,
-  ids: PropTypes.array,
-  current: PropTypes.number,
-}
-
 const withStepState = namespace(
   'step',
   options => [
-    setPropTypes(propTypes),
-    withProps(props => ({
-      loop: typeof props.loop !== 'undefined' ? props.loop : !!options.loop,
-    })),
-    withState('ids', 'setIds', props => props.ids || options.ids || []),
-    withState('ordered', 'setOrdered', {}),
-    withState('current', 'setCurrent', props => {
-      if (typeof props.current !== 'undefined') {
-        return props.current
-      }
-      return typeof options.current !== 'undefined' ? options.current : -1
-    }),
-    withHandlers(handlers),
+    withProps({ loop: !!options.loop }),
+    withStateHandlers(
+      {
+        ids: options.ids || [],
+        current: typeof options.current !== 'undefined' ? options.current : -1,
+        ordered: options.ordered || {},
+      },
+      handlers,
+    ),
+    withHandlers(selectors),
   ],
-  Object.keys(propTypes),
+  [
+    'loop',
+    'ids',
+    'current',
+    'ordered',
+    ...Object.keys(selectors),
+    ...Object.keys(handlers),
+  ],
 )
 
 export default withStepState
