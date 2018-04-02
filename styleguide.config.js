0beba7b3007783d0f714983cb5a6518050c67a15
Diego Haz <hazdiego@gmail.com>
2018-04-01 23:02:27 -0300

    Update root files

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path");
 const {
   createConfig,
   babel,
@@ -7,80 +7,80 @@ const {
   url,
   file,
   css,
-  sourceMaps,
-} = require('webpack-blocks')
+  sourceMaps
+} = require("webpack-blocks");
 
 module.exports = {
-  title: 'reas - React as Anything',
+  title: "reas - React as Anything",
   webpackConfig: createConfig([
     sourceMaps(),
     babel(),
     css(),
-    match(['*.eot', '*.ttf', '*.woff', '*.woff2'], [file()]),
+    match(["*.eot", "*.ttf", "*.woff", "*.woff2"], [file()]),
     match(
-      ['*.gif', '*.jpg', '*.jpeg', '*.png', '*.svg', '*.webp'],
-      [url({ limit: 10000 })],
+      ["*.gif", "*.jpg", "*.jpeg", "*.png", "*.svg", "*.webp"],
+      [url({ limit: 10000 })]
     ),
-    resolve({ alias: { reas: path.join(__dirname, 'src') } }),
+    resolve({ alias: { reas: path.join(__dirname, "src") } })
   ]),
   getComponentPathLine(componentPath) {
-    const name = path.basename(componentPath, '.js')
-    return `import { ${name} } from 'reas'`
+    const name = path.basename(componentPath, ".js");
+    return `import { ${name} } from 'reas'`;
   },
-  styleguideDir: 'docs',
-  template: 'docs/template.html',
+  styleguideDir: "docs",
+  template: "docs/template.html",
   styleguideComponents: {
     StyleGuideRenderer: path.join(
       __dirname,
-      'docs/components/StyleGuideRenderer.js',
+      "docs/components/StyleGuideRenderer.js"
     ),
-    ToolbarButton: path.join(__dirname, 'docs/components/ToolbarButton.js'),
-    Editor: path.join(__dirname, 'docs/components/Editor.js'),
+    ToolbarButton: path.join(__dirname, "docs/components/ToolbarButton.js"),
+    Editor: path.join(__dirname, "docs/components/Editor.js")
   },
   skipComponentsWithoutExample: true,
   compilerConfig: {
     transforms: {
-      dangerousTaggedTemplateString: true,
+      dangerousTaggedTemplateString: true
     },
-    objectAssign: 'Object.assign',
+    objectAssign: "Object.assign"
   },
   sections: [
     {
-      name: 'Introduction',
-      content: 'docs/contents/intro.md',
+      name: "Introduction",
+      content: "docs/contents/intro.md"
     },
     {
-      name: 'Guide',
+      name: "Guide",
       sections: [
         {
-          name: 'Installation',
-          content: 'docs/contents/installation.md',
+          name: "Installation",
+          content: "docs/contents/installation.md"
         },
         {
-          name: 'Create React App',
-          content: 'docs/contents/create-react-app.md',
+          name: "Create React App",
+          content: "docs/contents/create-react-app.md"
         },
         {
-          name: 'as',
-          content: 'docs/contents/as.md',
+          name: "as",
+          content: "docs/contents/as.md"
         },
         {
-          name: 'Styling',
-          content: 'docs/contents/styling.md',
+          name: "Styling",
+          content: "docs/contents/styling.md"
         },
         {
-          name: 'State',
-          content: 'docs/contents/state.md',
+          name: "State",
+          content: "docs/contents/state.md"
         },
         {
-          name: 'Behaviors',
-          content: 'docs/contents/behaviors.md',
-        },
-      ],
+          name: "Behaviors",
+          content: "docs/contents/behaviors.md"
+        }
+      ]
     },
     {
-      name: 'Components',
-      components: 'src/components/**/[A-Z]*.js',
-    },
-  ],
-}
+      name: "Components",
+      components: "src/components/**/[A-Z]*.js"
+    }
+  ]
+};
--- test.config.js
+++ test.config.js
@@ -1,5 +1,5 @@
-import 'raf/polyfill'
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import "raf/polyfill";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() });
