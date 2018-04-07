1cec7ef5caaeaeaf98ab8a6c9b23bf05ed3890a7
João Alberto <joaoalberto.contato@gmail.com>
2018-04-07 04:03:04 -0300

    #51 - Link component

---
+++ src/components/Link/Link.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import as from "../../enhancers/as";
+import Base from "../Base";
+
+const Link = styled(Base)`
+  color: #0366d6;
+  text-decoration: none;
+
+  &:hover {
+    outline-width: 0;
+    text-decoration: undeline;
+  }
+`;
+
+export default as("a")(Link);
---
+++ src/components/Link/Link.md
@@ -0,0 +1,3 @@
+```jsx
+<Link href="#">Link</Link>
+```
