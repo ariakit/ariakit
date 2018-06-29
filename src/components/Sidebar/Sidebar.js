fa92baf5f10a788125397cf7dbd0c067aac2cd89
Haz <hazdiego@gmail.com>
2018-06-29 16:41:42 -0300

    Sidebar (#103)
    * Sidebar
    
    * Remove unncessary default props

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { prop, ifProp } from "styled-tools";
+import as from "../../enhancers/as";
+import Overlay from "../Overlay";
+
+const Sidebar = styled(Overlay)`
+  top: 0;
+  height: 100vh;
+  transform: none;
+  left: ${ifProp({ align: "right" }, "auto", 0)};
+  right: ${ifProp({ align: "right" }, 0, "auto")};
+  ${prop("theme.Sidebar")};
+`;
+
+Sidebar.propTypes = {
+  align: PropTypes.oneOf(["left", "right"])
+};
+
+Sidebar.defaultProps = {
+  align: "left"
+};
+
+export default as("div")(Sidebar);
