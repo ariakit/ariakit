73a5fe7ad7975d5841c3fe1eded77ed4bc1a0b8a
Haz <hazdiego@gmail.com>
2018-06-29 13:26:05 -0300

    Overlay (#102)
    * Add Overlay
    
    * State -> Container

---
+++ src/components/Overlay/index.js
@@ -0,0 +1,9 @@
+import Overlay from "./Overlay";
+import Hidden from "../Hidden";
+
+Overlay.Container = Hidden.Container;
+Overlay.Toggle = Hidden.Toggle;
+Overlay.Show = Hidden.Show;
+Overlay.Hide = Hidden.Hide;
+
+export default Overlay;
