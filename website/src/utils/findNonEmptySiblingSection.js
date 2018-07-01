ce5244bedaf3f0ef3171ca6ab6175a7e8ed3b10d
Diego Haz <hazdiego@gmail.com>
2018-07-01 13:26:51 -0300

    Fix tests

--- website/src/utils/findNonEmptySiblingSection.js
+++ website/src/utils/findNonEmptySiblingSection.js
@@ -2,15 +2,15 @@ import flattenSections from "./flattenSections";
 import getSectionContent from "./getSectionContent";
 
 const findNonEmptySiblingSection = (sections, sectionOrName, findPrevious) => {
-  const flattenedSections = flattenSections(sections);
+  const flattenedSections = findPrevious
+    ? flattenSections(sections).reverse()
+    : flattenSections(sections);
   const name = sectionOrName.name || sectionOrName;
   const currentIndex = flattenedSections.findIndex(x => x.name === name);
-
   if (currentIndex >= 0) {
-    const finalSections = findPrevious
-      ? flattenedSections.reverse()
-      : flattenedSections;
-    return finalSections.slice(currentIndex).find(x => getSectionContent(x));
+    return flattenedSections
+      .slice(currentIndex + 1)
+      .find(x => getSectionContent(x));
   }
   return undefined;
 };
