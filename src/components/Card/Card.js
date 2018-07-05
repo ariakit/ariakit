028c4f9705dda5089d8441927e426f0a5cb197e4
Diego Haz <hazdiego@gmail.com>
2018-07-05 18:31:29 -0300

    Update Card styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,20 @@ import as from "../../enhancers/as";
 import Base from "../Base";
 
 const Card = styled(Base)`
-  display: inline-block;
-  box-shadow: rgba(51, 51, 51, 0.1) 0px 1px 6px,
-    rgba(51, 51, 51, 0.15) 0px 1px 4px;
-  padding: 0.5em 1em;
+  position: relative;
+  padding: 1rem 0;
+  &&& > * {
+    margin: 0 1rem;
+  }
+  &&& > img {
+    margin: 0;
+    &:first-child {
+      margin-top: -1rem;
+    }
+    &:last-child {
+      margin-bottom: -1rem;
+    }
+  }
   ${prop("theme.Card")};
 `;
 
