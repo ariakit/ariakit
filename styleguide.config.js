89098e4ed17522bcc562a3d64a9d3d27c69aa20c
Diego Haz <hazdiego@gmail.com>
2018-07-07 17:57:58 -0300

    Test

--- styleguide.config.js
+++ styleguide.config.js
@@ -58,15 +58,24 @@ module.exports = {
       raw: '<base href="/">',
       meta: [
         { name: "description", content: description },
+        { property: "og:title", content: "ReaKit" },
         { property: "og:description", content: description },
         { property: "og:site_name", content: "ReaKit" },
         { property: "og:url", content: "https://reakit.io" },
-        { property: "og:image", content: "/thumbnail.png" },
+        {
+          property: "og:image",
+          content:
+            "https://deploy-preview-127--reakit.netlify.com/thumbnail.png"
+        },
         { property: "og:image:type", content: "image/png" },
         { property: "og:image:width", content: "1200" },
         { property: "og:image:height", content: "630" },
         { property: "twitter:card", content: "summary_large_image" },
-        { property: "twitter:image:src", content: "/thumbnail.png" },
+        {
+          property: "twitter:image:src",
+          content:
+            "https://deploy-preview-127--reakit.netlify.com/thumbnail.png"
+        },
         { property: "twitter:description", content: description },
         { property: "twitter:creator", content: "@diegohaz" }
       ],
