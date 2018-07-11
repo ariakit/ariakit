ca9b532ca7ba299c780985e76513dc33657908e6
Diego Haz <hazdiego@gmail.com>
2018-07-11 19:53:58 -0300

    Add Google Tag Manager for Analytics

--- styleguide.config.js
+++ styleguide.config.js
@@ -55,7 +55,16 @@ module.exports = {
   },
   template: {
     head: {
-      raw: '<base href="/">',
+      raw: [
+        '<base href="/">',
+        `<!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NWWM5K3');</script>
+      <!-- End Google Tag Manager -->`
+      ].join("\n"),
       meta: [
         { name: "description", content: description },
         { property: "og:title", content: "ReaKit" },
