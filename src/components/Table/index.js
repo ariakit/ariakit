0d2e9f9359dd83c04fe1fcbb75d11b440d61887f
Joe Ng'ethe <>
2018-07-13 16:45:01 +0300

    defined Table.Wrapper as TableWrapper

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,12 +1,13 @@
-import Table from "./Table";
-import TableBody from "./TableBody";
-import TableCaption from "./TableCaption";
-import TableCell from "./TableCell";
-import TableColumn from "./TableColumn";
-import TableColumnGroup from "./TableColumnGroup";
-import TableFoot from "./TableFoot";
-import TableHead from "./TableHead";
-import TableRow from "./TableRow";
+import Table from './Table';
+import TableBody from './TableBody';
+import TableCaption from './TableCaption';
+import TableCell from './TableCell';
+import TableColumn from './TableColumn';
+import TableColumnGroup from './TableColumnGroup';
+import TableFoot from './TableFoot';
+import TableHead from './TableHead';
+import TableRow from './TableRow';
+import TableWrapper from './TableWrapper';
 
 Table.Body = TableBody;
 Table.Caption = TableCaption;
@@ -16,5 +17,6 @@ Table.ColumnGroup = TableColumnGroup;
 Table.Foot = TableFoot;
 Table.Head = TableHead;
 Table.Row = TableRow;
+Table.Wrapper = TableWrapper;
 
 export default Table;
