32abe8e5ab83c15decb6edae3dab29296bf6e881
Christian Schlensker <christian@cswebartisan.com>
2018-07-14 02:11:34 -0700

    Fix types in pickCssProps

--- src/utils/pickCSSProps.ts
+++ src/utils/pickCSSProps.ts
@@ -1,7 +1,7 @@
 import cssProps from "./cssProps";
 
-const pickCssProps = props =>
-  Object.keys(props).reduce((finalObject, key) => {
+const pickCssProps = (props: {[key: string]: any}) =>
+  Object.keys(props).reduce((finalObject: {}, key) => {
     if (cssProps[key]) {
       return { ...finalObject, [key]: props[key] };
     }
