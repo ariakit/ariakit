c0c9e8416b0ccfae5a96f70893af6aa81c6663a3
Diego Haz <hazdiego@gmail.com>
2018-07-16 14:52:50 -0300

    Add pickHTMLProps tests

--- src/utils/__tests__/transitions-test.js
+++ src/utils/__tests__/transitions-test.js
@@ -22,75 +22,67 @@ test("hasTransition", () => {
 });
 
 test("expand", () => {
-  expect(render()`${expand()}`).toMatchInlineSnapshot(`""`);
-  expect(render({ expand: true })`${expand()}`).toMatchInlineSnapshot(
-    `"scale(0.00001)"`
-  );
-  expect(render({ expand: "top" })`${expand()}`).toMatchInlineSnapshot(
-    `"scale(0.00001)"`
-  );
+  expect(render()`${expand()}`).toBe("");
+  expect(render({ expand: true })`${expand()}`).toBe("scale(0.00001)");
+  expect(render({ expand: "top" })`${expand()}`).toBe("scale(0.00001)");
 });
 
 test("origin", () => {
-  expect(render()`${origin()}`).toMatchInlineSnapshot(
-    `"calc(50% + 0px) calc(50% + 0px)"`
+  expect(render()`${origin()}`).toBe("calc(50% + 0px) calc(50% + 0px)");
+  expect(render()`${origin({ x: "12px", y: "15px" })}`).toBe(
+    "calc(50% + 12px) calc(50% + 15px)"
   );
-  expect(render()`${origin({ x: "12px", y: "15px" })}`).toMatchInlineSnapshot(
-    `"calc(50% + 12px) calc(50% + 15px)"`
+  expect(render({ expand: true })`${origin()}`).toBe(
+    "calc(50% + 0px) calc(50% + 0px)"
   );
-  expect(render({ expand: true })`${origin()}`).toMatchInlineSnapshot(
-    `"calc(50% + 0px) calc(50% + 0px)"`
+  expect(render({ expand: "top" })`${origin()}`).toBe(
+    "calc(50% + 0px) calc(100% + 0px)"
   );
-  expect(render({ expand: "top" })`${origin()}`).toMatchInlineSnapshot(
-    `"calc(50% + 0px) calc(100% + 0px)"`
+  expect(render({ expand: "right" })`${origin()}`).toBe(
+    "calc(0px + 0px) calc(50% + 0px)"
   );
-  expect(render({ expand: "right" })`${origin()}`).toMatchInlineSnapshot(
-    `"calc(0px + 0px) calc(50% + 0px)"`
+  expect(render({ expand: "bottom" })`${origin()}`).toBe(
+    "calc(50% + 0px) calc(0px + 0px)"
   );
-  expect(render({ expand: "bottom" })`${origin()}`).toMatchInlineSnapshot(
-    `"calc(50% + 0px) calc(0px + 0px)"`
+  expect(render({ expand: "left" })`${origin()}`).toBe(
+    "calc(100% + 0px) calc(50% + 0px)"
   );
-  expect(render({ expand: "left" })`${origin()}`).toMatchInlineSnapshot(
-    `"calc(100% + 0px) calc(50% + 0px)"`
+  expect(render({ expand: true })`${origin({ x: "12px", y: "15px" })}`).toBe(
+    "calc(50% + 12px) calc(50% + 15px)"
   );
-  expect(
-    render({ expand: true })`${origin({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"calc(50% + 12px) calc(50% + 15px)"`);
-  expect(
-    render({ expand: "top" })`${origin({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"calc(50% + 12px) calc(100% + 15px)"`);
-  expect(
-    render({ expand: "right" })`${origin({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"calc(0px + 12px) calc(50% + 15px)"`);
+  expect(render({ expand: "top" })`${origin({ x: "12px", y: "15px" })}`).toBe(
+    "calc(50% + 12px) calc(100% + 15px)"
+  );
+  expect(render({ expand: "right" })`${origin({ x: "12px", y: "15px" })}`).toBe(
+    "calc(0px + 12px) calc(50% + 15px)"
+  );
   expect(
     render({ expand: "bottom" })`${origin({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"calc(50% + 12px) calc(0px + 15px)"`);
-  expect(
-    render({ expand: "left" })`${origin({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"calc(100% + 12px) calc(50% + 15px)"`);
+  ).toBe("calc(50% + 12px) calc(0px + 15px)");
+  expect(render({ expand: "left" })`${origin({ x: "12px", y: "15px" })}`).toBe(
+    "calc(100% + 12px) calc(50% + 15px)"
+  );
 });
 
 test("slide", () => {
-  expect(render()`${slide()}`).toMatchInlineSnapshot(
-    `"translateX(0px) translateY(0px)"`
+  expect(render()`${slide()}`).toBe("translateX(0px) translateY(0px)");
+  expect(render()`${slide({ x: "12px", y: "15px" })}`).toBe(
+    "translateX(12px) translateY(15px)"
   );
-  expect(render()`${slide({ x: "12px", y: "15px" })}`).toMatchInlineSnapshot(
-    `"translateX(12px) translateY(15px)"`
+  expect(render({ slide: true })`${slide()}`).toBe(
+    "translateX(calc(-100% + 0px)) translateY(0px)"
   );
-  expect(render({ slide: true })`${slide()}`).toMatchInlineSnapshot(
-    `"translateX(calc(-100% + 0px)) translateY(0px)"`
+  expect(render({ slide: "top" })`${slide()}`).toBe(
+    "translateX(0px) translateY(calc(100% + 0px))"
   );
-  expect(render({ slide: "top" })`${slide()}`).toMatchInlineSnapshot(
-    `"translateX(0px) translateY(calc(100% + 0px))"`
+  expect(render({ slide: "right" })`${slide()}`).toBe(
+    "translateX(calc(-100% + 0px)) translateY(0px)"
   );
-  expect(render({ slide: "right" })`${slide()}`).toMatchInlineSnapshot(
-    `"translateX(calc(-100% + 0px)) translateY(0px)"`
+  expect(render({ slide: "bottom" })`${slide()}`).toBe(
+    "translateX(0px) translateY(calc(-100% + 0px))"
   );
-  expect(render({ slide: "bottom" })`${slide()}`).toMatchInlineSnapshot(
-    `"translateX(0px) translateY(calc(-100% + 0px))"`
-  );
-  expect(render({ slide: "left" })`${slide()}`).toMatchInlineSnapshot(
-    `"translateX(calc(100% + 0px)) translateY(0px)"`
+  expect(render({ slide: "left" })`${slide()}`).toBe(
+    "translateX(calc(100% + 0px)) translateY(0px)"
   );
   expect(render({ slide: true })`${slide({ defaultValue: "top" })}`).toEqual(
     render({ slide: "top" })`${slide()}`
@@ -104,19 +96,19 @@ test("slide", () => {
   expect(render({ slide: true })`${slide({ defaultValue: "left" })}`).toEqual(
     render({ slide: "left" })`${slide()}`
   );
-  expect(
-    render({ slide: true })`${slide({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"translateX(calc(-100% + 12px)) translateY(15px)"`);
-  expect(
-    render({ slide: "top" })`${slide({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"translateX(12px) translateY(calc(100% + 15px))"`);
-  expect(
-    render({ slide: "right" })`${slide({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"translateX(calc(-100% + 12px)) translateY(15px)"`);
-  expect(
-    render({ slide: "bottom" })`${slide({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"translateX(12px) translateY(calc(-100% + 15px))"`);
-  expect(
-    render({ slide: "left" })`${slide({ x: "12px", y: "15px" })}`
-  ).toMatchInlineSnapshot(`"translateX(calc(100% + 12px)) translateY(15px)"`);
+  expect(render({ slide: true })`${slide({ x: "12px", y: "15px" })}`).toBe(
+    "translateX(calc(-100% + 12px)) translateY(15px)"
+  );
+  expect(render({ slide: "top" })`${slide({ x: "12px", y: "15px" })}`).toBe(
+    "translateX(12px) translateY(calc(100% + 15px))"
+  );
+  expect(render({ slide: "right" })`${slide({ x: "12px", y: "15px" })}`).toBe(
+    "translateX(calc(-100% + 12px)) translateY(15px)"
+  );
+  expect(render({ slide: "bottom" })`${slide({ x: "12px", y: "15px" })}`).toBe(
+    "translateX(12px) translateY(calc(-100% + 15px))"
+  );
+  expect(render({ slide: "left" })`${slide({ x: "12px", y: "15px" })}`).toBe(
+    "translateX(calc(100% + 12px)) translateY(15px)"
+  );
 });
