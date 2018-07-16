31488ff6808d255d1c3e5e5846b5e31cd1e91dac
Joe Ng'ethe <>
2018-07-16 15:42:41 +0300

    used double quotes

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,13 +1,13 @@
-import Table from './Table';
-import TableBody from './TableBody';
-import TableCaption from './TableCaption';
-import TableCell from './TableCell';
-import TableColumn from './TableColumn';
-import TableColumnGroup from './TableColumnGroup';
-import TableFoot from './TableFoot';
-import TableHead from './TableHead';
-import TableRow from './TableRow';
-import TableWrapper from './TableWrapper';
+import Table from "./Table";
+import TableBody from "./TableBody";
+import TableCaption from "./TableCaption";
+import TableCell from "./TableCell";
+import TableColumn from "./TableColumn";
+import TableColumnGroup from "./TableColumnGroup";
+import TableFoot from "./TableFoot";
+import TableHead from "./TableHead";
+import TableRow from "./TableRow";
+import TableWrapper from "./TableWrapper";
 
 Table.Body = TableBody;
 Table.Caption = TableCaption;
