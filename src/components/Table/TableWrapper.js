0aab75dcaea8f6f75d1cf0e303eec4878b685af2
Joe Ng'ethe <>
2018-07-18 12:48:36 +0300

    export TableWrapper as div

--- src/components/Table/TableWrapper.js
+++ src/components/Table/TableWrapper.js
@@ -4,13 +4,11 @@ import { prop } from "styled-tools";
 import as from "../../enhancers/as";
 import Base from "../Base";
 
-const Component = props => <div {...props} />;
-
-const TableWrapper = styled(Base)(Component)`
+const TableWrapper = styled(Base)`
 	width: 100%;
 	overflow-x: auto;
 	-webkit-overflow-scrolling: touch;
 	${prop("theme.TableWrapper")};
 `;
 
-export default as("tableWrapper")(TableWrapper);
+export default as("div")(TableWrapper);
