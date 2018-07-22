daf5b658362935005dc1d3e23b4bc44a6dfdef26
Raphael Thomazella <thomazella9@gmail.com>
2018-07-22 15:59:07 -0300

    improved dev styleguide

--- styleguide.dev.config.js
+++ styleguide.dev.config.js
@@ -1,23 +1,28 @@
 const omit = require("lodash/omit");
 const config = require("./styleguide.config");
 
-module.exports = Object.assign(omit(config, ["styleguideComponents"]), {
-  styles: {
-    StyleGuide: {
-      "@global": {
-        "p, h1, h2, h3, h4, *[class*=lang], *[class*=rsg--sidebar], *[class^=Sidebar], *[class^=Overlay], *[name=rsg-code-editor], *[class*=rsg--toolbar]": {
-          display: "none"
-        },
-        ".rsg--content-3": {
-          "max-width": "100%"
-        },
-        ".rsg--hasSidebar-2": {
-          "padding-left": "initial"
-        },
-        ".rsg--preview-30": {
-          border: "none !important"
+module.exports = Object.assign(
+  omit(config, ["styleguideComponents"]),
+  { title: "Dev Styleguide" },
+  { serverPort: 6061 },
+  {
+    styles: {
+      StyleGuide: {
+        "@global": {
+          "p, h1, h3, h4, *[class*=lang], *[class*=rsg--sidebar], *[class^=Sidebar], *[class^=Overlay], *[class^=rsg--pathline], *[class*=rsg--toolbar], main > .rsg--root-8 > .rsg--root-9:nth-of-type(1)": {
+            display: "none"
+          },
+          ".rsg--content-3": {
+            "max-width": "100%"
+          },
+          ".rsg--hasSidebar-2": {
+            "padding-left": "initial"
+          },
+          ".rsg--preview-30": {
+            border: "none !important"
+          }
         }
       }
     }
   }
-});
+);
