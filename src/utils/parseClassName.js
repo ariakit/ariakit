aef395b2164007d53b1222fb2bcd80e51a87cefe
Juha Tauriainen <juha@bin.fi>
2018-07-30 15:45:13 +0300

    Remove lodash underscore

--- src/utils/parseClassName.js
+++ src/utils/parseClassName.js
@@ -1,6 +1,6 @@
 import uniq from "lodash/uniq";
 
 const parseClassName = className =>
-  className && _.uniq(className.split(" ")).join(" ");
+  className && uniq(className.split(" ")).join(" ");
 
 export default parseClassName;
