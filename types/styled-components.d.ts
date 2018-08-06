70309900034d067b76645ba0c568ad2a5d036893
Christian Schlensker <christian@cswebartisan.com>
2018-08-05 21:48:27 -0700

    add "target" to styled-component class

--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -1,10 +1,14 @@
+/*
+  TODO can be deleted when https://github.com/styled-components/styled-components/pull/1827 gets released
+*/
 import React, { ComponentType } from "react";
 import { StyledComponentClass } from "styled-components";
 
 declare module "styled-components" {
-  /*
-    TODO can be deleted when https://github.com/styled-components/styled-components/pull/1827 gets released
-  */
+  export interface StyledComponentClass<P, T, O = P> {
+    target: ComponentType<P>;
+  }
+
   export function isStyledComponent<Props>(
     target: ComponentType<Props> | StyledComponentClass<Props, any>
   ): target is StyledComponentClass<Props, any>;
