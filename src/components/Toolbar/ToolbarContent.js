e06df0438d77fd6b00a5009d284c862e66f98f7f
Haz <hazdiego@gmail.com>
2018-08-07 00:16:08 -0300

    Fix lint

--- src/components/Toolbar/ToolbarContent.js
+++ src/components/Toolbar/ToolbarContent.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { prop, withProp } from "styled-tools";
+import { prop } from "styled-tools";
 import as from "../../enhancers/as";
 import Base from "../Base";
 
