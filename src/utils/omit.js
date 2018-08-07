8425cf0367d29d3abd198db3fb7905d0fc3de616
Raphael Thomazella <thomazella9@gmail.com>
2018-08-07 17:12:30 -0300

    forEach -> for

--- src/utils/omit.js
+++ src/utils/omit.js
@@ -1,9 +1,12 @@
 const omit = (target, undesired = []) => {
-  let result = {};
-  Object.keys(target).forEach(key => {
-    if (undesired.indexOf(key) === -1)
-      result = Object.assign(result, { [key]: target[key] });
-  });
+  const targetKeys = Object.keys(target);
+  const result = {};
+
+  for (let index = 0; index < targetKeys.length; index += 1) {
+    const key = targetKeys[index];
+    if (undesired.indexOf(key) === -1) result[key] = target[key];
+  }
+
   return result;
 };
 
