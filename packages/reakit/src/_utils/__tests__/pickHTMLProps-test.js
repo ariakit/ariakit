d06ad4ed1db195284acc5c4e831feb18f11151b2
Haz <hazdiego@gmail.com>
2018-08-14 04:11:16 -0300

    Fix lint

--- packages/reakit/src/_utils/__tests__/pickHTMLProps-test.js
+++ packages/reakit/src/_utils/__tests__/pickHTMLProps-test.js
@@ -1,9 +1,7 @@
 import pickHTMLProps from "../pickHTMLProps";
 
 test("pickHTMLProps", () => {
-  expect(
-    pickHTMLProps({ "data-foo": true, foo: true, id: "foo" })
-  ).toEqual({
+  expect(pickHTMLProps({ "data-foo": true, foo: true, id: "foo" })).toEqual({
     "data-foo": true,
     id: "foo"
   });
