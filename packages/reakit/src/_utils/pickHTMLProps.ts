5574b1217d96f5afefb3e6a40520d9d864e4b6b8
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-08-22 09:33:56 +0200

    Change prop validator

--- packages/reakit/src/_utils/pickHTMLProps.ts
+++ packages/reakit/src/_utils/pickHTMLProps.ts
@@ -1,10 +1,10 @@
-import validAttr from "./validAttr";
+import isPropValid from "@emotion/is-prop-valid";
 
 function pickHTMLProps<P extends object>(props: P): Partial<P> {
   const filteredProps: Partial<P> = {};
   // eslint-disable-next-line no-restricted-syntax
   for (const prop in props) {
-    if (validAttr(prop)) {
+    if (isPropValid(prop)) {
       filteredProps[prop] = props[prop];
     }
   }
