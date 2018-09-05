a94d6b54ec39e8e8b2f9f171ccf8b503fd483616
Evgenii Khramkov <ewgeniux@gmail.com>
2018-09-05 21:00:07 +0300

    convert transform to ts

--- packages/reakit/src/_utils/transform.ts
+++ packages/reakit/src/_utils/transform.ts
@@ -1,18 +1,21 @@
 import { prop, ifProp, withProp, switchProp } from "styled-tools";
 import numberToPx from "./numberToPx";
 
-export const hasTransition = props =>
+type CSSValue = number | string;
+
+export const hasTransition = (props: any) =>
   Boolean(props.animated || props.fade || props.slide || props.expand);
 
-export const translate3d = (x, y, z) =>
+export const translate3d = (x: CSSValue, y: CSSValue, z: CSSValue) =>
   `translate3d(${numberToPx(x)}, ${numberToPx(y)}, ${numberToPx(z)})`;
 
 export const origin = (x = "center", y = "center") =>
   `${numberToPx(x)} ${numberToPx(y)}`;
 
-export const calc = (a, b) => `calc(${numberToPx(a)} + ${numberToPx(b)})`;
+export const calc = (a: CSSValue, b: CSSValue) =>
+  `calc(${numberToPx(a)} + ${numberToPx(b)})`;
 
-export const minus = v => `-${numberToPx(v)}`;
+export const minus = (v: CSSValue) => `-${numberToPx(v)}`;
 
 export const expand = ifProp(
   { expand: true },
@@ -53,11 +56,11 @@ export const slideWithProps = withProp(
     switchProp(
       slide,
       {
-        top: translate3d(x, calc(offset, y)),
-        right: translate3d(calc(minus(offset), x), y),
-        bottom: translate3d(x, calc(minus(offset), y)),
-        left: translate3d(calc(offset, x), y)
+        top: translate3d(x, calc(offset, y), 0),
+        right: translate3d(calc(minus(offset), x), y, 0),
+        bottom: translate3d(x, calc(minus(offset), y), 0),
+        left: translate3d(calc(offset, x), y, 0)
       },
-      translate3d(x, y)
+      translate3d(x, y, 0)
     )
 );
