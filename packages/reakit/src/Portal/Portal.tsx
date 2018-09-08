c18bba06cf53532cd884e7a5dcbfce4331cae839
Matheus Gonçalves da Silva <matheus.g.silva@hotmail.com>
2018-09-08 01:50:12 -0300

    feat: Add typescript support to Portal component (#223)

--- packages/reakit/src/Portal/Portal.tsx
+++ packages/reakit/src/Portal/Portal.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { prop } from "styled-tools";
 import hoistNonReactStatics from "hoist-non-react-statics";
 import styled from "../styled";
 import as from "../as";
 import Base from "../Base";
+import { BaseProps } from "../Base/Base";
 
-class Component extends React.Component {
+type ComponentState = {
+  wrapper?: React.ReactNode;
+};
+
+class Component extends React.Component<BaseProps, ComponentState> {
   state = { wrapper: null };
 
   componentDidMount() {
