7ece8394c111f5d85123387d88a6550319836328
Matheus Gonçalves da Silva <matheus.g.silva@hotmail.com>
2018-09-11 17:32:01 -0300

    feat(typescript): Convert Table components into typescript (#231)

---
+++ packages/reakit/src/Table/index.ts
@@ -0,0 +1,13 @@
+import Table from "./Table";
+import TableWrapper from "./TableWrapper";
+
+interface TableComponents {
+  Wrapper: typeof TableWrapper;
+}
+
+const T = Table as typeof Table & TableComponents;
+
+T.Wrapper = TableWrapper;
+
+export * from "./Table";
+export default T;
