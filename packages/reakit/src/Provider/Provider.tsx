680f80a7344465673789db52e00d9cd2b4f12846
Haz <hazdiego@gmail.com>
2018-09-12 20:57:09 -0300

    chore: Update Provider

--- packages/reakit/src/Provider/Provider.tsx
+++ packages/reakit/src/Provider/Provider.tsx
@@ -5,15 +5,30 @@ import {
   Provider as ConstateProvider,
   ProviderProps as ConstateProps
 } from "constate";
-import { ThemeProvider, ThemeProviderProps } from "../styled";
+import { ThemeProvider } from "../styled";
 
-export interface ProviderProps<S extends { [key: string]: any }>
-  extends ConstateProps<S>,
-    ThemeProviderProps<Object> {
+export interface ProviderProps<S, T> extends ConstateProps<S> {
+  theme?: T | ((theme: T) => T);
   children: React.ReactNode;
 }
 
-class Provider<S> extends React.Component<ProviderProps<S>> {
+class Provider<S, T extends object> extends React.Component<
+  ProviderProps<S, T>
+> {
+  static propTypes = {
+    initialState: PropTypes.object,
+    onMount: PropTypes.func,
+    onUpdate: PropTypes.func,
+    onUnmount: PropTypes.func,
+    theme: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    children: PropTypes.node,
+    devtools: PropTypes.bool
+  };
+
+  static defaultProps = {
+    initialState: {}
+  };
+
   render() {
     const { theme, children, ...props } = this.props;
     return (
@@ -27,16 +42,5 @@ class Provider<S> extends React.Component<ProviderProps<S>> {
     );
   }
 }
-
-// @ts-ignore
-Provider.propTypes = {
-  initialState: PropTypes.object,
-  onMount: PropTypes.func,
-  onUpdate: PropTypes.func,
-  onUnmount: PropTypes.func,
-  theme: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-  children: PropTypes.node,
-  devtools: PropTypes.bool
-};
 
 export default Provider;
