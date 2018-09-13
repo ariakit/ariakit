76f52b92649ac13055693c2c9a0a6cbde3a63624
Haz <hazdiego@gmail.com>
2018-09-13 00:36:21 -0300

    fix(website): Internal guide links

--- packages/website/src/utils/getSectionUrl.ts
+++ packages/website/src/utils/getSectionUrl.ts
@@ -2,24 +2,26 @@ import { Section } from "./types";
 
 const getSectionUrl = (
   sections: Section[],
-  sectionOrName: Section | string,
+  sectionOrNameOrSlug: Section | string,
   prepend = "/"
 ): string => {
-  const name =
-    typeof sectionOrName === "string" ? sectionOrName : sectionOrName.name;
+  const nameOrSlug =
+    typeof sectionOrNameOrSlug === "string"
+      ? sectionOrNameOrSlug
+      : sectionOrNameOrSlug.name;
 
   return sections
     .filter(Boolean)
     .reduce(
       (slugs, section) => {
-        if (section.name === name) {
+        if (section.name === nameOrSlug || section.slug === nameOrSlug) {
           return [...slugs, section.slug];
         }
         if (section.sections && section.sections.length) {
           const slugsString = slugs.join("").replace("//", "/");
           const childUrl = getSectionUrl(
             section.sections,
-            name,
+            nameOrSlug,
             slugsString
           ).replace("//", "/");
           if (childUrl !== slugsString) {
