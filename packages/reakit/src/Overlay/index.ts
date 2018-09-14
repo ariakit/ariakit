92a676f73f9caeefd454ae957cc0ceadc874bd61
Evgenii Khramkov <ewgeniux@gmail.com>
2018-09-14 22:44:23 +0300

    conver Overlay component to Typescript

---
+++ packages/reakit/src/Overlay/index.ts
@@ -0,0 +1,21 @@
+import Overlay from "./Overlay";
+import OverlayContainer from "./OverlayContainer";
+import OverlayToggle from "./OverlayToggle";
+import OverlayShow from "./OverlayShow";
+import OverlayHide from "./OverlayHide";
+
+interface OverlayComponents {
+  Container: typeof OverlayContainer;
+  Hide: typeof OverlayHide;
+  Show: typeof OverlayShow;
+  Toggle: typeof OverlayToggle;
+}
+
+const O = Overlay as typeof Overlay & OverlayComponents;
+
+O.Container = OverlayContainer;
+O.Toggle = OverlayToggle;
+O.Show = OverlayShow;
+O.Hide = OverlayHide;
+
+export default Overlay;
