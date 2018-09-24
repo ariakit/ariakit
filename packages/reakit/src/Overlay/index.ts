a2730a53342c89313ad035c18f16b6be12f7832d
Renato Ribeiro <renatoribroman@gmail.com>
2018-09-24 18:48:54 -0300

    refactor(typescript): Change components index to use Object.assign (#252)

--- packages/reakit/src/Overlay/index.ts
+++ packages/reakit/src/Overlay/index.ts
@@ -4,24 +4,15 @@ import OverlayToggle from "./OverlayToggle";
 import OverlayShow from "./OverlayShow";
 import OverlayHide from "./OverlayHide";
 
-interface OverlayComponents {
-  Container: typeof OverlayContainer;
-  Hide: typeof OverlayHide;
-  Show: typeof OverlayShow;
-  Toggle: typeof OverlayToggle;
-}
-
-const O = Overlay as typeof Overlay & OverlayComponents;
-
-O.Container = OverlayContainer;
-O.Toggle = OverlayToggle;
-O.Show = OverlayShow;
-O.Hide = OverlayHide;
-
 export * from "./Overlay";
 export * from "./OverlayContainer";
 export * from "./OverlayToggle";
 export * from "./OverlayShow";
 export * from "./OverlayHide";
 
-export default O;
+export default Object.assign(Overlay, {
+  Container: OverlayContainer,
+  Toggle: OverlayToggle,
+  Show: OverlayShow,
+  Hide: OverlayHide
+});
--- packages/reakit/src/Sidebar/index.ts
+++ packages/reakit/src/Sidebar/index.ts
@@ -4,24 +4,15 @@ import SidebarToggle from "./SidebarToggle";
 import SidebarShow from "./SidebarShow";
 import SidebarHide from "./SidebarHide";
 
-interface SidebarComponents {
-  Container: typeof SidebarContainer;
-  Hide: typeof SidebarHide;
-  Show: typeof SidebarShow;
-  Toggle: typeof SidebarToggle;
-}
-
-const S = Sidebar as typeof Sidebar & SidebarComponents;
-
-S.Container = SidebarContainer;
-S.Toggle = SidebarToggle;
-S.Show = SidebarShow;
-S.Hide = SidebarHide;
-
 export * from "./Sidebar";
 export * from "./SidebarContainer";
 export * from "./SidebarToggle";
 export * from "./SidebarShow";
 export * from "./SidebarHide";
 
-export default S;
+export default Object.assign(Sidebar, {
+  Container: SidebarContainer,
+  Toggle: SidebarToggle,
+  Show: SidebarShow,
+  Hide: SidebarHide
+});
