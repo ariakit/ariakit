3a5986fb6ed219cc41ddb4b0f25dd8fb7914546f
PlayMa256 <matheus.g.silva@hotmail.com>
2018-09-27 15:09:16 -0300

    types: add types to createElementRef

--- packages/reakit/src/_utils/createElementRef.ts
+++ packages/reakit/src/_utils/createElementRef.ts
@@ -1,5 +1,9 @@
 /* eslint-disable no-param-reassign */
-const createElementRef = (scope, property) => element => {
+import * as React from "react";
+
+const createElementRef = (scope: any, property: string) => (
+  element: React.ReactNode | HTMLElement
+) => {
   scope[property] = element;
   if (scope.props && scope.props.elementRef) {
     scope.props.elementRef(element);
