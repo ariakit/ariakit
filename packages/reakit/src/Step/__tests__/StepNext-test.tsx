3bcb4f38300f0f42004679ab90532d678fb773a1
Haz <hazdiego@gmail.com>
2018-10-04 23:35:57 -0300

    test: Fix tests

--- packages/reakit/src/Step/__tests__/StepNext-test.tsx
+++ packages/reakit/src/Step/__tests__/StepNext-test.tsx
@@ -25,14 +25,3 @@ it("calls next and onClick on click", () => {
   expect(props.next).toHaveBeenCalledTimes(1);
   expect(props.onClick).toHaveBeenCalledTimes(1);
 });
-
-it("wrapper should be disabled", () => {
-  const props = {
-    next: jest.fn(),
-    onClick: jest.fn(),
-    loop: false,
-    hasNext: jest.fn().mockReturnValue(false)
-  };
-  const wrapper = mount(<StepNext {...props} />);
-  expect(wrapper.find("Box").props().disabled).toEqual(true);
-});
