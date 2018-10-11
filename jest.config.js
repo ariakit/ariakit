985eb735bdece3d7830a3cb446a20299add97aea
Haz <hazdiego@gmail.com>
2018-10-10 22:33:44 -0300

    Remove enzyme

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ const { defaults } = require("jest-config");
 
 module.exports = {
   rootDir: __dirname,
-  snapshotSerializers: ["enzyme-to-json/serializer", "jest-serializer-html"],
   collectCoverageFrom: [
     "packages/reakit/src/**/*.{js,ts,tsx}",
     "!**/*-test.{js,ts,tsx}"
--- jest.setup.js
+++ jest.setup.js
@@ -2,10 +2,6 @@ import "jest-dom/extend-expect";
 import "jest-styled-components";
 import "react-testing-library/cleanup-after-each";
 import "raf/polyfill";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-configure({ adapter: new Adapter() });
 
 // polyfill for document.createRange
 if (global.document) {
