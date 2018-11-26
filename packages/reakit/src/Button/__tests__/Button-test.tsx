1373cf48568b765387411089d3c2120b3823060c
Lluis Agusti <hi@llu.lu>
2018-11-26 16:40:55 +0100

    test: `Code`, `Divider`, `Field`, `Button` (#294)

---
+++ packages/reakit/src/Button/__tests__/Button-test.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { render } from "react-testing-library";
+import Button from "../Button";
+
+test("html attrs", () => {
+  const { getByTestId } = render(
+    <Button id="test" aria-label="test" data-testid="test" />
+  );
+
+  expect(getByTestId("test")).toHaveAttribute("id", "test");
+  expect(getByTestId("test")).toHaveAttribute("aria-label", "test");
+});
+
+test("styled", () => {
+  const { container } = render(<Button />);
+  expect(container.firstChild).toMatchInlineSnapshot(`
+.c0 {
+  margin: unset;
+  padding: unset;
+  border: unset;
+  background: unset;
+  font: unset;
+  font-family: inherit;
+  font-size: 100%;
+  box-sizing: border-box;
+  background-color: unset;
+  color: inherit;
+}
+
+<button
+  class="c0"
+/>
+`);
+});
