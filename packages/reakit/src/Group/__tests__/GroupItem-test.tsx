1456ce531f580bcec8fdf65950908a72f18071c0
Lluis Agusti <ll@llu.lu>
2018-12-01 11:45:05 +0100

    Test <Group />

---
+++ packages/reakit/src/Group/__tests__/GroupItem-test.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { render } from "react-testing-library";
+import GroupItem from "../GroupItem";
+
+test("html attrs", () => {
+  const { getByTestId } = render(
+    <GroupItem id="test" aria-label="test" data-testid="test" />
+  );
+
+  expect(getByTestId("test")).toHaveAttribute("id", "test");
+  expect(getByTestId("test")).toHaveAttribute("aria-label", "test");
+});
+
+test("styled", () => {
+  const { container } = render(<GroupItem />);
+  expect(container.firstChild).toMatchInlineSnapshot(`
+.c0 {
+  margin: unset;
+  padding: unset;
+  border: unset;
+  background: unset;
+  font: unset;
+  font-family: inherit;
+  font-size: 100%;
+  box-sizing: border-box;
+  background-color: unset;
+  color: inherit;
+}
+
+<div
+  class="c0"
+/>
+`);
+});
