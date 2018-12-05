c59dfd974acb9e67b7f928670844a95c85be644f
ravi <rafael.vitor@jusbrasil.com.br>
2018-12-04 22:12:22 -0300

    Add test to nav component

---
+++ packages/reakit/src/Navigation/__tests__/Navigation-test.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { render } from "react-testing-library";
+import Navigation from "../Navigation";
+
+test("html attrs", () => {
+  const { getByTestId } = render(
+    <Navigation id="test" aria-label="test" data-testid="test" />
+  );
+  expect(getByTestId("test")).toHaveAttribute("id", "test");
+  expect(getByTestId("test")).toHaveAttribute("aria-label", "test");
+});
+test("styled", () => {
+  const { container } = render(<Navigation />);
+  expect(container.firstChild).toMatchInlineSnapshot(`
+.c0 {
+  margin: unset;
+  padding: unset;
+  border: unset;
+  background: unset;
+  font: unset;
+  font-family: inherit;
+  font-size: 100%;
+  box-sizing: border-box;
+  background-color: unset;
+  color: inherit;
+}
+
+<nav
+  class="c0"
+/>
+`);
+});
