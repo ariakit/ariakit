c40c0947925858305f5110c7a9083f0de8ea932d
ravi <rafael.vitor@jusbrasil.com.br>
2018-12-04 22:15:24 -0300

    Add test to List component

---
+++ packages/reakit/src/List/__tests__/List-test.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { render } from "react-testing-library";
+import List from "../List";
+
+test("html attrs", () => {
+  const { getByTestId } = render(
+    <List id="test" aria-label="test" data-testid="test" />
+  );
+  expect(getByTestId("test")).toHaveAttribute("id", "test");
+  expect(getByTestId("test")).toHaveAttribute("aria-label", "test");
+});
+
+test("styled", () => {
+  const { container } = render(<List />);
+  expect(container.firstChild).toMatchInlineSnapshot(`
+.c0 {
+  margin: unset;
+  padding: unset;
+  border: unset;
+  background: unset;
+  font: unset;
+  font-family: inherit;
+  font-size: 100%;
+  box-sizing: border-box;
+  background-color: unset;
+  color: inherit;
+}
+
+<ul
+  class="c0"
+/>
+`);
+});
