230b0fcb73d0d329329f2308a4611f2b175b942a
Haz <hazdiego@gmail.com>
2019-03-13 19:51:23 -0300

    chore: Upgrade dependencies

--- packages/reakit/src/Dialog/__tests__/index-test.tsx
+++ packages/reakit/src/Dialog/__tests__/index-test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render, fireEvent, act, wait } from "react-testing-library";
+import { render, fireEvent, act } from "react-testing-library";
 import {
   focusNextTabbableIn,
   focusPreviousTabbableIn
