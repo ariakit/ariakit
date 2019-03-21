274908db3b925c144306b57db2e5dbd6f22cd105
Haz <hazdiego@gmail.com>
2019-03-21 17:29:04 -0300

    test utils

--- packages/reakit/src/__utils/useUpdateEffect.ts
+++ packages/reakit/src/__utils/useUpdateEffect.ts
@@ -3,7 +3,7 @@ import * as React from "react";
 
 export function useUpdateEffect(
   effect: React.EffectCallback,
-  deps: ReadonlyArray<any> | undefined
+  deps?: ReadonlyArray<any> | undefined
 ) {
   const mounted = React.useRef(false);
   React.useEffect(() => {
