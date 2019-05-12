1f49a21575bd6ebdee0b40ef638f2de61f1f0edd
Haz <hazdiego@gmail.com>
2019-05-12 02:40:34 -0300

    chore: Fix `isTouchDevice` method

--- packages/reakit/src/__utils/isTouchDevice.ts
+++ packages/reakit/src/__utils/isTouchDevice.ts
@@ -1,9 +1,9 @@
 // TODO: Find a better implementation
 
 export function isTouchDevice() {
-  if (process.env.NODE_ENV === "test") return false;
+  if (process.env.NODE_ENV === "test" || typeof window === "undefined")
+    return false;
   return (
-    typeof window !== "undefined" ||
     "ontouchstart" in window ||
     navigator.maxTouchPoints > 0 ||
     navigator.msMaxTouchPoints > 0
