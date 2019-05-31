4ba7f617da8ce2e0ac69819829f317cd31474505
Haz <hazdiego@gmail.com>
2019-05-31 19:44:18 -0300

    feat: Add `unstable_animated` option to `useHiddenState` and its derivatives (#370)

--- packages/reakit/src/Hidden/Hidden.ts
+++ packages/reakit/src/Hidden/Hidden.ts
@@ -7,18 +7,14 @@ import { useAllCallbacks } from "../__utils/useAllCallbacks";
 import { useHiddenState, HiddenStateReturn } from "./HiddenState";
 
 export type HiddenOptions = BoxOptions &
-  Pick<Partial<HiddenStateReturn>, "unstable_hiddenId" | "visible"> & {
-    /**
-     * If `true`, the hidden element attributes will be set in different
-     * timings to enable CSS transitions. This means that you can safely use the `.hidden` selector in the CSS to
-     * create transitions.
-     *  - When it becomes visible, immediatelly remove the `hidden` attribute,
-     * then add the `hidden` class.
-     *  - When it becomes hidden, immediatelly remove the `hidden` class, then
-     * add the `hidden` attribute.
-     */
-    unstable_animated?: boolean;
-  };
+  Pick<
+    Partial<HiddenStateReturn>,
+    | "unstable_hiddenId"
+    | "visible"
+    | "unstable_animating"
+    | "unstable_animated"
+    | "unstable_stopAnimation"
+  >;
 
 export type HiddenHTMLProps = BoxHTMLProps;
 
@@ -28,49 +24,45 @@ export const useHidden = unstable_createHook<HiddenOptions, HiddenHTMLProps>({
   name: "Hidden",
   compose: useBox,
   useState: useHiddenState,
-  keys: ["unstable_animated"],
 
   useProps(
     options,
     {
+      onAnimationEnd: htmlOnAnimationEnd,
       onTransitionEnd: htmlOnTransitionEnd,
       className: htmlClassName,
       style: htmlStyle,
       ...htmlProps
     }
   ) {
-    const [delayedVisible, setDelayedVisible] = React.useState(options.visible);
+    const [hiddenClass, setHiddenClass] = React.useState<string | null>(null);
 
     React.useEffect(() => {
-      if (options.unstable_animated && options.visible) {
-        setDelayedVisible(options.visible);
-      }
-    }, [options.visible, options.unstable_animated]);
+      setHiddenClass(!options.visible ? "hidden" : null);
+    }, [options.visible]);
 
     const onTransitionEnd = React.useCallback(() => {
-      if (options.unstable_animated && !options.visible) {
-        setDelayedVisible(options.visible);
+      if (
+        options.unstable_animated &&
+        options.unstable_stopAnimation &&
+        !options.visible
+      ) {
+        options.unstable_stopAnimation();
       }
-    }, [options.visible, options.unstable_animated]);
+    }, [
+      options.unstable_animated,
+      options.unstable_stopAnimation,
+      options.visible
+    ]);
 
-    // delays hiding
-    const hidden = options.unstable_animated
-      ? options.visible
-        ? false
-        : !delayedVisible
-      : !options.visible;
-
-    // delays showing
-    const shouldAddHiddenClass = options.unstable_animated
-      ? options.visible
-        ? !delayedVisible
-        : true
-      : !options.visible;
+    const animating = options.unstable_animated && options.unstable_animating;
+    const hidden = !options.visible && !animating;
 
     return {
       role: "region",
       id: options.unstable_hiddenId,
-      className: cx(shouldAddHiddenClass && "hidden", htmlClassName),
+      className: cx(hiddenClass, htmlClassName),
+      onAnimationEnd: useAllCallbacks(onTransitionEnd, htmlOnAnimationEnd),
       onTransitionEnd: useAllCallbacks(onTransitionEnd, htmlOnTransitionEnd),
       hidden,
       ...(hidden
--- packages/reakit/src/Hidden/HiddenState.ts
+++ packages/reakit/src/Hidden/HiddenState.ts
@@ -15,6 +15,18 @@ export type HiddenState = {
    * Whether it's visible or not.
    */
   visible: boolean;
+  /**
+   * If `true`, `animating` will be set to `true` when `visible` changes.
+   * It'll wait for `stopAnimation` to be called or a CSS transition ends.
+   * If it's a number, `stopAnimation` will be called automatically after
+   * given milliseconds.
+   */
+  unstable_animated: boolean | number;
+  /**
+   * Whether it's animating or not.
+   * @private
+   */
+  unstable_animating: boolean;
 };
 
 export type HiddenActions = {
@@ -30,23 +42,56 @@ export type HiddenActions = {
    * Toggles the `visible` state
    */
   toggle: () => void;
+  /**
+   * Stops animation. It's called automatically if there's a CSS transition.
+   * It's called after given milliseconds if `animated` is a number.
+   */
+  unstable_stopAnimation: () => void;
 };
 
 export type HiddenInitialState = Partial<
-  Pick<HiddenState, "unstable_hiddenId" | "visible">
+  Pick<HiddenState, "unstable_hiddenId" | "visible" | "unstable_animated">
 >;
 
 export type HiddenStateReturn = HiddenState & HiddenActions;
+
+function useLastValue<T>(value: T) {
+  const lastValue = React.useRef<T | null>(null);
+  React.useLayoutEffect(() => {
+    lastValue.current = value;
+  }, [value]);
+  return lastValue;
+}
 
 export function useHiddenState(
   initialState: unstable_SealedInitialState<HiddenInitialState> = {}
 ): HiddenStateReturn {
   const {
     unstable_hiddenId: hiddenId = unstable_useId("hidden-"),
+    unstable_animated: animated = false,
     visible: sealedVisible = false
   } = unstable_useSealedState(initialState);
 
   const [visible, setVisible] = React.useState(sealedVisible);
+  const [animating, setAnimating] = React.useState(false);
+  const lastVisible = useLastValue(visible);
+
+  if (
+    animated &&
+    !animating &&
+    lastVisible.current != null &&
+    lastVisible.current !== visible
+  ) {
+    // Sets animating to true when when visible changes
+    setAnimating(true);
+  }
+
+  React.useLayoutEffect(() => {
+    if (visible || typeof animated !== "number") return undefined;
+    // Stops animation after an interval defined by animated
+    const id = setTimeout(() => setAnimating(false), animated);
+    return () => clearTimeout(id);
+  }, [animated, visible]);
 
   const show = React.useCallback(() => setVisible(true), []);
 
@@ -54,21 +99,29 @@ export function useHiddenState(
 
   const toggle = React.useCallback(() => setVisible(v => !v), []);
 
+  const stopAnimation = React.useCallback(() => setAnimating(false), []);
+
   return {
     unstable_hiddenId: hiddenId,
+    unstable_animated: animated,
+    unstable_animating: animating,
     visible,
     show,
     hide,
-    toggle
+    toggle,
+    unstable_stopAnimation: stopAnimation
   };
 }
 
 const keys: Array<keyof HiddenStateReturn> = [
   "unstable_hiddenId",
+  "unstable_animated",
+  "unstable_animating",
   "visible",
   "show",
   "hide",
-  "toggle"
+  "toggle",
+  "unstable_stopAnimation"
 ];
 
 useHiddenState.__keys = keys;
