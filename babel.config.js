7580a4ab933bce8c1d1dbbfa2318f4df954dcf64
Haz <hazdiego@gmail.com>
2019-06-27 05:28:32 -0300

    chore: Avoid resolution warnings from babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 const test = process.env.NODE_ENV === "test";
 const prod = process.env.NODE_ENV === "production";
 
+const { warn } = console;
+
+// Prevents resolution warnings from babel-plugin-module-resolver
+// See https://github.com/tleunen/babel-plugin-module-resolver/issues/315
+// eslint-disable-next-line no-console
+console.warn = (...args) => {
+  for (const arg of args) {
+    if (arg.startsWith("Could not resolve") && /src/.test(arg)) {
+      return;
+    }
+  }
+  warn(...args);
+};
+
 module.exports = {
   presets: [
     [
