fe95402c57e6b4f088d7f56f1de1f43001f104b2
Haz <hazdiego@gmail.com>
2019-08-23 21:54:21 -0300

    fix: Fix `Checkbox` `event.target.checked` value inside `onChange` event

---
+++ packages/reakit/src/Checkbox/__utils/useDelayedEvent.ts
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { useLiveRef } from "reakit-utils/useLiveRef";
+
+// https://github.com/reakit/reakit/issues/393
+export function useDelayedEvent<E extends React.SyntheticEvent>(
+  event?: React.EventHandler<E>
+) {
+  const eventRef = useLiveRef(event);
+  const [delayedEvent, setDelayedEvent] = React.useState<E | null>(null);
+
+  React.useLayoutEffect(() => {
+    if (delayedEvent && eventRef.current) {
+      eventRef.current(delayedEvent);
+      setDelayedEvent(null);
+    }
+  }, [delayedEvent]);
+
+  return setDelayedEvent;
+}
