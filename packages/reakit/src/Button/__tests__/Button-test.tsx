b93d10374da1141c2afd2d12e47007122075b9a7
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-11-01 16:01:44 -0300

    test: Fix type on anchor

--- packages/reakit/src/Button/__tests__/Button-test.tsx
+++ packages/reakit/src/Button/__tests__/Button-test.tsx
@@ -16,7 +16,9 @@ test("render", () => {
 test("render anchor", () => {
   const { getByText } = render(<Button as="a">button</Button>);
   expect(getByText("button")).toMatchInlineSnapshot(`
-    <a>
+    <a
+      type="button"
+    >
       button
     </a>
   `);
