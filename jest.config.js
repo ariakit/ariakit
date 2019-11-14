b1c4ff8e38bc3645d5633eb6b774776d1debde3c
Haz <hazdiego@gmail.com>
2019-11-14 14:12:30 +0100

    chore: Upgrade dependencies (#491)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testEnvironment: "jest-environment-jsdom-fifteen",
   rootDir: __dirname,
   collectCoverageFrom: [
     "packages/reakit/src/**/*.{js,ts,tsx}",
--- packages/reakit/src/Menu/__tests__/Menu-test.tsx
+++ packages/reakit/src/Menu/__tests__/Menu-test.tsx
@@ -11,7 +11,10 @@ function createRef(id: string) {
 
 const props: Parameters<typeof Menu>[0] = {
   unstable_hiddenId: "hidden",
-  stops: [{ id: "a", ref: createRef("a") }, { id: "b", ref: createRef("b") }],
+  stops: [
+    { id: "a", ref: createRef("a") },
+    { id: "b", ref: createRef("b") }
+  ],
   move: jest.fn(),
   next: jest.fn(),
   previous: jest.fn(),
