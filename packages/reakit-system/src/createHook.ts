d9500f64c522b7328bfeddf01edbda8f9f1c1e09
Haz <hazdiego@gmail.com>
2019-11-14 13:58:08 +0100

    chore: Upgrade dependencies

--- packages/reakit-system/src/createHook.ts
+++ packages/reakit-system/src/createHook.ts
@@ -49,13 +49,10 @@ export function createHook<O, P>(options: CreateHookOptions<O, P>) {
   }
 
   useHook.__keys = [
-    ...composedHooks.reduce(
-      (allKeys, hook) => {
-        allKeys.push(...(hook.__keys || []));
-        return allKeys;
-      },
-      [] as string[]
-    ),
+    ...composedHooks.reduce((allKeys, hook) => {
+      allKeys.push(...(hook.__keys || []));
+      return allKeys;
+    }, [] as string[]),
     ...(options.useState ? options.useState.__keys : []),
     ...(options.keys || [])
   ];
