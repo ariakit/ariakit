b372e0f31018ac70b06a2f5ed441259706fa95ce
Haz <hazdiego@gmail.com>
2019-11-15 18:07:13 +0100

    chore: Update warning tests

--- packages/reakit-utils/src/__tests__/warning-test.ts
+++ packages/reakit-utils/src/__tests__/warning-test.ts
@@ -1,30 +1,29 @@
-describe("warning", () => {
-  const initialNodeEnv = process.env.NODE_ENV;
-  const warnSpy = jest.spyOn(global.console, "warn").mockImplementation();
+const initialNodeEnv = process.env.NODE_ENV;
+const warnSpy = jest.spyOn(global.console, "warn").mockImplementation();
 
-  afterEach(() => {
-    process.env.NODE_ENV = initialNodeEnv;
-    warnSpy.mockRestore();
+afterEach(() => {
+  process.env.NODE_ENV = initialNodeEnv;
+  warnSpy.mockRestore();
+});
+
+test('log to console.warn when NODE_ENV is not "production"', () => {
+  process.env.NODE_ENV = "development";
+
+  jest.isolateModules(() => {
+    const { warning } = require("../warning");
+    warning(true, "warn", "ing");
   });
 
-  it('logs to console.warn when NODE_ENV is not "production"', () => {
-    process.env.NODE_ENV = "development";
+  expect(warnSpy).toHaveBeenCalledWith("warn\ning");
+});
 
-    jest.isolateModules(() => {
-      const { warning } = require("../warning");
-      warning(true, "warn", "ing");
-    });
+test('do not log to console.warn if NODE_ENV is "production"', () => {
+  process.env.NODE_ENV = "production";
 
-    expect(warnSpy).toHaveBeenCalledWith("warn\ning");
+  jest.isolateModules(() => {
+    const { warning } = require("../warning");
+    warning(true, "warn", "ing");
   });
-  it('does not log to console.warn if NODE_ENV is "production"', () => {
-    process.env.NODE_ENV = "production";
 
-    jest.isolateModules(() => {
-      const { warning } = require("../warning");
-      warning(true, "warn", "ing");
-    });
-
-    expect(warnSpy).not.toHaveBeenCalled();
-  });
+  expect(warnSpy).not.toHaveBeenCalled();
 });
