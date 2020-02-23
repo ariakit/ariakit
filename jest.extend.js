9659f9705ea563867e05b23e5b39cb1ac8f4d0b9
Haz <hazdiego@gmail.com>
2020-02-23 06:38:37 -0300

    WIP

---
+++ jest.extend.js
@@ -0,0 +1,32 @@
+const {
+  matcherHint,
+  printReceived,
+  printExpected
+} = require("jest-matcher-utils");
+const matchers = require("@testing-library/jest-dom/matchers");
+
+function toHaveFocus(element) {
+  const result = matchers.toHaveFocus(element);
+  const { activeElement } = element.ownerDocument;
+  const activeId =
+    activeElement && activeElement.getAttribute("aria-activedescendant");
+  return {
+    ...result,
+    pass: result.pass || activeId === element.id,
+    message: () => {
+      if (activeId) {
+        return [
+          matcherHint(`${this.isNot ? ".not" : ""}.toHaveFocus`, "element", ""),
+          "",
+          "Expected",
+          `  ${printExpected(element)}`,
+          "Received:",
+          `  ${printReceived(element.ownerDocument.getElementById(activeId))}`
+        ].join("\n");
+      }
+      return result.message();
+    }
+  };
+}
+
+expect.extend({ ...matchers, toHaveFocus });
