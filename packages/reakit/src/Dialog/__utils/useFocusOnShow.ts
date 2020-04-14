50928075ae5b115a939f74d72529b904b455d775
Haz <hazdiego@gmail.com>
2020-04-14 18:49:19 -0300

    Simplify code

--- packages/reakit/src/Dialog/__utils/useFocusOnShow.ts
+++ packages/reakit/src/Dialog/__utils/useFocusOnShow.ts
@@ -5,35 +5,6 @@ import { getFirstTabbableIn, ensureFocus } from "reakit-utils/tabbable";
 import { hasFocusWithin } from "reakit-utils/hasFocusWithin";
 import { DialogOptions } from "../Dialog";
 
-function focusOnShow(
-  dialog: HTMLElement,
-  nestedDialogs: Array<React.RefObject<HTMLElement>>,
-  initialFocusRef: DialogOptions["unstable_initialFocusRef"]
-) {
-  // If there're nested open dialogs, let them handle focus
-  if (nestedDialogs.some((child) => !child.current?.hidden)) {
-    return;
-  }
-  if (initialFocusRef?.current) {
-    initialFocusRef.current.focus({ preventScroll: true });
-  } else {
-    const tabbable = getFirstTabbableIn(dialog, true);
-    const isActive = () => hasFocusWithin(dialog);
-    if (tabbable) {
-      ensureFocus(tabbable, { preventScroll: true, isActive });
-    } else {
-      ensureFocus(dialog, { preventScroll: true, isActive });
-      warning(
-        dialog.tabIndex === undefined || dialog.tabIndex < 0,
-        "It's recommended to have at least one tabbable element inside dialog. The dialog element has been automatically focused.",
-        "If this is the intended behavior, pass `tabIndex={0}` to the dialog element to disable this warning.",
-        "See https://reakit.io/docs/dialog/#initial-focus",
-        dialog
-      );
-    }
-  }
-}
-
 export function useFocusOnShow(
   dialogRef: React.RefObject<HTMLElement>,
   nestedDialogs: Array<React.RefObject<HTMLElement>>,
@@ -57,6 +28,28 @@ export function useFocusOnShow(
     if (!dialog) return;
     if (animating) return;
 
-    focusOnShow(dialog, nestedDialogs, initialFocusRef);
+    // If there're nested open dialogs, let them handle focus
+    if (nestedDialogs.some((child) => !child.current?.hidden)) {
+      return;
+    }
+
+    if (initialFocusRef?.current) {
+      initialFocusRef.current.focus({ preventScroll: true });
+    } else {
+      const tabbable = getFirstTabbableIn(dialog, true);
+      const isActive = () => hasFocusWithin(dialog);
+      if (tabbable) {
+        ensureFocus(tabbable, { preventScroll: true, isActive });
+      } else {
+        ensureFocus(dialog, { preventScroll: true, isActive });
+        warning(
+          dialog.tabIndex === undefined || dialog.tabIndex < 0,
+          "It's recommended to have at least one tabbable element inside dialog. The dialog element has been automatically focused.",
+          "If this is the intended behavior, pass `tabIndex={0}` to the dialog element to disable this warning.",
+          "See https://reakit.io/docs/dialog/#initial-focus",
+          dialog
+        );
+      }
+    }
   }, [dialogRef, shouldFocus, animating, nestedDialogs, initialFocusRef]);
 }
