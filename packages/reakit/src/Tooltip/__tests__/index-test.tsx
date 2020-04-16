b19792835043174f3b898b312f3f1db3c83393d3
Haz <hazdiego@gmail.com>
2020-04-16 17:22:41 -0300

    Add tests

--- packages/reakit/src/Tooltip/__tests__/index-test.tsx
+++ packages/reakit/src/Tooltip/__tests__/index-test.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
-import { render, hover, wait } from "reakit-test-utils";
+import { render, hover, wait, focus } from "reakit-test-utils";
 import { Tooltip, TooltipReference, useTooltipState } from "..";
+
+afterEach(() => {
+  hover(document.body);
+});
 
 test("show tooltip on hover", async () => {
   const Test = () => {
@@ -12,12 +16,53 @@ test("show tooltip on hover", async () => {
       </>
     );
   };
-  const { baseElement, getByText } = render(<Test />);
-  const reference = getByText("reference");
-  const tooltip = getByText("tooltip");
-  expect(tooltip).not.toBeVisible();
-  hover(reference);
-  await wait(expect(tooltip).toBeVisible);
+  const { baseElement, getByText: text } = render(<Test />);
+  expect(text("tooltip")).not.toBeVisible();
+  hover(text("reference"));
+  await wait(expect(text("tooltip")).toBeVisible);
   hover(baseElement);
-  await wait(expect(tooltip).not.toBeVisible);
+  expect(text("tooltip")).not.toBeVisible();
+});
+
+test("show only one tooltip", async () => {
+  const Test = () => {
+    const tooltip1 = useTooltipState();
+    const tooltip2 = useTooltipState();
+    return (
+      <>
+        <TooltipReference {...tooltip1}>reference1</TooltipReference>
+        <Tooltip {...tooltip1}>tooltip1</Tooltip>
+        <TooltipReference {...tooltip2}>reference2</TooltipReference>
+        <Tooltip {...tooltip2}>tooltip2</Tooltip>
+      </>
+    );
+  };
+  const { getByText: text } = render(<Test />);
+  expect(text("tooltip1")).not.toBeVisible();
+  expect(text("tooltip2")).not.toBeVisible();
+  focus(text("reference1"));
+  await wait(expect(text("tooltip1")).toBeVisible);
+  expect(text("tooltip2")).not.toBeVisible();
+  hover(text("reference2"));
+  expect(text("tooltip1")).not.toBeVisible();
+  await wait(expect(text("tooltip2")).toBeVisible);
+});
+
+test("show tooltip with a timeout", async () => {
+  const Test = () => {
+    const tooltip = useTooltipState({ unstable_timeout: 250 });
+    return (
+      <>
+        <TooltipReference {...tooltip}>reference</TooltipReference>
+        <Tooltip {...tooltip}>tooltip</Tooltip>
+      </>
+    );
+  };
+  const { baseElement, getByText: text } = render(<Test />);
+  expect(text("tooltip")).not.toBeVisible();
+  hover(text("reference"));
+  expect(text("tooltip")).not.toBeVisible();
+  await wait(expect(text("tooltip")).toBeVisible);
+  hover(baseElement);
+  expect(text("tooltip")).not.toBeVisible();
 });
