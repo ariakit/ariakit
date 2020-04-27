a8f4c5430e91ab29a721fdf0c4bf94285a0d50b8
Haz <hazdiego@gmail.com>
2020-04-27 00:03:47 -0300

    Try a different approach to memoize Menu

--- packages/reakit/src/Menu/__examples__/ChromeMenuBar/index.tsx
+++ packages/reakit/src/Menu/__examples__/ChromeMenuBar/index.tsx
@@ -91,258 +91,23 @@ const menuBar: MenuBarType = [
           { label: "Check Grammar With Spelling", type: "checkbox" },
         ],
       },
-    ],
-  },
-  {
-    label: "Edit",
-    menu: [
-      { label: "Undo" },
-      { label: "Redo" },
-      { type: "separator" },
-      { label: "Cut" },
-      { label: "Copy" },
-      { label: "Paste" },
-      { label: "Paste and Match Style" },
-      { label: "Delete" },
-      { label: "Select All" },
-      { type: "separator" },
       {
-        label: "Find",
+        label: "Substitutions",
         menu: [
-          { label: "Search the Web..." },
+          { label: "Show Substitutions" },
           { type: "separator" },
-          { label: "Find..." },
-          { label: "Find Next" },
-          { label: "Find Previous" },
-          { label: "Use Selection for Find" },
-          { label: "Jump to Selection" },
+          { label: "Smart Quotes", type: "checkbox" },
+          { label: "Smart Dashes", type: "checkbox" },
+          { label: "Text Replacement", type: "checkbox" },
         ],
       },
       {
-        label: "Spelling and Grammar",
-        menu: [
-          { label: "Show Spelling and Grammar" },
-          { label: "Check Document Now" },
-          { label: "Check Spelling While Typing", type: "checkbox" },
-          { label: "Check Grammar With Spelling", type: "checkbox" },
-        ],
+        label: "Speech",
+        menu: [{ label: "Start Speaking" }, { label: "Stop Speaking" }],
       },
-    ],
-  },
-  {
-    label: "Edit",
-    menu: [
-      { label: "Undo" },
-      { label: "Redo" },
       { type: "separator" },
-      { label: "Cut" },
-      { label: "Copy" },
-      { label: "Paste" },
-      { label: "Paste and Match Style" },
-      { label: "Delete" },
-      { label: "Select All" },
-      { type: "separator" },
-      {
-        label: "Find",
-        menu: [
-          { label: "Search the Web..." },
-          { type: "separator" },
-          { label: "Find..." },
-          { label: "Find Next" },
-          { label: "Find Previous" },
-          { label: "Use Selection for Find" },
-          { label: "Jump to Selection" },
-        ],
-      },
-      {
-        label: "Spelling and Grammar",
-        menu: [
-          { label: "Show Spelling and Grammar" },
-          { label: "Check Document Now" },
-          { label: "Check Spelling While Typing", type: "checkbox" },
-          { label: "Check Grammar With Spelling", type: "checkbox" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Edit",
-    menu: [
-      { label: "Undo" },
-      { label: "Redo" },
-      { type: "separator" },
-      { label: "Cut" },
-      { label: "Copy" },
-      { label: "Paste" },
-      { label: "Paste and Match Style" },
-      { label: "Delete" },
-      { label: "Select All" },
-      { type: "separator" },
-      {
-        label: "Find",
-        menu: [
-          { label: "Search the Web..." },
-          { type: "separator" },
-          { label: "Find..." },
-          { label: "Find Next" },
-          { label: "Find Previous" },
-          { label: "Use Selection for Find" },
-          { label: "Jump to Selection" },
-        ],
-      },
-      {
-        label: "Spelling and Grammar",
-        menu: [
-          { label: "Show Spelling and Grammar" },
-          { label: "Check Document Now" },
-          { label: "Check Spelling While Typing", type: "checkbox" },
-          { label: "Check Grammar With Spelling", type: "checkbox" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Edit",
-    menu: [
-      { label: "Undo" },
-      { label: "Redo" },
-      { type: "separator" },
-      { label: "Cut" },
-      { label: "Copy" },
-      { label: "Paste" },
-      { label: "Paste and Match Style" },
-      { label: "Delete" },
-      { label: "Select All" },
-      { type: "separator" },
-      {
-        label: "Find",
-        menu: [
-          { label: "Search the Web..." },
-          { type: "separator" },
-          { label: "Find..." },
-          { label: "Find Next" },
-          { label: "Find Previous" },
-          { label: "Use Selection for Find" },
-          { label: "Jump to Selection" },
-        ],
-      },
-      {
-        label: "Spelling and Grammar",
-        menu: [
-          { label: "Show Spelling and Grammar" },
-          { label: "Check Document Now" },
-          { label: "Check Spelling While Typing", type: "checkbox" },
-          { label: "Check Grammar With Spelling", type: "checkbox" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Edit",
-    menu: [
-      { label: "Undo" },
-      { label: "Redo" },
-      { type: "separator" },
-      { label: "Cut" },
-      { label: "Copy" },
-      { label: "Paste" },
-      { label: "Paste and Match Style" },
-      { label: "Delete" },
-      { label: "Select All" },
-      { type: "separator" },
-      {
-        label: "Find",
-        menu: [
-          { label: "Search the Web..." },
-          { type: "separator" },
-          { label: "Find..." },
-          { label: "Find Next" },
-          { label: "Find Previous" },
-          { label: "Use Selection for Find" },
-          { label: "Jump to Selection" },
-        ],
-      },
-      {
-        label: "Spelling and Grammar",
-        menu: [
-          { label: "Show Spelling and Grammar" },
-          { label: "Check Document Now" },
-          { label: "Check Spelling While Typing", type: "checkbox" },
-          { label: "Check Grammar With Spelling", type: "checkbox" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Edit",
-    menu: [
-      { label: "Undo" },
-      { label: "Redo" },
-      { type: "separator" },
-      { label: "Cut" },
-      { label: "Copy" },
-      { label: "Paste" },
-      { label: "Paste and Match Style" },
-      { label: "Delete" },
-      { label: "Select All" },
-      { type: "separator" },
-      {
-        label: "Find",
-        menu: [
-          { label: "Search the Web..." },
-          { type: "separator" },
-          { label: "Find..." },
-          { label: "Find Next" },
-          { label: "Find Previous" },
-          { label: "Use Selection for Find" },
-          { label: "Jump to Selection" },
-        ],
-      },
-      {
-        label: "Spelling and Grammar",
-        menu: [
-          { label: "Show Spelling and Grammar" },
-          { label: "Check Document Now" },
-          { label: "Check Spelling While Typing", type: "checkbox" },
-          { label: "Check Grammar With Spelling", type: "checkbox" },
-        ],
-      },
-    ],
-  },
-  {
-    label: "Edit",
-    menu: [
-      { label: "Undo" },
-      { label: "Redo" },
-      { type: "separator" },
-      { label: "Cut" },
-      { label: "Copy" },
-      { label: "Paste" },
-      { label: "Paste and Match Style" },
-      { label: "Delete" },
-      { label: "Select All" },
-      { type: "separator" },
-      {
-        label: "Find",
-        menu: [
-          { label: "Search the Web..." },
-          { type: "separator" },
-          { label: "Find..." },
-          { label: "Find Next" },
-          { label: "Find Previous" },
-          { label: "Use Selection for Find" },
-          { label: "Jump to Selection" },
-        ],
-      },
-      {
-        label: "Spelling and Grammar",
-        menu: [
-          { label: "Show Spelling and Grammar" },
-          { label: "Check Document Now" },
-          { label: "Check Spelling While Typing", type: "checkbox" },
-          { label: "Check Grammar With Spelling", type: "checkbox" },
-        ],
-      },
+      { label: "Start Dictation..." },
+      { label: "Emoji & Symbols" },
     ],
   },
 ];
@@ -352,46 +117,47 @@ type MenuProps = MenuButtonHTMLProps & { item: MenuBarType[number] };
 const MyMenu = React.memo(
   React.forwardRef<HTMLButtonElement, MenuProps>(({ item, ...props }, ref) => {
     const menu = useMenuState({ loop: true });
+    const { visible, unstable_popoverStyles, ...lol } = menu;
+    const itemss = React.useMemo(
+      () =>
+        item.menu?.map((subitem, i) => {
+          const id = `${menu.baseId}-${i}`;
+          if ("type" in subitem) {
+            if (subitem.type === "separator") {
+              return <MenuSeparator {...menu} key={id} />;
+            }
+            return (
+              <MenuItemCheckbox {...menu} name={subitem.label} key={id} id={id}>
+                {subitem.label}
+              </MenuItemCheckbox>
+            );
+          }
+          if (subitem.menu) {
+            return (
+              <MenuItem
+                {...menu}
+                key={id}
+                as={MyMenu}
+                id={id}
+                item={subitem as MenuBarType[number]}
+              />
+            );
+          }
+          return (
+            <MenuItem {...menu} key={id} id={id}>
+              {subitem.label}
+            </MenuItem>
+          );
+        }),
+      [item, ...Object.values(lol)]
+    );
     return (
       <>
         <MenuButton {...menu} {...props} ref={ref}>
           {item.label}
         </MenuButton>
         <Menu {...menu} aria-label={item.label}>
-          {item.menu?.map((subitem, i) => {
-            const id = `${menu.baseId}-${i}`;
-            if ("type" in subitem) {
-              if (subitem.type === "separator") {
-                return <MenuSeparator {...menu} key={i} />;
-              }
-              return (
-                <MenuItemCheckbox
-                  {...menu}
-                  name={subitem.label}
-                  key={i}
-                  id={id}
-                >
-                  {subitem.label}
-                </MenuItemCheckbox>
-              );
-            }
-            if (subitem.menu) {
-              return (
-                <MenuItem
-                  {...menu}
-                  key={i}
-                  as={MyMenu}
-                  id={id}
-                  item={subitem as MenuBarType[number]}
-                />
-              );
-            }
-            return (
-              <MenuItem {...menu} key={i} id={id}>
-                {subitem.label}
-              </MenuItem>
-            );
-          })}
+          {itemss}
         </Menu>
       </>
     );
@@ -402,15 +168,17 @@ export default function ChromeMenuBar() {
   const bar = useMenuBarState({ loop: true });
   return (
     <MenuBar {...bar}>
-      {menuBar.map((item, i) => (
-        <MenuItem
-          {...bar}
-          key={i}
-          as={MyMenu}
-          item={item}
-          id={`${bar.baseId}-${i}`}
-        />
-      ))}
+      {[...menuBar, ...menuBar, ...menuBar, ...menuBar, ...menuBar].map(
+        (item, i) => (
+          <MenuItem
+            {...bar}
+            key={i}
+            as={MyMenu}
+            item={item}
+            id={`${bar.baseId}-${i}`}
+          />
+        )
+      )}
     </MenuBar>
   );
 }
