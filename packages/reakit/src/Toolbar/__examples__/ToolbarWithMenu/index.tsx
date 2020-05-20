b670ee53c34f41c5a59f14f7051d3a95b5a4d283
Lluis Agusti <ll@llu.lu>
2020-05-20 12:23:17 +0200

    refactor(code-review): suggestions (4)
    - Fix linting
    - Be less verbose

--- packages/reakit/src/Toolbar/__examples__/ToolbarWithMenu/index.tsx
+++ packages/reakit/src/Toolbar/__examples__/ToolbarWithMenu/index.tsx
@@ -1,22 +1,20 @@
-import React, { RefObject } from "react";
-import { useToolbarState, Toolbar, ToolbarItem } from "reakit/Toolbar";
+import React from "react";
+import {
+  useToolbarState,
+  Toolbar,
+  ToolbarItem,
+  ToolbarProps,
+} from "reakit/Toolbar";
 import { useMenuState, MenuButton, Menu, MenuItem } from "reakit/Menu";
 import { Button } from "reakit/Button";
-import { ToolbarProps } from "../../Toolbar";
 
 const MoreItems = React.forwardRef<HTMLButtonElement, ToolbarProps>(
   (props, ref) => {
     const menu = useMenuState({ placement: "bottom-end" });
-    const buttonRef = ref;
 
     return (
       <>
-        <MenuButton
-          {...menu}
-          {...props}
-          ref={buttonRef}
-          aria-label="Other fruits"
-        >
+        <MenuButton {...menu} {...props} ref={ref} aria-label="Other fruits">
           Other Fruits
         </MenuButton>
         <Menu {...menu} aria-label="Other fruits">
