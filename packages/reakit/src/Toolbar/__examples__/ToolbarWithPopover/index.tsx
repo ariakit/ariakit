9d67d2133f3a1d586e9c19d291e4b1b889c62654
Lluis Agusti <ll@llu.lu>
2020-05-25 12:01:47 +0200

    refactor(reakit): code-review suggestions (2)
    - Fix types

--- packages/reakit/src/Toolbar/__examples__/ToolbarWithPopover/index.tsx
+++ packages/reakit/src/Toolbar/__examples__/ToolbarWithPopover/index.tsx
@@ -9,23 +9,24 @@ import {
 } from "reakit/Popover";
 import { Button } from "reakit/Button";
 
-const PopoverItem = React.forwardRef<HTMLButtonElement, PopoverDisclosureProps>(
-  (props, ref) => {
-    const popover = usePopoverState();
-    return (
-      <>
-        <PopoverDisclosure {...popover} {...props} ref={ref}>
-          Mars
-        </PopoverDisclosure>
-        <Popover {...popover} aria-label="Trip to Mars details" tabIndex={0}>
-          <PopoverArrow {...popover} />
-          This trip lasts three days and takes place in the otherworldly
-          environment of Ares Station.
-        </Popover>
-      </>
-    );
-  }
-);
+const PopoverItem = React.forwardRef<
+  HTMLButtonElement,
+  Partial<PopoverDisclosureProps>
+>((props, ref) => {
+  const popover = usePopoverState();
+  return (
+    <>
+      <PopoverDisclosure {...popover} {...props} ref={ref}>
+        Mars
+      </PopoverDisclosure>
+      <Popover {...popover} aria-label="Trip to Mars details" tabIndex={0}>
+        <PopoverArrow {...popover} />
+        This trip lasts three days and takes place in the otherworldly
+        environment of Ares Station.
+      </Popover>
+    </>
+  );
+});
 
 export default function ToolbarWithPopover() {
   const toolbar = useToolbarState();
