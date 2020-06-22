b3a238c5d5077048d123ba8446e8df2ab046ec65
Fabien Rassinier <frassinier@talend.com>
2020-06-22 22:14:43 +0200

    fix(Tabbable): deal with labels

--- packages/reakit-utils/src/__tests__/isRadio-test.tsx
+++ packages/reakit-utils/src/__tests__/isRadio-test.tsx
@@ -3,13 +3,28 @@ import { render } from "reakit-test-utils";
 import { isRadio } from "../isRadio";
 
 test("isRadio", () => {
-  const { getByLabelText } = render(
+  const { getByText, getByLabelText } = render(
     <>
       <input type="radio" aria-label="item1" />
       <input type="text" aria-label="item2" />
+      <label>
+        <input type="radio" />
+        <span aria-label="item3" />
+      </label>
+      <div>
+        <input id="radio1" type="radio" />
+        <label htmlFor="radio1">item4</label>
+      </div>
+      <div>
+        <label htmlFor="radio2">item5</label>
+        <input id="radio2" type="radio" />
+      </div>
     </>
   );
 
   expect(isRadio(getByLabelText("item1"))).toBe(true);
   expect(isRadio(getByLabelText("item2"))).toBe(false);
+  expect(isRadio(getByLabelText("item3"))).toBe(true);
+  expect(isRadio(getByText("item4"))).toBe(true);
+  expect(isRadio(getByText("item5"))).toBe(true);
 });
--- packages/reakit-utils/src/isRadio.ts
+++ packages/reakit-utils/src/isRadio.ts
@@ -1,21 +1,36 @@
+import { closest } from "./closest";
+
 /**
  * Checks whether `element` is a native HTML radio element or not.
  *
  * @example
  * import { isRadio } from "reakit-utils";
  *
- * isButton(document.querySelector("input[type='radio']")); // true
- * isButton(document.querySelector("div")); // false
+ * isRadio(document.querySelector("input[type='radio']")); // true
+ * isRadio(document.querySelector("div")); // false
  *
  * @returns {boolean}
  */
 export function isRadio(
   element: Element
 ): element is HTMLButtonElement | HTMLInputElement {
+  let input = element as HTMLInputElement | any;
   if (element.tagName === "INPUT") {
-    const input = element as HTMLInputElement;
     return input.type === "radio";
   }
-
+  let label = element as HTMLLabelElement;
+  if (element.tagName === "LABEL") {
+    input = document.getElementById(label.htmlFor);
+    if (input) {
+      return input.type === "radio";
+    }
+  }
+  label = closest(element, "label");
+  if (label) {
+    input = label.querySelector<HTMLInputElement>("input");
+    if (input) {
+      return input.type === "radio";
+    }
+  }
   return false;
 }
