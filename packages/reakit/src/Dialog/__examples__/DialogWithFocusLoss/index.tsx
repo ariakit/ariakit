da6d1854441be78d9626d9a3ac3f639b7f0d73e1
Haz <hazdiego@gmail.com>
2020-07-18 04:39:41 -0300

    Update example and add tests

--- packages/reakit/src/Dialog/__examples__/DialogWithFocusLoss/index.tsx
+++ packages/reakit/src/Dialog/__examples__/DialogWithFocusLoss/index.tsx
@@ -1,34 +1,41 @@
 import * as React from "react";
-import { Button } from "reakit/Button";
+import { Button, ButtonProps } from "reakit/Button";
 import { useDialogState, Dialog, DialogDisclosure } from "reakit/Dialog";
 
-export default function DialogWithFocusLoss() {
-  const dialog1 = useDialogState();
-  const dialog2 = useDialogState({ modal: false });
-  const [on, setOn] = React.useState(true);
-  const [on2, setOn2] = React.useState(true);
+function BlurOnClickButton(props: ButtonProps) {
+  return <Button onClick={(event) => event.currentTarget.blur()} {...props} />;
+}
+
+function UnmountOnFocusButton(props: ButtonProps) {
+  const [mounted, setMounted] = React.useState(true);
+  if (mounted) {
+    return <Button onFocus={() => setMounted(!mounted)} {...props} />;
+  }
+  return null;
+}
+
+function NestedDialog() {
+  const dialog = useDialogState({ modal: false });
   return (
     <>
-      <DialogDisclosure {...dialog1}>Open dialog</DialogDisclosure>
-      <Dialog
-        {...dialog1}
-        hideOnClickOutside={false}
-        aria-label="Dialog with focus loss"
-      >
-        <Button onClick={(event) => event.currentTarget.blur()}>onClick</Button>
-        <DialogDisclosure {...dialog2}>Open dialog 2</DialogDisclosure>
-        <Dialog {...dialog2} aria-label="Dialog with focus loss">
-          <Button onClick={(event) => event.currentTarget.blur()}>
-            onClick
-          </Button>
-          {on && <Button onFocus={() => setOn(!on)}>On</Button>}
-          <Button
-            onFocus={() => setOn2(!on2)}
-            style={{ display: on2 ? "block" : "none" }}
-          >
-            Display
-          </Button>
-        </Dialog>
+      <DialogDisclosure {...dialog}>Open nested dialog</DialogDisclosure>
+      <Dialog {...dialog} aria-label="Nested">
+        <BlurOnClickButton>Nested blur on click</BlurOnClickButton>
+        <UnmountOnFocusButton>Nested unmount on focus</UnmountOnFocusButton>
+      </Dialog>
+    </>
+  );
+}
+
+export default function DialogWithFocusLoss() {
+  const dialog = useDialogState();
+  return (
+    <>
+      <DialogDisclosure {...dialog}>Open dialog</DialogDisclosure>
+      <Dialog {...dialog} aria-label="Focus loss">
+        <BlurOnClickButton>Blur on click</BlurOnClickButton>
+        <UnmountOnFocusButton>Unmount on focus</UnmountOnFocusButton>
+        <NestedDialog />
       </Dialog>
     </>
   );
