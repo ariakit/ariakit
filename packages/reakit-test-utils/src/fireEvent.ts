b30c0e58499267648aefc17c76014d0466bb99ff
Haz <hazdiego@gmail.com>
2020-08-16 20:37:28 -0300

    fix(reakit-test-utils): Fix `fireEvent` when element has `pointerEvents: none`

--- packages/reakit-test-utils/src/fireEvent.ts
+++ packages/reakit-test-utils/src/fireEvent.ts
@@ -1,1 +1,40 @@
-export { fireEvent } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+
+const pointerEvents = [
+  "mouseMove",
+  "mouseOver",
+  "mouseEnter",
+  "mouseOut",
+  "mouseLeave",
+  "mouseDown",
+  "mouseUp",
+  "pointerMove",
+  "pointerOver",
+  "pointerEnter",
+  "pointerOut",
+  "pointerLeave",
+  "pointerDown",
+  "pointerUp",
+  "click",
+] as const;
+
+// Disable pointer events when the element has pointerEvents: none
+for (const event of pointerEvents) {
+  const baseFireEvent = fireEvent[event];
+  fireEvent[event] = (element, options) => {
+    if (element instanceof Element) {
+      const style = getComputedStyle(element);
+      if (style.pointerEvents === "none") {
+        if (element.parentElement) {
+          // Recursive so we'll repeat the process the parent element also has
+          // pointerEvents: none
+          fireEvent[event](element.parentElement, options);
+        }
+        return false;
+      }
+    }
+    return baseFireEvent(element, options);
+  };
+}
+
+export { fireEvent };
