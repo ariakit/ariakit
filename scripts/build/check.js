483ce6c0e123980010b6e1dba24577ee7c1019a8
Tom <the.tomsherman@gmail.com>
2020-08-19 23:02:40 +0100

    Add modified files check script

---
+++ scripts/build/check.js
@@ -0,0 +1,28 @@
+const { execSync } = require("child_process");
+
+const ALLOW_LIST = ["yarn.lock"];
+
+function getModifiedFiles() {
+  const statusOutput = execSync("git status --short").toString();
+
+  return statusOutput
+    .split("\n")
+    .filter((file) => file.length > 0 && !ALLOW_LIST.includes(file))
+    .map((file) => file.slice(3));
+}
+
+const modifiedFiles = getModifiedFiles();
+
+if (modifiedFiles.length === 0) {
+  console.log("Looks great!");
+  process.exit(0);
+}
+
+console.error(
+  `${
+    "here are modified files after running the build, did you forget to checkin after building?\n\n" +
+    "Files changed:\n"
+  }${modifiedFiles.join("\n")}`
+);
+
+process.exit(1);
