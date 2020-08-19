1c37d3ff244faa3e77b5605c3f267ac6507c6f4b
Tom <the.tomsherman@gmail.com>
2020-08-19 23:43:02 +0100

    Explain slice()

--- scripts/build/check.js
+++ scripts/build/check.js
@@ -6,10 +6,14 @@ const ALLOW_LIST = ["yarn.lock"];
 function getModifiedFiles() {
   const statusOutput = execSync("git status --short").toString();
 
-  return statusOutput
-    .split("\n")
-    .filter((file) => file.length > 0 && !ALLOW_LIST.includes(file))
-    .map((file) => file.slice(3));
+  return (
+    statusOutput
+      .split("\n")
+      .filter((file) => file.length > 0 && !ALLOW_LIST.includes(file))
+      // First two characters are flags to indicate what kind of change we are dealing with (added, modified, untracked)
+      // Third character is a space. We discard all three to just get the file path.
+      .map((file) => file.slice(3))
+  );
 }
 
 const modifiedFiles = getModifiedFiles();
