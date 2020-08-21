a22a901d8d46933f349e002dc7a5777f9c923eb5
Navin <navin007.a@gmail.com>
2020-08-21 15:37:43 +0530

    fix: getWindow to use cached window

--- packages/reakit-utils/src/canUseDOM.ts
+++ packages/reakit-utils/src/canUseDOM.ts
@@ -1,19 +1,9 @@
-// Thanks to Fluent UI for doing the [research on IE11 memery leak](https://github.com/microsoft/fluentui/pull/9010#issuecomment-490768427)
-// and for creating this [utils](https://github.com/microsoft/fluentui/blob/f6af0479a54f17fd16b9616a62d6be38686a5c1e/packages/utilities/src/dom/getWindow.ts)
-
-let _window: Window | undefined;
-
-// Note: Accessing "window" in IE11 is somewhat expensive, and calling "typeof window"
-// hits a memory leak, whereas aliasing it and calling "typeof _window" does not.
-// Caching the window value at the file scope lets us minimize the impact.
-try {
-  _window = window;
-} catch (e) {
-  /* no-op */
-}
+import { getWindow } from "./getWindow";
 
 // Check if we can use the DOM. Useful for SSR purposes
 function checkIsBrowser() {
+  const _window = getWindow();
+
   return Boolean(
     typeof _window !== "undefined" &&
       _window.document &&
