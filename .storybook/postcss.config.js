305e195f38effbe54f64f7d1c2566e104062c797
Haz <hazdiego@gmail.com>
2020-08-23 01:16:49 -0300

    chore: Fix examples styles

--- .storybook/postcss.config.js
+++ .storybook/postcss.config.js
@@ -1,13 +1,31 @@
 const path = require("path");
+const convertImports = require("postcss-import");
 const scopify = require("postcss-scopify");
 const { kebabCase } = require("lodash");
 
-function getId(filename) {
-  const basename = path.basename(path.dirname(filename));
-  return `examples--${kebabCase(basename)}`;
+function rewriteRootRule() {
+  return (root) => {
+    root.walkRules((rule) => {
+      rule.selectors = rule.selectors.map((selector) => {
+        if (selector === ":root") {
+          return "&";
+        }
+        return selector;
+      });
+    });
+  };
+}
+
+function addIdScope() {
+  return (root) => {
+    const filename = root.source.input.file;
+    const basename = path.basename(path.dirname(filename));
+    const id = `examples--${kebabCase(basename)}`;
+    return scopify(`#${id}`)(root);
+  };
 }
 
 module.exports = {
   exec: true,
-  plugins: [(root) => scopify(`#${getId(root.source.input.file)}`)(root)],
+  plugins: [convertImports(), rewriteRootRule(), addIdScope()],
 };
