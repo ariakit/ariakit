feab92559a4cf2c2874478ff5a613b2776ba5b0e
Guillaume Clochard <guillaume.clochard@iadvize.com>
2020-09-22 13:51:31 +0200

    fix: handle sort inconsistencies in sortBasedOnDOMPosition

--- packages/reakit/src/Composite/__utils/sortBasedOnDOMPosition.ts
+++ packages/reakit/src/Composite/__utils/sortBasedOnDOMPosition.ts
@@ -6,20 +6,39 @@ type Item = {
 };
 
 export function sortBasedOnDOMPosition<T extends Item>(items: T[]): T[] {
-  const copy = items.slice();
+  const copy = items.map((item, index) => [index, item] as [number, T]).slice();
+
   let isOrderDifferent = false;
+
   copy.sort((a, b) => {
-    const elementA = a.ref.current;
-    const elementB = b.ref.current;
+    const indexA = a[0];
+    const elementA = a[1].ref.current;
+
+    const indexB = b[0];
+    const elementB = b[1].ref.current;
+
     if (!elementA || !elementB) return 0;
+
+    // a before b
     if (isElementPreceding(elementA, elementB)) {
-      isOrderDifferent = true;
+      if (indexA > indexB) {
+        isOrderDifferent = true;
+      }
       return -1;
     }
+
+    // a after b
+
+    if (indexA < indexB) {
+      isOrderDifferent = true;
+    }
+
     return 1;
   });
+
   if (isOrderDifferent) {
-    return copy;
+    return copy.map(([_, item]) => item);
   }
+
   return items;
 }
