1c828f7dabb582faf34c1299d604f8e16aec2cd3
Haz <hazdiego@gmail.com>
2020-09-22 11:33:43 -0300

    Refactor code

--- packages/reakit/src/Composite/__utils/sortBasedOnDOMPosition.ts
+++ packages/reakit/src/Composite/__utils/sortBasedOnDOMPosition.ts
@@ -6,19 +6,14 @@ type Item = {
 };
 
 export function sortBasedOnDOMPosition<T extends Item>(items: T[]): T[] {
-  const copy = items.map((item, index) => [index, item] as [number, T]).slice();
+  const pairs = items.map((item, index) => [index, item] as const);
 
   let isOrderDifferent = false;
 
-  copy.sort((a, b) => {
-    const indexA = a[0];
-    const elementA = a[1].ref.current;
-
-    const indexB = b[0];
-    const elementB = b[1].ref.current;
-
+  pairs.sort(([indexA, a], [indexB, b]) => {
+    const elementA = a.ref.current;
+    const elementB = b.ref.current;
     if (!elementA || !elementB) return 0;
-
     // a before b
     if (isElementPreceding(elementA, elementB)) {
       if (indexA > indexB) {
@@ -26,18 +21,15 @@ export function sortBasedOnDOMPosition<T extends Item>(items: T[]): T[] {
       }
       return -1;
     }
-
     // a after b
-
     if (indexA < indexB) {
       isOrderDifferent = true;
     }
-
     return 1;
   });
 
   if (isOrderDifferent) {
-    return copy.map(([_, item]) => item);
+    return pairs.map(([_, item]) => item);
   }
 
   return items;
