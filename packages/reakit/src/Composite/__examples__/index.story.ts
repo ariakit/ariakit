7db719aa2045e0c36a4c7979f853a543ad73c594
David Szabo <david.szabo97@gmail.com>
2020-11-03 17:24:13 +0100

    Group stories

---
+++ packages/reakit/src/Composite/__examples__/index.story.ts
@@ -0,0 +1,16 @@
+import { Composite } from "../Composite";
+
+export { default as AngularComposite } from "./AngularComposite";
+export { default as ButtonAsDiv } from "./CompositeVirtualAsProp";
+export { default as ButtonAsLink } from "./CompositeWithTooltip";
+export { default as DrillUpComposite } from "./DrillUpComposite";
+export { default as DynamicComposite } from "./DynamicComposite";
+export { default as NestedCompositeItems } from "./NestedCompositeItems";
+export { default as VirtualCompositeWithFocusBlur } from "./VirtualCompositeWithFocusBlur";
+export { default as VirtualCompositeWithTooltip } from "./VirtualCompositeWithTooltip";
+export { default as VirtualNestedCompositeItems } from "./VirtualNestedCompositeItems";
+
+export default {
+  title: "Composite",
+  component: Composite,
+};
