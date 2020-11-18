f00ae8fa67ca304b3750bcb8838a435ded28077d
zak <134242+strass@users.noreply.github.com>
2020-11-18 03:23:13 +0000

    fix tests

--- packages/reakit/src/Alert/__examples__/Alert/index.tsx
+++ packages/reakit/src/Alert/__examples__/Alert/index.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import { Alert as AlertComponent } from "reakit/Alert";
 
 export default function AlertAsAside() {
-  return <AlertComponent as="aside">Alert</AlertComponent>;
+  return <AlertComponent>Alert</AlertComponent>;
 }
--- packages/reakit/src/Alert/__examples__/AlertAsAside/index.tsx
+++ packages/reakit/src/Alert/__examples__/AlertAsAside/index.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import { Alert as AlertComponent } from "reakit/Alert";
 
 export default function Alert() {
-  return <AlertComponent>Alert</AlertComponent>;
+  return <AlertComponent as="aside">Alert</AlertComponent>;
 }
