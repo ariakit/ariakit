6a436bb5adccd53e37604195980bd67e7119972e
Dávid Szabó <david.szabo97@gmail.com>
2020-11-26 15:40:39 +0100

    fix: Native radio input firing onChange callback twice on click (#791)
    Closes #790

--- packages/reakit/src/Radio/__tests__/index-test.tsx
+++ packages/reakit/src/Radio/__tests__/index-test.tsx
@@ -50,6 +50,33 @@ test("onChange", () => {
   expect(radio.checked).toBe(true);
 });
 
+test("Native radio onChange called only once when clicked", async () => {
+  const Test = () => {
+    const { state, setState, ...radio } = useRadioState();
+    const [checked, setChecked] = React.useState(false);
+    const [counter, setCounter] = React.useState(0);
+    const toggle = () => {
+      setCounter((s) => s + 1);
+      setChecked(!checked);
+    };
+    return (
+      <>
+        <label>
+          <Radio {...radio} value="radio" onChange={toggle} checked={checked} />
+          radio
+        </label>
+        <p data-testid="counter">{counter}</p>
+      </>
+    );
+  };
+  const { getByLabelText, getByTestId } = render(<Test />);
+  const radio = getByLabelText("radio") as HTMLInputElement;
+  click(radio);
+
+  const counter = getByTestId("counter") as HTMLElement;
+  expect(counter).toHaveTextContent("1");
+});
+
 test("onChange non-native radio", () => {
   const Test = () => {
     const { state, setState, ...radio } = useRadioState();
