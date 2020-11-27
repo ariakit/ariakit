2ae46a7a4c0c1cfd8ba7542bab57cdb5602a9bf0
David Szabo <david.szabo97@gmail.com>
2020-11-27 12:55:05 +0100

    Restore story

--- packages/reakit/src/Combobox/__examples__/ComboboxNonContextVsContext/index.tsx
+++ packages/reakit/src/Combobox/__examples__/ComboboxNonContextVsContext/index.tsx
@@ -7,6 +7,28 @@ import {
 import "./style.css";
 import * as React from "react";
 
+function ComboboxOriginal() {
+  const items = Array.from({ length: 250 }).map((_, i) => `Item ${i}`);
+
+  const combobox = useComboboxState({
+    values: items,
+    gutter: 8,
+    limit: false,
+    autoSelect: true,
+  });
+
+  return (
+    <>
+      <Combobox {...combobox} aria-label="Color" placeholder="Type a color" />
+      <ComboboxPopover {...combobox} aria-label="Colors">
+        {combobox.matches.map((value) => (
+          <ComboboxOption {...combobox} key={value} value={value} />
+        ))}
+      </ComboboxPopover>
+    </>
+  );
+}
+
 function ComboboxContext() {
   const items = Array.from({ length: 250 }).map((_, i) => `Item ${i}`);
 
@@ -32,6 +54,16 @@ function ComboboxContext() {
 export default function ComboBoxNonContextVsContext() {
   return (
     <>
+      <div>
+        <p>
+          <strong>Original Combobox</strong>, clicking on it may take some time.
+          Use arrows (hold down your down-arrow key) to navigate between
+          elements. You will notice how slow it is.
+        </p>
+
+        <ComboboxOriginal />
+      </div>
+
       <div>
         <p>
           <strong>New Combobox using Context</strong>, clicking on it takes less
