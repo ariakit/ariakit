b39e552a9f80111c32b37798ac7dfff6a657c2c4
David Szabo <david.szabo97@gmail.com>
2020-11-27 10:47:38 +0100

    Typing

--- packages/reakit-system/src/StateContextHoc.tsx
+++ packages/reakit-system/src/StateContextHoc.tsx
@@ -1,35 +1,50 @@
 import React from "react";
 import { unstable_useId as useId } from "reakit/Id";
 
-export const StateContextHoc = (Comp, ctx) => (props) => {
-  const context = React.useContext(ctx);
+export type StateContext<O> = React.Context<{
+  initialState: O;
+  subscribe: StateContextSubscribe<O>;
+}>;
+export type StateContextListener<O> = (options: O) => void;
+export type StateContextSubscribe<O> = (callback: (options: O) => any) => any;
+export type HOCInner<T, O> = (props: O) => T;
 
-  let subscribe;
-  let initialState;
-  if (context) {
-    const { subscribe: _subscribe, ..._initialState } = context;
-    subscribe = _subscribe;
-    initialState = _initialState;
-  }
+export function StateContextHoc<
+  T extends React.ComponentType<any>,
+  O extends React.HTMLAttributes<any>
+>(Comp: T, ctx: StateContext<O>): HOCInner<T, O> {
+  const inner: HOCInner<T, O> = (props: O) => {
+    const context = React.useContext(ctx);
 
-  const { id } = useId({ ...initialState, ...props });
-  const [state, setState] = React.useState(initialState);
+    let subscribe: StateContextSubscribe<O> | null = null;
+    let initialState: O = {} as O;
+    if (context) {
+      const { subscribe: _subscribe, initialState: _initialState } = context;
+      subscribe = _subscribe;
+      initialState = _initialState;
+    }
 
-  React.useEffect(
-    () =>
-      subscribe
-        ? subscribe((nextState) => {
-            if (
-              state.currentId === null ||
-              id === state.currentId ||
-              id === nextState.currentId
-            ) {
-              setState(nextState);
-            }
-          })
-        : undefined,
-    [subscribe, state?.currentId, id, props.id]
-  );
+    const { id } = useId({ ...initialState, ...props });
+    const [state, setState] = React.useState<any>(initialState);
 
-  return <Comp {...state} {...props} id={id} />;
-};
+    React.useEffect(
+      () =>
+        subscribe
+          ? subscribe((nextState: any) => {
+              if (
+                state.currentId === null ||
+                id === state.currentId ||
+                id === nextState.currentId
+              ) {
+                setState(nextState);
+              }
+            })
+          : undefined,
+      [subscribe, state?.currentId, id, props.id]
+    );
+
+    return ((<Comp {...state} {...props} id={id} />) as unknown) as T;
+  };
+
+  return inner;
+}
