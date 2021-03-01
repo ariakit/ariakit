83dd5a6b41fe9cab25516c45fdb42a5c08c31e21
Đoàn Thế Hào <haodt1990@gmail.com>
2021-03-01 15:54:30 +0700

    avoid layout shift when first render popper

--- packages/reakit/src/Menu/__examples__/SimpleMenu/index.tsx
+++ packages/reakit/src/Menu/__examples__/SimpleMenu/index.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import { useMenuState, Menu, MenuButton, MenuItem } from "reakit/Menu";
+import { Portal } from "reakit/Portal";
 
 export default function SimpleMenu() {
   const menu = useMenuState();
   return (
     <>
       <MenuButton {...menu}>Preferences</MenuButton>
-      <Menu {...menu} aria-label="Preferences">
-        <MenuItem {...menu}>Settings</MenuItem>
-        <MenuItem {...menu}>Extensions</MenuItem>
-        <MenuItem {...menu}>Keyboard shortcuts</MenuItem>
-      </Menu>
+      <Portal>
+        <Menu {...menu} aria-label="Preferences">
+          <MenuItem {...menu}>Settings</MenuItem>
+          <MenuItem {...menu}>Extensions</MenuItem>
+          <MenuItem {...menu}>Keyboard shortcuts</MenuItem>
+        </Menu>
+      </Portal>
+      <div style={{ marginTop: "100vh" }}>Bottom</div>
     </>
   );
 }
