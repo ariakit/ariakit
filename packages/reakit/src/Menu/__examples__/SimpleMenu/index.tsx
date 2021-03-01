7920076a9a804d84e49dd09e74fa9b80757ae857
Đoàn Thế Hào <haodt1990@gmail.com>
2021-03-01 16:01:19 +0700

    remove portal from story

--- packages/reakit/src/Menu/__examples__/SimpleMenu/index.tsx
+++ packages/reakit/src/Menu/__examples__/SimpleMenu/index.tsx
@@ -1,20 +1,16 @@
 import * as React from "react";
 import { useMenuState, Menu, MenuButton, MenuItem } from "reakit/Menu";
-import { Portal } from "reakit/Portal";
 
 export default function SimpleMenu() {
   const menu = useMenuState();
   return (
     <>
       <MenuButton {...menu}>Preferences</MenuButton>
-      <Portal>
-        <Menu {...menu} aria-label="Preferences">
-          <MenuItem {...menu}>Settings</MenuItem>
-          <MenuItem {...menu}>Extensions</MenuItem>
-          <MenuItem {...menu}>Keyboard shortcuts</MenuItem>
-        </Menu>
-      </Portal>
-      <div style={{ marginTop: "100vh" }}>Bottom</div>
+      <Menu {...menu} aria-label="Preferences">
+        <MenuItem {...menu}>Settings</MenuItem>
+        <MenuItem {...menu}>Extensions</MenuItem>
+        <MenuItem {...menu}>Keyboard shortcuts</MenuItem>
+      </Menu>
     </>
   );
 }
