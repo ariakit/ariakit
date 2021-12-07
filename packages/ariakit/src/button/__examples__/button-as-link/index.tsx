a23afc8d3ca1c2ed3fdafcefe2ed9956486ced1c
Alex Munoz <AlexMunoz@users.noreply.github.com>
2021-12-08 00:51:07 +0100

    chore: Add button as link example (#940)

---
+++ packages/ariakit/src/button/__examples__/button-as-link/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ariakit/button";
+import "./style.css";
+
+export default function Example() {
+  return (
+    <Button as="a" className="button" href="#">
+      Button
+    </Button>
+  );
+}
