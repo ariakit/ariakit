768514a4acd4899d5a09ca8159ed974b2cbec5eb
saideepesh000 <saideepesh000@gmail.com>
2021-12-13 23:40:07 +0530

    add a hover test

--- packages/ariakit/src/tooltip/__examples__/tooltip/index.tsx
+++ packages/ariakit/src/tooltip/__examples__/tooltip/index.tsx
@@ -3,11 +3,11 @@ import { Tooltip, useTooltipState, TooltipAnchor } from "ariakit/tooltip";
 export default function Example() {
   const tooltip = useTooltipState();
   return (
-    <div>
-      <TooltipAnchor state={tooltip}>
-        Hover on me
+    <>
+      <TooltipAnchor state={tooltip} as="button">
+        Hover on me!
       </TooltipAnchor>
-      <Tooltip state={tooltip}>This is a tooltip example</Tooltip>
-    </div>
+      <Tooltip state={tooltip}>You hovered on a button</Tooltip>
+    </>
   );
 }
--- packages/ariakit/src/tooltip/__examples__/tooltip/test.tsx
+++ packages/ariakit/src/tooltip/__examples__/tooltip/test.tsx
@@ -1,9 +1,19 @@
 import { getByRole, hover, render } from "ariakit-test-utils";
 import Example from ".";
 
+test("render tooltip", () => {
+  render(<Example />);
+  expect(getByRole("button")).toMatchInlineSnapshot(
+  `<button
+  aria-labelledby="r:0"
+  tabindex="0"
+  >
+  Hover on me!
+  </button>`);
+});
 
-test("render", async () => {
-    const { container } = render(<Example />);
-    await hover(container);
-    expect(getByRole("tooltip")).toBeInTheDocument();
-  });
+test("hover tooltip", async () => {
+  render(<Example />);
+  await hover(getByRole("button"));
+  expect(getByRole("tooltip")).toBeVisible();
+});
