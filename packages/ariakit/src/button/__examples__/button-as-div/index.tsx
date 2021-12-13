616cb7d3c2f9a9eb0df19f9ecb6c418de32f615b
Sai Deepesh <saideepesh000@gmail.com>
2021-12-13 15:02:24 +0530

    chore: Add `Button as div` example (#948)

---
+++ packages/ariakit/src/button/__examples__/button-as-div/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ariakit/button";
+import "./style.css";
+
+export default function Example() {
+  return (
+    <Button as="div" className="button">
+      Button
+    </Button>
+  );
+}
---
+++ packages/ariakit/src/button/__examples__/button-as-div/test.tsx
@@ -0,0 +1,22 @@
+import { getByRole, render } from "ariakit-test-utils";
+import { axe } from "jest-axe";
+import Example from ".";
+
+test("a11y", async () => {
+  render(<Example />);
+  expect(await axe(getByRole("button"))).toHaveNoViolations();
+});
+
+test("render button", () => {
+  render(<Example />);
+  expect(getByRole("button")).toMatchInlineSnapshot(`
+    <div
+      class="button"
+      data-command=""
+      role="button"
+      tabindex="0"
+    >
+      Button
+    </div>
+  `);
+});
