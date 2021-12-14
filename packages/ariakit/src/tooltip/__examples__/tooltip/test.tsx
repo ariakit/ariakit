1fdf1b57f2ab673581c5476e4d273811555f781a
saideepesh000 <saideepesh000@gmail.com>
2021-12-14 17:47:05 +0530

    fix lint errors

--- packages/ariakit/src/tooltip/__examples__/tooltip/test.tsx
+++ packages/ariakit/src/tooltip/__examples__/tooltip/test.tsx
@@ -3,34 +3,36 @@ import Example from ".";
 
 test("render tooltip", () => {
   const { baseElement } = render(<Example />);
-  expect(baseElement).toMatchInlineSnapshot(`<body>
-  <div>
-    <button
-      aria-labelledby="r:0"
-      class="button"
-      data-command=""
-      tabindex="0"
-      type="button"
-    >
-      Hover or focus on me
-    </button>
-  </div>
-  <div
-    id="r:0-portal"
-  >
-    <div>
-      <div
-        class="tooltip"
-        hidden=""
-        id="r:0"
-        role="tooltip"
-        style="display: none;"
-      >
-        Tooltip
+  expect(baseElement).toMatchInlineSnapshot(`
+    <body>
+      <div>
+        <button
+          aria-labelledby="r:0"
+          class="button"
+          data-command=""
+          tabindex="0"
+          type="button"
+        >
+          Hover or focus on me
+        </button>
       </div>
-    </div>
-  </div>
-</body>`);
+      <div
+        id="r:0-portal"
+      >
+        <div>
+          <div
+            class="tooltip"
+            hidden=""
+            id="r:0"
+            role="tooltip"
+            style="display: none;"
+          >
+            Tooltip
+          </div>
+        </div>
+      </div>
+    </body>
+  `);
 });
 
 test("show tooltip on hover", async () => {
