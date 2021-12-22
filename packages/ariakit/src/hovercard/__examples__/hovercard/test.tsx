57b1ec0e5d8b582366133ed89970f2cdf15108a6
Leonardo Elias <leonardoelias@Leonardos-MacBook-Pro.local>
2021-12-22 17:08:34 -0300

    chore(hovercard): Add test hover after timeout

--- packages/ariakit/src/hovercard/__examples__/hovercard/test.tsx
+++ packages/ariakit/src/hovercard/__examples__/hovercard/test.tsx
@@ -1,8 +1,23 @@
-import { render } from "ariakit-test-utils";
+import { hover, render, screen, waitFor } from "ariakit-test-utils";
 import { axe } from "jest-axe";
+
 import Example from ".";
 
 test("a11y", async () => {
   const { container } = render(<Example />);
   expect(await axe(container)).toHaveNoViolations();
+});
+
+test("show hovercar on hover after timeout", async () => {
+  render(<Example />);
+  const avatar = screen.getByRole("img", {
+    name: /the a11y project/i,
+  });
+  expect(avatar).toBeVisible();
+  expect(screen.getByRole("dialog", { hidden: true })).not.toBeVisible();
+  await hover(avatar);
+
+  await waitFor(() => expect(screen.getByRole("dialog")).toBeVisible(), {
+    timeout: 2000,
+  });
 });
