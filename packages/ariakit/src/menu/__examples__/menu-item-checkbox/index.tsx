bd1ebe7dd322a338ca3776c52e2f0d57a008951a
Leonardo Elias <leonardo.elias4@gmail.com>
2021-12-23 21:02:13 +0000

    chore: Add `menu-item-checkbox` example  (#961)

---
+++ packages/ariakit/src/menu/__examples__/menu-item-checkbox/index.tsx
@@ -0,0 +1,68 @@
+import {
+  Menu,
+  MenuArrow,
+  MenuButton,
+  MenuButtonArrow,
+  MenuItemCheck,
+  MenuItemCheckbox,
+  useMenuState,
+} from "ariakit/menu";
+import { HiOutlineEye } from "react-icons/hi";
+import "./style.css";
+
+export default function Example() {
+  const menu = useMenuState({
+    defaultValues: {
+      watching: ["issues"],
+    },
+  });
+  const watching = menu.values.watching as string[];
+  return (
+    <>
+      <MenuButton state={menu} className="button">
+        <HiOutlineEye />
+        {!!watching.length ? "Unwatch" : "Watch"}
+        <MenuButtonArrow />
+      </MenuButton>
+      <Menu state={menu} className="menu">
+        <MenuArrow className="menu-arrow" />
+        <MenuItemCheckbox name="watching" value="issues" className="menu-item">
+          <MenuItemCheck />
+          Issues
+        </MenuItemCheckbox>
+        <MenuItemCheckbox
+          name="watching"
+          value="pull-requests"
+          className="menu-item"
+        >
+          <MenuItemCheck />
+          Pull requests
+        </MenuItemCheckbox>
+        <MenuItemCheckbox
+          name="watching"
+          value="releases"
+          className="menu-item"
+        >
+          <MenuItemCheck />
+          Releases
+        </MenuItemCheckbox>
+        <MenuItemCheckbox
+          name="watching"
+          value="discussions"
+          className="menu-item"
+        >
+          <MenuItemCheck />
+          Discussions
+        </MenuItemCheckbox>
+        <MenuItemCheckbox
+          name="watching"
+          value="security-alerts"
+          className="menu-item"
+        >
+          <MenuItemCheck />
+          Security alerts
+        </MenuItemCheckbox>
+      </Menu>
+    </>
+  );
+}
