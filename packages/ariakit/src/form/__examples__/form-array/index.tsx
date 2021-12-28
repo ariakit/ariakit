77bcf0f47e38674fd09e921fad5fa7fee717dada
Haz <hazdiego@gmail.com>
2021-12-28 22:49:31 +0100

    chore: Add `form-array` example

---
+++ packages/ariakit/src/form/__examples__/form-array/index.tsx
@@ -0,0 +1,48 @@
+import {
+  Form,
+  FormError,
+  FormInput,
+  FormLabel,
+  FormPush,
+  FormRemove,
+  FormSubmit,
+  useFormState,
+} from "ariakit";
+import "./style.css";
+
+export default function Example() {
+  const form = useFormState({
+    defaultValues: {
+      guests: [""],
+    },
+  });
+  const { values, names } = form;
+
+  form.useSubmit(() => {
+    alert(JSON.stringify(values, null, 2));
+  });
+
+  return (
+    <Form state={form} className="form">
+      {values.guests.map(
+        (guest, i) =>
+          guest !== null && (
+            <div className="field" key={i}>
+              <div className="field-header">
+                <FormLabel name={names.guests[i]!}>Email address</FormLabel>
+                <FormRemove name={names.guests} index={i}>
+                  Remove
+                </FormRemove>
+              </div>
+              <FormInput name={names.guests[i]!} type="email" />
+              <FormError name={names.guests[i]!} className="error" />
+            </div>
+          )
+      )}
+      <FormPush name={names.guests} value="" className="add-new">
+        Add new guest
+      </FormPush>
+      <FormSubmit className="button">Submit</FormSubmit>
+    </Form>
+  );
+}
