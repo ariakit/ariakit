fe74041885d6bb412a4b8b278f35dbe6399c90d9
Haz <hazdiego@gmail.com>
2022-01-03 23:56:38 +0100

    chore: Update example

--- packages/ariakit/src/disclosure/__examples__/disclosure/index.tsx
+++ packages/ariakit/src/disclosure/__examples__/disclosure/index.tsx
@@ -12,12 +12,13 @@ export default function Example() {
       <Disclosure state={disclosure} className="button">
         What are vegetables?
       </Disclosure>
-      <DisclosureContent state={disclosure} className="content">
-        <ul>
-          <li>🍎 Apple</li>
-          <li>🍇 Grape</li>
-          <li>🍊 Orange</li>
-        </ul>
+      <DisclosureContent state={disclosure}>
+        <p>
+          Vegetables are parts of plants that are consumed by humans or other
+          animals as food. The original meaning is still commonly used and is
+          applied to plants collectively to refer to all edible plant matter,
+          including the flowers, fruits, stems, leaves, roots, and seeds.
+        </p>
       </DisclosureContent>
     </div>
   );
