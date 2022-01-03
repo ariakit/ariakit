56d64e5c11d36fa35ff01f1837abbb0398aa749c
Haz <hazdiego@gmail.com>
2022-01-03 22:07:52 +0100

    Simplify example

--- packages/ariakit/src/hovercard/__examples__/hovercard/index.tsx
+++ packages/ariakit/src/hovercard/__examples__/hovercard/index.tsx
@@ -1,27 +1,36 @@
 import {
   Hovercard,
   HovercardAnchor,
+  HovercardHeading,
   useHovercardState,
 } from "ariakit/hovercard";
-import { Avatar, Content, Profile } from "./components";
 import "./style.css";
 
 export default function Example() {
-  const hovercard = useHovercardState({ placement: "bottom-start" });
-
+  const hovercard = useHovercardState({ gutter: 16 });
   return (
-    <div className="container">
-      <div className="item">
-        <div>
-          <HovercardAnchor state={hovercard}>
-            <Avatar />
-          </HovercardAnchor>
-          <Hovercard state={hovercard} className="hovercard">
-            <Profile />
-          </Hovercard>
-        </div>
-        <Content />
-      </div>
+    <div className="wrapper">
+      <HovercardAnchor
+        state={hovercard}
+        href="https://twitter.com/A11YProject"
+        className="anchor"
+      >
+        @A11YProject
+      </HovercardAnchor>
+      <Hovercard state={hovercard} className="hovercard">
+        <img
+          src="https://pbs.twimg.com/profile_images/1282181187184754688/zr1yW3wE_400x400.png"
+          alt="The A11Y Project"
+          className="avatar"
+        />
+        <HovercardHeading className="username">
+          The A11Y Project
+        </HovercardHeading>
+        <p>A community-driven effort to make digital accessibility easier.</p>
+        <a href="https://twitter.com/A11YProject" className="button">
+          Follow
+        </a>
+      </Hovercard>
     </div>
   );
 }
--- packages/ariakit/src/hovercard/__examples__/hovercard/test.tsx
+++ packages/ariakit/src/hovercard/__examples__/hovercard/test.tsx
@@ -1,23 +1,69 @@
-import { hover, render, screen, waitFor } from "ariakit-test-utils";
+import {
+  act,
+  click,
+  getByRole,
+  hover,
+  render,
+  waitFor,
+} from "ariakit-test-utils";
 import { axe } from "jest-axe";
-
 import Example from ".";
+
+const sleep = (timeout = 1000) =>
+  act(() => new Promise((resolve) => setTimeout(resolve, timeout)));
+
+const getAnchor = () => getByRole("link", { name: "@A11YProject" });
+const getHovercard = () => getByRole("dialog", { hidden: true });
+
+const waitForHovercardToShow = (timeout = 1000) =>
+  waitFor(expect(getHovercard()).toBeVisible, { timeout });
+
+const waitForHovercardToHide = (timeout = 1000) =>
+  waitFor(expect(getHovercard()).not.toBeVisible, { timeout });
 
 test("a11y", async () => {
   const { container } = render(<Example />);
   expect(await axe(container)).toHaveNoViolations();
 });
 
-test("show hovercar on hover after timeout", async () => {
+test("show hovercard on hover after timeout", async () => {
   render(<Example />);
-  const avatar = screen.getByRole("img", {
-    name: /the a11y project/i,
-  });
-  expect(avatar).toBeVisible();
-  expect(screen.getByRole("dialog", { hidden: true })).not.toBeVisible();
-  await hover(avatar);
+  expect(getHovercard()).not.toBeVisible();
+  await hover(getAnchor());
+  expect(getHovercard()).not.toBeVisible();
+  await waitForHovercardToShow();
+});
 
-  await waitFor(() => expect(screen.getByRole("dialog")).toBeVisible(), {
-    timeout: 2000,
-  });
+test("hide hovercard on hover outside after timeout", async () => {
+  const { baseElement } = render(<Example />);
+  await hover(getAnchor());
+  await waitForHovercardToShow();
+  await hover(baseElement);
+  expect(getHovercard()).toBeVisible();
+  await waitForHovercardToHide();
+});
+
+test("keep hovercard visible when it has focus", async () => {
+  const { baseElement } = render(<Example />);
+  await hover(getAnchor());
+  await waitForHovercardToShow();
+  await click(getHovercard());
+  await hover(baseElement);
+  await sleep();
+  await expect(getHovercard()).toBeVisible();
+});
+
+test("keep hovercard visible when hovering out and in quickly", async () => {
+  const { baseElement } = render(<Example />);
+  await hover(getAnchor());
+  await waitForHovercardToShow();
+  await hover(baseElement);
+  await sleep(400);
+  await hover(getAnchor());
+  await expect(getHovercard()).toBeVisible();
+  await hover(baseElement);
+  await sleep(400);
+  await hover(getHovercard());
+  await sleep();
+  await expect(getHovercard()).toBeVisible();
 });
