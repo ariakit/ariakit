dadb73124718480f124e675bcaee1093c9b77d5a
Your Name <you@example.com>
2022-01-13 17:32:54 +0100

    chore: Add `disclosure-animated` example

---
+++ packages/ariakit/src/disclosure/__examples__/disclosure-animated/index.tsx
@@ -0,0 +1,25 @@
+import {
+  Disclosure,
+  DisclosureContent,
+  useDisclosureState,
+} from "ariakit/disclosure";
+import "./style.css";
+
+export default function Example() {
+  const disclosure = useDisclosureState({ animated: true });
+  return (
+    <div className="wrapper">
+      <Disclosure state={disclosure} className="button">
+        What are vegetables?
+      </Disclosure>
+      <DisclosureContent state={disclosure} className="content">
+        <p>
+          Vegetables are parts of plants that are consumed by humans or other
+          animals as food. The original meaning is still commonly used and is
+          applied to plants collectively to refer to all edible plant matter,
+          including the flowers, fruits, stems, leaves, roots, and seeds.
+        </p>
+      </DisclosureContent>
+    </div>
+  );
+}
