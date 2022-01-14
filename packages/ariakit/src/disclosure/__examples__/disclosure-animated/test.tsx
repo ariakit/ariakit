c8fe024a62ef4f11967df07aa0ba5e49ec087a58
Your Name <you@example.com>
2022-01-14 10:05:36 +0100

    updated markup test

--- packages/ariakit/src/disclosure/__examples__/disclosure-animated/test.tsx
+++ packages/ariakit/src/disclosure/__examples__/disclosure-animated/test.tsx
@@ -23,6 +23,7 @@ test("markup", () => {
           What are vegetables?
         </button>
         <div
+          class="content"
           hidden=""
           id="r:0"
           style="display: none;"
@@ -34,36 +35,4 @@ test("markup", () => {
       </div>
     </div>
   `);
-});
-
-test("show/hide on click", async () => {
-  render(<Example />);
-  expect(getContent()).not.toBeVisible();
-  expect(getDisclosure()).toHaveAttribute("aria-expanded", "false");
-  await click(getDisclosure());
-  expect(getContent()).toBeVisible();
-  expect(getDisclosure()).toHaveAttribute("aria-expanded", "true");
-  await click(getDisclosure());
-  expect(getContent()).not.toBeVisible();
-  expect(getDisclosure()).toHaveAttribute("aria-expanded", "false");
-});
-
-test("show/hide on enter", async () => {
-  render(<Example />);
-  expect(getContent()).not.toBeVisible();
-  await press.Tab();
-  await press.Enter();
-  expect(getContent()).toBeVisible();
-  await press.Enter();
-  expect(getContent()).not.toBeVisible();
-});
-
-test("show/hide on space", async () => {
-  render(<Example />);
-  expect(getContent()).not.toBeVisible();
-  await press.Tab();
-  await press.Space();
-  expect(getContent()).toBeVisible();
-  await press.Space();
-  expect(getContent()).not.toBeVisible();
 });
