762856c869baace9ef5ba6bbbd5587b8bd92821a
Haz <hazdiego@gmail.com>
2022-02-03 19:59:07 +0100

    Remove outdated comment

--- packages/ariakit/src/disclosure/disclosure.ts
+++ packages/ariakit/src/disclosure/disclosure.ts
@@ -56,8 +56,6 @@ export const useDisclosure = createHook<DisclosureOptions>(
 
     const onClick = useCallback(
       (event: MouseEvent<HTMLButtonElement>) => {
-        // TODO: Consider doing this assignment on mouse down as well. See the
-        // Select component mouse down.
         state.disclosureRef.current = event.currentTarget;
         onClickProp(event);
         if (event.defaultPrevented) return;
