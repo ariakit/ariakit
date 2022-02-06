119321d98c415b2cb39e20b07e4a35f001c17e97
Haz <hazdiego@gmail.com>
2022-02-06 13:40:02 +0100

    feat: Improve `typeahead` support for repeating keys

--- packages/ariakit/src/composite/composite-typeahead.ts
+++ packages/ariakit/src/composite/composite-typeahead.ts
@@ -8,7 +8,7 @@ import {
   createHook,
 } from "ariakit-utils/system";
 import { As, Options, Props } from "ariakit-utils/types";
-import { CompositeContext, Item } from "./__utils";
+import { CompositeContext, Item, flipItems } from "./__utils";
 import { CompositeState } from "./composite-state";
 
 let chars = "";
@@ -22,8 +22,8 @@ function isValidTypeaheadEvent(event: KeyboardEvent) {
     !event.ctrlKey &&
     !event.altKey &&
     !event.metaKey &&
-    // TODO: We may need to revisit this arabic pattern.
-    /^[ا-يa-z0-9_-]$/i.test(event.key)
+    // Matches any letter or number of any language.
+    /^[\p{Letter}\p{Number}]$/u.test(event.key)
   );
 }
 
@@ -39,14 +39,32 @@ function getEnabledItems(items: Item[]) {
   return items.filter((item) => !item.disabled);
 }
 
-function itemTextStartsWith(text: string) {
-  return (item: Item) => {
-    const itemText = item.ref.current?.textContent;
-    if (!itemText) return false;
-    return normalizeString(itemText)
-      .toLowerCase()
-      .startsWith(text.toLowerCase());
-  };
+function itemTextStartsWith(item: Item, text: string) {
+  const itemText = item.ref.current?.textContent;
+  if (!itemText) return false;
+  return normalizeString(itemText).toLowerCase().startsWith(text.toLowerCase());
+}
+
+function getSameInitialItems(
+  items: Item[],
+  char: string,
+  activeId?: string | null
+) {
+  if (!activeId) return items;
+  const activeItem = items.find((item) => item.id === activeId);
+  if (!activeItem) return items;
+  if (!itemTextStartsWith(activeItem, char)) return items;
+  // Typing "oo" will match "oof" instead of moving to the next item.
+  if (chars !== char && itemTextStartsWith(activeItem, chars)) return items;
+  // If we're looping through the items, we'll want to reset the chars so "oo"
+  // becomes just "o".
+  chars = char;
+  // flipItems will put the previous items at the end of the list so we can loop
+  // through them.
+  return flipItems(
+    items.filter((item) => itemTextStartsWith(item, chars)),
+    activeId
+  ).filter((item) => item.id !== activeId);
 }
 
 /**
@@ -81,7 +99,7 @@ export const useCompositeTypeahead = createHook<CompositeTypeaheadOptions>(
         if (!typeahead) return;
         if (!state?.items) return;
         if (!isValidTypeaheadEvent(event)) return;
-        const items = getEnabledItems(state.items);
+        let items = getEnabledItems(state.items);
         if (!isSelfTargetOrItem(event, items)) return;
         event.preventDefault();
         // We need to clear the previous cleanup timeout so we can append the
@@ -92,8 +110,11 @@ export const useCompositeTypeahead = createHook<CompositeTypeaheadOptions>(
         cleanupTimeoutRef.current = window.setTimeout(() => {
           chars = "";
         }, 500);
-        chars += event.key;
-        const item = items.find(itemTextStartsWith(chars));
+        // Always consider the lowercase version of the key.
+        const char = event.key.toLowerCase();
+        chars += char;
+        items = getSameInitialItems(items, char, state?.activeId);
+        const item = items.find((item) => itemTextStartsWith(item, chars));
         if (item) {
           state.move(item.id);
         } else {
@@ -102,7 +123,13 @@ export const useCompositeTypeahead = createHook<CompositeTypeaheadOptions>(
           chars = "";
         }
       },
-      [onKeyDownCaptureProp, typeahead, state?.items, state?.move]
+      [
+        onKeyDownCaptureProp,
+        typeahead,
+        state?.items,
+        state?.activeId,
+        state?.move,
+      ]
     );
 
     props = {
