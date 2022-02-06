29d0188355c2cbf151596d2adeb3050901199112
Haz <hazdiego@gmail.com>
2022-02-06 23:35:02 +0100

    WIP

--- packages/ariakit/src/select/__examples__/select-item-custom/index.tsx
+++ packages/ariakit/src/select/__examples__/select-item-custom/index.tsx
@@ -2,7 +2,6 @@ import {
   Select,
   SelectArrow,
   SelectItem,
-  SelectItemCheck,
   SelectLabel,
   SelectPopover,
   useSelectState,
@@ -10,28 +9,7 @@ import {
 import startCase from "lodash/startCase";
 import "./style.css";
 
-function getImage(email: string) {
-  const image = `https://i.pravatar.cc/120?u=${email}`;
-  return image;
-}
-
-function renderValue(email: string | string[]) {
-  if (Array.isArray(email)) {
-    if (email.length === 1) {
-      email = email[0]!;
-    } else {
-      return (
-        <>
-          <div className="photo-stack">
-            {email.map((m) => (
-              <img key={m} src={getImage(m)} alt="" className="photo" />
-            ))}
-          </div>
-          <div className="value">{email.length} accounts</div>
-        </>
-      );
-    }
-  }
+function renderValue(email: string) {
   const [username = ""] = email.split("@");
   const name = startCase(username.replace(/[._]/g, " "));
   const image = `https://i.pravatar.cc/120?u=${email}`;
@@ -70,7 +48,6 @@ export default function Example() {
         {accounts.map((email) => (
           <SelectItem key={email} value={email}>
             {renderValue(email)}
-            <SelectItemCheck />
           </SelectItem>
         ))}
       </SelectPopover>
