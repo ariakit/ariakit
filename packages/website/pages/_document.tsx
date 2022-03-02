4fd6a2340efe3a00747a7a43991581865f4e4ed8
Haz <hazdiego@gmail.com>
2022-03-02 06:11:18 +0100

    chore: Update dark mode script placement

--- packages/website/pages/_document.tsx
+++ packages/website/pages/_document.tsx
@@ -6,6 +6,19 @@ import Document, {
   NextScript,
 } from "next/document";
 
+const darkModeScript = `
+function classList(action) {
+  document.documentElement.classList[action]("dark");
+}
+classList(localStorage.theme === "dark" ? "add" : "remove");
+if (!("theme" in localStorage)) {
+  const query = window.matchMedia("(prefers-color-scheme: dark)");
+  classList(query.matches ? "add" : "remove");
+  query.addEventListener("change", (event) => {
+    classList(event.matches ? "add" : "remove");
+  })
+}`;
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -17,6 +30,7 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <body>
+          <script dangerouslySetInnerHTML={{ __html: darkModeScript }} />
           <Main />
           <NextScript />
         </body>
