6f3ff9e60aabdd8c7ee1d5f6c11eceaf89c8e30c
Stuart Casarotto <stuart.casarotto@38thstreetstudios.com>
2022-03-14 09:17:05 -0500

    removed duplicative tests

--- packages/ariakit/src/command/__examples__/command-enter-disabled/test.tsx
+++ packages/ariakit/src/command/__examples__/command-enter-disabled/test.tsx
@@ -1,31 +1,5 @@
 import { getByText, press, render } from "ariakit-test-utils";
-import { axe } from "jest-axe";
 import Example from ".";
-
-test("a11y", async () => {
-  render(<Example />);
-  expect(await axe(getByText("Accessible button"))).toHaveNoViolations();
-});
-
-test("render command as div", () => {
-  render(<Example />);
-  expect(getByText("Accessible button")).toMatchInlineSnapshot(`
-    <div
-      class="button"
-      data-command=""
-      tabindex="0"
-    >
-      Accessible button
-    </div>
-  `);
-});
-
-test("tab", async () => {
-  render(<Example />);
-  expect(getByText("Accessible button")).not.toHaveFocus();
-  await press.Tab();
-  expect(getByText("Accessible button")).toHaveFocus();
-});
 
 test("enter doesn't trigger the alert", async () => {
   const alertMock = jest.spyOn(window, "alert").mockImplementation();
