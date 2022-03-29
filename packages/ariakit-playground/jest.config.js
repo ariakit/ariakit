0635c38575a5f773da32da9db23e12d66991915e
Haz <hazdiego@gmail.com>
2022-03-29 15:34:57 +0200

    Update jest.config.js files

--- packages/ariakit-playground/jest.config.js
+++ packages/ariakit-playground/jest.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 const { join } = require("path");
-const { version: reactVersion } = require("react");
+const React = require("react");
 const config = require("../../jest.config");
 const pkg = require("./package.json");
 
@@ -8,7 +8,7 @@ const { projects, ...baseConfig } = config;
 
 const react18OnlyTests = ["playground"];
 
-const notReact18 = /^(16|17)/.test(reactVersion);
+const notReact18 = /^(16|17)/.test(React.version);
 const testPathIgnorePatterns = notReact18 ? react18OnlyTests : [];
 
 /** @type {import("@jest/types").Config.ProjectConfig} */
