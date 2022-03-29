0a59368c7895185307178909f56de0a0eaa00dd9
Haz <hazdiego@gmail.com>
2022-03-29 14:15:47 +0200

    chore: Remove tooltip snapshot test

--- packages/ariakit/src/tooltip/__examples__/tooltip/test.tsx
+++ packages/ariakit/src/tooltip/__examples__/tooltip/test.tsx
@@ -1,43 +1,6 @@
 import { blur, getByRole, hover, press, render } from "ariakit-test-utils";
 import Example from ".";
 
-test("render tooltip", () => {
-  const { baseElement } = render(<Example />);
-  expect(baseElement).toMatchInlineSnapshot(`
-    <body>
-      <div>
-        <button
-          aria-labelledby=":r0:"
-          class="button"
-          data-command=""
-          tabindex="0"
-          type="button"
-        >
-          Hover or focus on me
-        </button>
-      </div>
-      <div
-        id=":r0:-portal"
-      >
-        <div
-          role="presentation"
-          style="position: absolute; left: 0px; top: 0px; margin: 0px;"
-        >
-          <div
-            class="tooltip"
-            hidden=""
-            id=":r0:"
-            role="tooltip"
-            style="display: none;"
-          >
-            Tooltip
-          </div>
-        </div>
-      </div>
-    </body>
-  `);
-});
-
 test("show tooltip on hover", async () => {
   const { baseElement } = render(<Example />);
   expect(getByRole("tooltip", { hidden: true })).not.toBeVisible();
