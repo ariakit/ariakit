701985784807b1629699165a7761721273c079b0
Haz <hazdiego@gmail.com>
2022-04-04 18:11:40 +0200

    Add tests

--- packages/ariakit/src/menu/__examples__/menu-context-menu/test.tsx
+++ packages/ariakit/src/menu/__examples__/menu-context-menu/test.tsx
@@ -1,12 +1,48 @@
-import { click, getByRole, press, render } from "ariakit-test-utils";
-import { axe } from "jest-axe";
+import {
+  click,
+  fireEvent,
+  getByRole,
+  getByText,
+  press,
+  render,
+} from "ariakit-test-utils";
 import Example from ".";
 
-const getMenuButton = (name: string) => getByRole("button", { name });
-const getMenuItem = (name: string) => getByRole("menuitemcheckbox", { name });
+const getContextMenuArea = () => getByText("Right click here");
+const getMenu = () => getByRole("menu", { hidden: true });
+const getMenuItem = (name: string) => getByRole("menuitem", { name });
 
-test("a11y", () => {
-  // const { container } = render(<Example />);
-  // await click(getMenuButton("Unwatch"));
-  // expect(await axe(container)).toHaveNoViolations();
+test("show context menu and hide it with escape", async () => {
+  render(<Example />);
+  expect(getMenu()).not.toBeVisible();
+  fireEvent.contextMenu(getContextMenuArea());
+  expect(getMenu()).toBeVisible();
+  expect(getMenu()).toHaveFocus();
+  fireEvent.contextMenu(getContextMenuArea());
+  expect(getMenu()).toBeVisible();
+  expect(getMenu()).toHaveFocus();
+  await press.Escape();
+  expect(getMenu()).not.toBeVisible();
+});
+
+test("show context menu and hide it by clicking outside", async () => {
+  render(<Example />);
+  fireEvent.contextMenu(getContextMenuArea());
+  expect(getMenu()).toBeVisible();
+  await click(document.body);
+  expect(getMenu()).not.toBeVisible();
+});
+
+test("navigate through context menu with keyboard", async () => {
+  render(<Example />);
+  fireEvent.contextMenu(getContextMenuArea());
+  expect(getMenu()).toBeVisible();
+  await press.ArrowDown();
+  expect(getMenuItem("Back")).toHaveFocus();
+  await press.ArrowDown();
+  expect(getMenuItem("Reload")).toHaveFocus();
+  await press.Home();
+  expect(getMenuItem("Back")).toHaveFocus();
+  await press.End();
+  expect(getMenuItem("Inspect")).toHaveFocus();
 });
