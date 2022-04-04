d65ffbeabd651de1c5aaf9a87cfe08d026af8c36
Haz <hazdiego@gmail.com>
2022-04-05 00:38:45 +0200

    WIP

--- packages/ariakit-playground/src/__examples__/playground/e2e.ts
+++ packages/ariakit-playground/src/__examples__/playground/e2e.ts
@@ -5,20 +5,21 @@ navigateToExamplePage(__dirname);
 test("edit playground", async () => {
   await expect(page.title()).resolves.toMatch("");
   await page.click(
-    "[role=group][aria-label='index.js'] [contenteditable=true]"
+    "[role=group][aria-label='index.js'] [contenteditable=true]",
+    { delay: 100 }
   );
-  await page.keyboard.press("ArrowUp");
-  await page.keyboard.press("ArrowUp");
+  await page.keyboard.press("ArrowUp", { delay: 100 });
+  await page.keyboard.press("ArrowUp", { delay: 100 });
   await page.keyboard.down("Alt");
-  await page.keyboard.press("ArrowRight");
-  await page.keyboard.press("ArrowRight");
-  await page.keyboard.press("ArrowRight");
-  await page.keyboard.press("ArrowRight");
+  await page.keyboard.press("ArrowRight", { delay: 100 });
+  await page.keyboard.press("ArrowRight", { delay: 100 });
+  await page.keyboard.press("ArrowRight", { delay: 100 });
+  await page.keyboard.press("ArrowRight", { delay: 100 });
   await page.keyboard.down("Shift");
-  await page.keyboard.press("ArrowRight");
+  await page.keyboard.press("ArrowRight", { delay: 100 });
   await page.keyboard.up("Alt");
   await page.keyboard.up("Shift");
-  await page.keyboard.type("Hey");
+  await page.keyboard.type("Hey", { delay: 100 });
 
   await expect(page).toMatchElement(".playground .preview div", {
     text: "Hey World",
