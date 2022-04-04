3c2ed6db4b572dfdf1a10fef5912c40f62d2b5fd
Haz <hazdiego@gmail.com>
2022-04-05 00:48:19 +0200

    WIP

--- packages/ariakit-playground/src/__examples__/playground/e2e.ts
+++ packages/ariakit-playground/src/__examples__/playground/e2e.ts
@@ -13,21 +13,19 @@ test("edit playground", async () => {
     text: "Hello World",
   });
 
-  await page.keyboard.press("ArrowUp", { delay: 100 });
-  await page.keyboard.press("ArrowUp", { delay: 100 });
-  await page.keyboard.down("Alt");
-  await page.keyboard.press("ArrowRight", { delay: 100 });
-  await page.keyboard.press("ArrowRight", { delay: 100 });
-  await page.keyboard.press("ArrowRight", { delay: 100 });
-  await page.keyboard.press("ArrowRight", { delay: 100 });
+  await page.keyboard.press("ArrowUp");
+  await page.keyboard.press("ArrowUp");
+  for await (const _ of Array(14)) {
+    await page.keyboard.press("ArrowRight");
+  }
   await page.keyboard.down("Shift");
-  await page.keyboard.press("ArrowRight", { delay: 100 });
-  await page.keyboard.up("Alt");
-  await page.keyboard.up("Shift");
-  await page.keyboard.type("Hey", { delay: 100 });
+  for await (const _ of Array(5)) {
+    await page.keyboard.press("ArrowRight");
+  }
+  await page.keyboard.type("Hey");
 
   await expect(page).toMatchElement(".playground .preview div", {
     text: "Hey World",
-    timeout: 7000,
+    timeout: 1000,
   });
-}, 10000);
+});
