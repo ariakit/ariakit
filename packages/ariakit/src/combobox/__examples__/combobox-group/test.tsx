ad15148dbb8400f4d02cea46c8209f2604646144
Haz <hazdiego@gmail.com>
2022-04-13 11:53:43 +0200

    Fix `Combobox` with `autoSelect` + `autoComplete="both" props

---
+++ packages/ariakit/src/combobox/__examples__/combobox-group/test.tsx
@@ -0,0 +1,35 @@
+import { getByRole, getByText, press, render, type } from "ariakit-test-utils";
+import { axe } from "jest-axe";
+import Example from ".";
+
+const getCombobox = () => getByRole("combobox");
+
+function getSelectionValue(element: Element) {
+  const input = element as HTMLInputElement;
+  const { selectionStart, selectionEnd } = input;
+  const selectionValue = input.value.slice(selectionStart!, selectionEnd!);
+  return selectionValue;
+}
+
+test("a11y", async () => {
+  const { container } = render(<Example />);
+  expect(await axe(container)).toHaveNoViolations();
+});
+
+test("inline autocomplete", async () => {
+  render(<Example />);
+  await press.Tab();
+  await type("a");
+  expect(getCombobox()).toHaveValue("apple");
+  expect(getSelectionValue(getCombobox())).toBe("pple");
+  await press.ArrowDown();
+  expect(getCombobox()).toHaveValue("Avocado");
+  expect(getSelectionValue(getCombobox())).toBe("");
+  await press.ArrowUp();
+  expect(getCombobox()).toHaveValue("apple");
+  expect(getSelectionValue(getCombobox())).toBe("pple");
+  await type("e");
+  expect(getCombobox()).toHaveValue("ae");
+  expect(getSelectionValue(getCombobox())).toBe("");
+  expect(getByText("No results found")).toBeInTheDocument();
+});
