b70593c5e17867e6b30bb2fb7f5cdea30df9581e
Haz <hazdiego@gmail.com>
2022-04-23 04:12:32 +0200

    Fix Dialog body scroll on iOS

---
+++ .changeset/dialog-prevent-body-scroll-ios.md
@@ -0,0 +1,5 @@
+---
+"ariakit": patch
+---
+
+Fixed `Dialog` not correctly preventing body scroll on iOS. ([#1271](https://github.com/ariakit/ariakit/pull/1271))
--- packages/ariakit/src/dialog/__utils/use-prevent-body-scroll.ts
+++ packages/ariakit/src/dialog/__utils/use-prevent-body-scroll.ts
@@ -1,23 +1,52 @@
+// Based on https://github.com/floating-ui/floating-ui/blob/01aeb53ad7e4d105469b7fdc5057c067b6767a8b/packages/react-dom-interactions/src/FloatingOverlay.tsx
 import { useSafeLayoutEffect } from "ariakit-utils/hooks";
+
+function assignStyle(
+  element: HTMLElement | null | undefined,
+  style: Partial<CSSStyleDeclaration>
+) {
+  if (!element) return () => {};
+  const previousStyle = element.style.cssText;
+  Object.assign(element.style, style);
+  return () => {
+    element.style.cssText = previousStyle;
+  };
+}
 
 export function usePreventBodyScroll(enabled?: boolean) {
   useSafeLayoutEffect(() => {
     if (!enabled) return;
-    const { documentElement } = document;
+    const { documentElement, body } = document;
     const scrollbarWidth = window.innerWidth - documentElement.clientWidth;
-    const previousOverflow = documentElement.style.overflow;
-    const previousPaddingRight = documentElement.style.paddingRight;
+    const scrollX = window.pageXOffset;
+    const scrollY = window.pageYOffset;
+
+    // RTL <body> scrollbar
+    const documentLeft = documentElement.getBoundingClientRect().left;
+    const scrollbarX = Math.round(documentLeft) + documentElement.scrollLeft;
+    const paddingProp = scrollbarX ? "paddingLeft" : "paddingRight";
+
+    const restoreStyle = assignStyle(body, {
+      position: "fixed",
+      overflow: "hidden",
+      top: `-${scrollY}px`,
+      left: `-${scrollX}px`,
+      right: "0",
+      [paddingProp]: `${scrollbarWidth}px`,
+    });
+
     const previousScrollbarWidth =
       documentElement.style.getPropertyValue("--scrollbar-width");
-    documentElement.style.overflow = "hidden";
-    documentElement.style.paddingRight = `${scrollbarWidth}px`;
+
     documentElement.style.setProperty(
       "--scrollbar-width",
       `${scrollbarWidth}px`
     );
+
     return () => {
-      documentElement.style.overflow = previousOverflow;
-      documentElement.style.paddingRight = previousPaddingRight;
+      restoreStyle();
+      window.scrollTo(scrollX, scrollY);
+
       if (previousScrollbarWidth) {
         documentElement.style.setProperty(
           "--scrollbar-width",
