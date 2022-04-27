62c142db0267b3a956061a3381cb9bdad0c42fd2
Haz <hazdiego@gmail.com>
2022-04-27 23:41:36 +0200

    WIP

--- packages/ariakit/src/combobox/__examples__/combobox-textarea/test-chrome.ts
+++ packages/ariakit/src/combobox/__examples__/combobox-textarea/test-chrome.ts
@@ -9,7 +9,7 @@ test("popover is rendered correctly", async ({ page }) => {
   await expect(popover).toBeVisible();
   let boundingBox = await popover.boundingBox();
   expect(boundingBox).toMatchObject({ width: 180, height: 186, y: 336 });
-  expect(boundingBox?.x).toBeCloseTo(516);
+  expect(boundingBox?.x.toString()).toMatch(/(515|516|517)/);
   await textarea.type("\n\n\n\n\n\n\n\n\n\n");
   await textarea.press("ArrowUp");
   await textarea.press("ArrowUp");
@@ -19,9 +19,10 @@ test("popover is rendered correctly", async ({ page }) => {
   boundingBox = await popover.boundingBox();
   expect(boundingBox).toMatchObject({ width: 180, height: 186, y: 346 });
   expect(boundingBox?.x).toBeCloseTo(473);
+  expect(boundingBox?.x.toString()).toMatch(/(472|473|474)/);
   await page.mouse.wheel(0, -50);
   await page.waitForTimeout(250);
   boundingBox = await popover.boundingBox();
   expect(boundingBox).toMatchObject({ width: 180, height: 186, y: 396 });
-  expect(boundingBox?.x).toBeCloseTo(473);
+  expect(boundingBox?.x.toString()).toMatch(/(472|473|474)/);
 });
