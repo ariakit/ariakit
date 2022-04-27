05a285e1270be70510633ec3379a3f9b92e75958
Haz <hazdiego@gmail.com>
2022-04-27 03:31:14 +0200

    WIP

--- packages/ariakit/src/combobox/__examples__/combobox-textarea/test-chrome.ts
+++ packages/ariakit/src/combobox/__examples__/combobox-textarea/test-chrome.ts
@@ -1,4 +1,48 @@
-import { expect, test } from "@playwright/test";
+import * as crypto from "crypto";
+import * as fs from "fs";
+import * as path from "path";
+import { test as baseTest, expect } from "@playwright/test";
+
+const istanbulCLIOutput = path.join(process.cwd(), "coverage");
+
+export function generateUUID(): string {
+  return crypto.randomBytes(16).toString("hex");
+}
+
+export const test = baseTest.extend({
+  context: async ({ context }, use) => {
+    await context.addInitScript(() =>
+      window.addEventListener("beforeunload", () =>
+        (window as any).collectIstanbulCoverage(
+          JSON.stringify((window as any).__coverage__)
+        )
+      )
+    );
+    await fs.promises.mkdir(istanbulCLIOutput, { recursive: true });
+    await context.exposeFunction(
+      "collectIstanbulCoverage",
+      (coverageJSON: string) => {
+        if (coverageJSON)
+          fs.writeFileSync(
+            path.join(
+              istanbulCLIOutput,
+              `playwright_coverage_${generateUUID()}.json`
+            ),
+            coverageJSON
+          );
+      }
+    );
+    await use(context);
+    for (const page of context.pages()) {
+      await page.evaluate(() =>
+        (window as any).collectIstanbulCoverage(
+          JSON.stringify((window as any).__coverage__)
+        )
+      );
+      await page.close();
+    }
+  },
+});
 
 test("basic test", async ({ page }, testInfo) => {
   console.log(testInfo.file);
