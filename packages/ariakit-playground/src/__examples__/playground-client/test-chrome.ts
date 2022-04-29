2359adeb577e0d84c1582e8d33c8fd2bc55e7657
Haz <hazdiego@gmail.com>
2022-04-29 13:09:39 +0200

    Increase timeout

--- packages/ariakit-playground/src/__examples__/playground-client/test-chrome.ts
+++ packages/ariakit-playground/src/__examples__/playground-client/test-chrome.ts
@@ -4,7 +4,9 @@ test("updating code reflects on preview", async ({ page }) => {
   await page.goto("/examples/playground-client");
   const editor = await page.locator("role=textbox[name=index.js]");
   const preview = await page.frameLocator(".preview");
-  await expect(preview.locator("role=button[name=Button]")).toBeVisible();
+  await expect(preview.locator("role=button[name=Button]")).toBeVisible({
+    timeout: 15000,
+  });
   await editor
     .locator("text=<Button>")
     .dblclick({ position: { x: 150, y: 10 } });
