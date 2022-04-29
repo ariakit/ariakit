06cf3788f5bb8cdd717edbee8f2a9644ac1abab8
Haz <hazdiego@gmail.com>
2022-04-29 15:51:01 +0200

    Update snapshot tolerance

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,6 +17,7 @@ const config: PlaywrightTestConfig = {
   },
   expect: {
     toMatchSnapshot: {
+      maxDiffPixels: headed ? undefined : 1,
       maxDiffPixelRatio: headed ? 1 : undefined,
     },
   },
