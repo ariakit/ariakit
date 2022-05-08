45c64f9050ffac8c507a2afdbc65b71dc5501ae0
George Karan <georgekaran12@gmail.com>
2022-05-08 18:32:58 -0300

    Update packages/ariakit/src/tooltip/__examples__/tooltip-fallback-placement/test-chrome.ts
    Co-authored-by: Haz <hazdiego@gmail.com>

--- packages/ariakit/src/tooltip/__examples__/tooltip-fallback-placement/test-chrome.ts
+++ packages/ariakit/src/tooltip/__examples__/tooltip-fallback-placement/test-chrome.ts
@@ -3,7 +3,8 @@ import { Page, devices, expect, test } from "@playwright/test";
 const getCanvas = (page: Page) =>
   page.locator(`role=button[name='Tooltip'] >> xpath=..`);
 
-test("tooltip fallback placement", async ({ page }) => {
+test("tooltip fallback placement", async ({ page }, testInfo) => {
+  testInfo.snapshotSuffix = "";
   await page.goto("/examples/tooltip-fallback-placement");
   // Resize the viewport to ensure it overflows
   await page.setViewportSize(devices["iPhone 8"].viewport);
