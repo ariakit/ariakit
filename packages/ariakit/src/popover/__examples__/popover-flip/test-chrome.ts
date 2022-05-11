8bfac0096b15081af192eb0e21e1aef472a634eb
georgekaran <georgekaran12@gmail.com>
2022-05-11 12:49:24 -0300

    Update popover-flip test

--- packages/ariakit/src/popover/__examples__/popover-flip/test-chrome.ts
+++ packages/ariakit/src/popover/__examples__/popover-flip/test-chrome.ts
@@ -2,9 +2,9 @@ import { devices, expect, test } from "@playwright/test";
 
 test("popover flip", async ({ page }, testInfo) => {
   testInfo.snapshotSuffix = "";
-  await page.goto("/examples/popover-flip");
   // Resize the viewport to ensure it overflows
   await page.setViewportSize(devices["iPhone 8"].viewport);
+  await page.goto("/examples/popover-flip");
   await expect(page.locator("role=dialog[name='Team meeting']")).toBeVisible();
   // Expect the popover to be placed on the bottom
   expect(await page.screenshot()).toMatchSnapshot();
