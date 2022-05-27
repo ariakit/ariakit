e9d87d9298571eba3c046f83c8c86e5a34ad7101
Haz <hazdiego@gmail.com>
2022-05-27 03:27:01 +0200

    Adjust website color-scheme

--- packages/website/components/markdown-page/index.tsx
+++ packages/website/components/markdown-page/index.tsx
@@ -15,7 +15,7 @@ const { Compiler: renderAst } = new RehypeReact({
   Fragment: Fragment,
   components: {
     p: (props) => {
-      // @ts-expect-error
+      // @ts-ignore
       const [child] = props.children;
       if (child.props && "data-playground" in child.props) {
         return <>{props.children}</>;
@@ -23,6 +23,7 @@ const { Compiler: renderAst } = new RehypeReact({
       return <p {...props} />;
     },
     pre: (props) => {
+      // @ts-ignore
       const [child] = props.children;
       if (child.type === "code") {
         return (
@@ -79,12 +80,10 @@ export default function MarkdownPage(props) {
             if (document.documentElement.classList.contains("dark")) {
               document.documentElement.classList.remove("dark");
               document.documentElement.classList.add("light");
-              document.documentElement.style.colorScheme = "light";
               localStorage.setItem("theme", "light");
             } else {
               document.documentElement.classList.remove("light");
               document.documentElement.classList.add("dark");
-              document.documentElement.style.colorScheme = "dark";
               localStorage.setItem("theme", "dark");
             }
           }}
