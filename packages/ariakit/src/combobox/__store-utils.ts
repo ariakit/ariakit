c45382e697ebfe1005c171b458b76c18e5ae2990
Haz <hazdiego@gmail.com>
2022-11-28 01:43:56 +0100

    WIP

--- packages/ariakit/src/combobox/__store-utils.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { createStoreContext } from "ariakit-react-utils/store";
-import { ComboboxStore } from "./store-combobox-store";
-
-export const ComboboxContext = createStoreContext<ComboboxStore>();
-
-export const ComboboxItemValueContext = createContext<string | undefined>(
-  undefined
-);
--- packages/ariakit/src/popover/__store-utils.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { PopoverStore } from "./store-popover-store";
-
-export const PopoverContext = createContext<PopoverStore | undefined>(
-  undefined
-);
