b543bb1a6b7ddb7fc8161ccc074b223c4313463c
Haz <hazdiego@gmail.com>
2023-03-23 10:29:19 +0100

    WIP

--- scripts/build/build.mjs
+++ scripts/build/build.mjs
@@ -1,8 +1,11 @@
 import "./clean.mjs";
+import { join } from "path";
+import { fileURLToPath } from "url";
 import spawn from "cross-spawn";
-import { build } from "esbuild";
+// import { build } from "esbuild";
 import fse from "fs-extra";
 import { globSync } from "glob";
+import { build } from "tsup";
 import {
   getMainDir,
   getModuleDir,
@@ -25,34 +28,57 @@ const publicFiles = getPublicFiles(sourcePath);
 makeGitignore(cwd);
 makeProxies(cwd);
 
-const esmDir = getModuleDir(cwd);
-
-spawn.sync("tsc", ["--emitDeclarationOnly", "--outDir", esmDir], {
-  stdio: "inherit",
-});
-
 await build({
-  entryPoints: publicFiles,
-  outdir: esmDir,
-  format: "esm",
-  platform: "node",
-  target: ["chrome58", "edge18", "firefox57", "safari11"],
-  bundle: true,
-  splitting: true,
-  packages: "external",
-  chunkNames: "__chunks/[hash]",
-});
-
-const cjsDir = getMainDir(cwd);
-
-fse.copySync(esmDir, cjsDir);
-
-const builtFiles = globSync(`${cjsDir}/**/*.js`);
-
-await build({
-  entryPoints: builtFiles,
-  outdir: cjsDir,
+  entry: publicFiles,
   format: "cjs",
-  platform: "node",
-  allowOverwrite: true,
+  outDir: getMainDir(cwd),
+  splitting: true,
+  clean: true,
 });
+
+// spawn.sync("tsc", ["--emitDeclarationOnly", "--outDir", esmDir], {
+//   stdio: "inherit",
+// });
+
+// await build({
+//   entryPoints: publicFiles,
+//   outdir: esmDir,
+//   format: "esm",
+//   platform: "node",
+//   target: ["chrome58", "edge18", "firefox57", "safari11"],
+//   bundle: true,
+//   splitting: true,
+//   packages: "external",
+//   chunkNames: "__chunks/[hash]",
+// });
+
+// const cjsDir = getMainDir(cwd);
+
+// fse.copySync(esmDir, cjsDir, {
+//   filter: (src) => src.endsWith(".d.ts"),
+// });
+
+// const builtFiles = globSync(`${esmDir}/**/*.js`);
+
+// await build({
+//   entryPoints: builtFiles,
+//   outdir: cjsDir,
+//   format: "cjs",
+//   platform: "node",
+//   outExtension: { ".js": ".cjs" },
+//   plugins: [
+//     {
+//       name: "add-cjs",
+//       setup(build) {
+//         build.onLoad({ filter: /\.js$/ }, (args) => {
+//           return {
+//             path: args.path
+//               .replace(/^\.\/esm/, "./cjs")
+//               .replace(/\.js$/, ".cjs"),
+//             namespace: args.resolveDir,
+//           };
+//         });
+//       },
+//     },
+//   ],
+// });
