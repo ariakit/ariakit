112eec9db0a61698a639dd7d6049f7d7fd791816
Haz <hazdiego@gmail.com>
2023-04-25 20:22:40 +0200

    Update form-radio icon

--- examples/form-radio/icon.tsx
+++ examples/form-radio/icon.tsx
@@ -1,12 +1,3 @@
-function getItem(textWidth = "w-10") {
-  return (
-    <div className="flex items-center gap-2">
-      <div className="h-2.5 w-2.5 rounded-full border-2 border-blue-600 bg-white dark:bg-black" />
-      <div className={`h-1 ${textWidth} bg-black/70 dark:bg-white/70`} />
-    </div>
-  );
-}
-
 export default function Icon() {
   return (
     <svg viewBox="0 0 128 128" width={128} height={128}>
@@ -14,9 +5,18 @@ export default function Icon() {
         <div className="flex h-full flex-col items-start justify-center gap-3 p-5">
           <div className="h-1.5 w-12 bg-black/70 dark:bg-white/70" />
           <div className="flex flex-col items-start gap-1">
-            {getItem("w-10")}
-            {getItem("w-8")}
-            {getItem("w-14")}
+            <div className="flex items-center gap-2">
+              <div className="h-2.5 w-2.5 rounded-full border-2 border-blue-600 bg-white dark:bg-black" />
+              <div className="h-1 w-10 bg-black/70 dark:bg-white/70" />
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="h-2.5 w-2.5 rounded-full border-2 border-blue-600 bg-white dark:bg-black" />
+              <div className="h-1 w-8 bg-black/70 dark:bg-white/70" />
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="h-2.5 w-2.5 rounded-full border-2 border-blue-600 bg-white dark:bg-black" />
+              <div className="h-1 w-14 bg-black/70 dark:bg-white/70" />
+            </div>
           </div>
           <div className="h-4 w-10 rounded-sm bg-blue-600" />
         </div>
