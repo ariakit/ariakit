a1b36a0e25b0679be7656e6760e83a3d38633df0
Haz <hazdiego@gmail.com>
2023-06-18 16:33:16 +0200

    Add retry block

--- examples/toolbar/test-browser-js-disabled.ts
+++ examples/toolbar/test-browser-js-disabled.ts
@@ -8,8 +8,10 @@ test.use({ javaScriptEnabled: false });
 
 test("make sure elements are tabbable with JS disabled", async ({ page }) => {
   await page.goto("/previews/toolbar", { waitUntil: "networkidle" });
-  await page.keyboard.press("Tab");
-  await expect(getButton(page, "Undo")).toBeFocused();
+  await expect(async () => {
+    await page.keyboard.press("Tab");
+    await expect(getButton(page, "Undo")).toBeFocused();
+  }).toPass();
   await page.keyboard.press("Tab");
   await expect(getButton(page, "Bold")).toBeFocused();
 });
