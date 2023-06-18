96475727c21030acf59f6fec69203367f210584b
Haz <hazdiego@gmail.com>
2023-06-18 16:42:35 +0200

    Remove wrong macos test

--- examples/toolbar/test-chrome-js-disabled.ts
+++ examples/toolbar/test-chrome-js-disabled.ts
@@ -8,10 +8,8 @@ test.use({ javaScriptEnabled: false });
 
 test("make sure elements are tabbable with JS disabled", async ({ page }) => {
   await page.goto("/previews/toolbar", { waitUntil: "networkidle" });
-  await expect(async () => {
-    await page.keyboard.press("Tab");
-    await expect(getButton(page, "Undo")).toBeFocused();
-  }).toPass();
+  await page.keyboard.press("Tab");
+  await expect(getButton(page, "Undo")).toBeFocused();
   await page.keyboard.press("Tab");
   await expect(getButton(page, "Bold")).toBeFocused();
 });
