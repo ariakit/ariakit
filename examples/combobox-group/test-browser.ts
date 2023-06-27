ee2c0612460766d6a2829e71d29173da5371a5fe
Haz <hazdiego@gmail.com>
2023-06-27 21:18:46 +0200

    Do not scroll composite item into view on hover

--- examples/combobox-group/test-browser.ts
+++ examples/combobox-group/test-browser.ts
@@ -2,6 +2,7 @@ import type { Page } from "@playwright/test";
 import { expect, test } from "@playwright/test";
 
 const getCombobox = (page: Page) => page.getByRole("combobox");
+const getPopover = (page: Page) => page.getByRole("listbox");
 
 function getSelectionValue(page: Page) {
   return getCombobox(page).evaluate((element) => {
@@ -27,4 +28,13 @@ test("maintain completion string while typing", async ({ page }) => {
   await page.keyboard.type("ca");
   await expect(getCombobox(page)).toHaveValue("avocado");
   expect(await getSelectionValue(page)).toBe("do");
+});
+
+test("do not scroll when hovering over an item", async ({ page }) => {
+  await getCombobox(page).click();
+  await getPopover(page).click({ position: { x: 2, y: 2 } });
+  await getPopover(page).evaluate((el) => (el.scrollTop = 100));
+  expect(await getPopover(page).evaluate((el) => el.scrollTop)).toBe(100);
+  await getPopover(page).hover({ position: { x: 40, y: 10 } });
+  expect(await getPopover(page).evaluate((el) => el.scrollTop)).toBe(100);
 });
