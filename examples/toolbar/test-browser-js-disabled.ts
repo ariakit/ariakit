20ee8f5f1f5b8ed72d148415f52df34a1ddc5e91
Haz <hazdiego@gmail.com>
2023-06-29 03:41:57 +0200

    Try timeout

--- examples/toolbar/test-browser-js-disabled.ts
+++ examples/toolbar/test-browser-js-disabled.ts
@@ -8,6 +8,7 @@ test.use({ javaScriptEnabled: false });
 
 test("make sure elements are tabbable with JS disabled", async ({ page }) => {
   await page.goto("/previews/toolbar", { waitUntil: "networkidle" });
+  await page.waitForTimeout(100);
   await page.keyboard.press("Tab");
   await expect(getButton(page, "Undo")).toBeFocused();
   await page.keyboard.press("Tab");
