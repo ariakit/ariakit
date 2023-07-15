64507047e67c7f4edb203a0e2f05eb46693faaeb
Haz <hazdiego@gmail.com>
2023-07-15 03:30:36 +0200

    cx -> twJoin

--- website/app/(main)/updates/page.tsx
+++ website/app/(main)/updates/page.tsx
@@ -1,5 +1,5 @@
-import { cx } from "@ariakit/core/utils/misc";
 import { UpdateLink } from "components/update-link.jsx";
+import { twJoin } from "tailwind-merge";
 import { getUpdates } from "utils/get-updates.js";
 import { PageContainer } from "../page-container.jsx";
 import { SeeNow } from "./see-now.jsx";
@@ -10,7 +10,7 @@ export default function Page() {
     <div className="flex items-start justify-center md:flex-row-reverse">
       <SeeNow />
       <main
-        className={cx(
+        className={twJoin(
           "relative mt-8 flex w-full min-w-[1px] flex-col items-center gap-8 px-3 sm:mt-12 sm:px-4 lg:px-8 [&>*]:w-full",
           "max-w-7xl [&>*]:max-w-[1040px]"
         )}
