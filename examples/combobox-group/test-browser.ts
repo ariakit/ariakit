0633e9d7ff6b40f15f6476653802baefc0392057
Haz <hazdiego@gmail.com>
2023-07-25 20:54:03 +0200

    Flush updates synchronously on controlled component stores

--- examples/combobox-group/test-browser.ts
+++ examples/combobox-group/test-browser.ts
@@ -109,3 +109,11 @@ test("set value on click outside", async ({ page }) => {
   await page.mouse.up();
   await expect(getPopover(page)).not.toBeVisible();
 });
+
+test("keep caret position when typing", async ({ page }) => {
+  await getCombobox(page).click();
+  await page.keyboard.type("ap");
+  await page.keyboard.press("ArrowLeft");
+  await page.keyboard.type("pp");
+  await expect(getCombobox(page)).toHaveValue("apppple");
+});
