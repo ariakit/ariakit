dc732fec2bb0fde68e91b22bcac6af033bade95d
Haz <hazdiego@gmail.com>
2023-07-31 03:21:02 +0200

    Update example icons

--- examples/checkbox-custom/site-icon.tsx
+++ examples/checkbox-custom/site-icon.tsx
@@ -2,9 +2,9 @@ export default function Icon() {
   return (
     <svg viewBox="0 0 128 128" width={128} height={128}>
       <foreignObject width={128} height={128}>
-        <div className="flex h-full items-center justify-center p-4">
-          <div className="flex flex-col gap-2 rounded-lg border-[3px] border-blue-600 bg-white p-2 dark:bg-gray-700">
-            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-600 p-2">
+        <div className="h-full overflow-hidden">
+          <div className="ml-6 mt-6 flex w-40 flex-col gap-4 rounded-xl border-4 border-blue-600 bg-white p-4 pb-6 dark:bg-gray-700">
+            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-blue-600 p-2">
               <svg
                 fill="none"
                 viewBox="0 0 24 24"
@@ -14,7 +14,7 @@ export default function Icon() {
                 <path d="M4.5 12.75l6 6 9-13.5" />
               </svg>
             </div>
-            <div className="h-2 w-14 bg-black/50 dark:bg-white/50" />
+            <div className="h-2 w-16 bg-black/50 dark:bg-white/50" />
           </div>
         </div>
       </foreignObject>
