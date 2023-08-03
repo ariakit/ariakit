5ee5dbad8a89da46c848c36d7a95d8b2eb6b5906
Haz <hazdiego@gmail.com>
2023-08-03 05:39:11 +0200

    Fix nested dialog with custom parent backdrop (#2688)
    Closes #2687

--- packages/ariakit-react-core/src/dialog/utils/mark-tree-outside.ts
+++ packages/ariakit-react-core/src/dialog/utils/mark-tree-outside.ts
@@ -13,6 +13,9 @@ function getPropertyName(id = "", ancestor = false) {
 
 export function markElement(element: Element, id = "") {
   return chain(
+    // Debug
+    // setAttribute(element, getPropertyName(), "true"),
+    // setAttribute(element, getPropertyName(id), "true"),
     setProperty(element, getPropertyName(), true),
     setProperty(element, getPropertyName(id), true),
   );
@@ -20,6 +23,9 @@ export function markElement(element: Element, id = "") {
 
 export function markAncestor(element: Element, id = "") {
   return chain(
+    // Debug
+    // setAttribute(element, getPropertyName("", true), "true"),
+    // setAttribute(element, getPropertyName(id, true), "true"),
     setProperty(element, getPropertyName("", true), true),
     setProperty(element, getPropertyName(id, true), true),
   );
@@ -46,7 +52,13 @@ export function markTreeOutside(dialogId: string, ...elements: Elements) {
       if (isBackdrop(element, ...ids)) return;
       cleanups.unshift(markElement(element, dialogId));
     },
-    (ancestor) => cleanups.unshift(markAncestor(ancestor, dialogId)),
+    (ancestor, element) => {
+      // See https://github.com/ariakit/ariakit/issues/2687
+      const isAnotherDialogAncestor =
+        element.hasAttribute("data-dialog") && element.id !== dialogId;
+      if (isAnotherDialogAncestor) return;
+      cleanups.unshift(markAncestor(ancestor, dialogId));
+    },
   );
 
   const restoreAccessibilityTree = () => {
