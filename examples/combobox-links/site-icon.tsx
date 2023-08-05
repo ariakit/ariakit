701fb815f97fb4ddc63c078a083aa46e884440a7
Haz <hazdiego@gmail.com>
2023-08-05 12:37:44 +0200

    Fix combobox-links site icon on Safari

--- examples/combobox-links/site-icon.tsx
+++ examples/combobox-links/site-icon.tsx
@@ -1,8 +1,8 @@
 export default function Icon() {
   return (
     <svg viewBox="0 0 128 128" width={128} height={128}>
-      <foreignObject width={128} height={128}>
-        <div className="-ml-14 mt-6 flex w-40 flex-col gap-2">
+      <foreignObject width={128} height={104} y={24}>
+        <div className="-ml-14 flex w-40 flex-col gap-2">
           <div className="h-8 w-full rounded-md border border-black/30 bg-white dark:border-white/30 dark:bg-black" />
           <div className="flex h-20 w-full flex-col gap-2 rounded-md border border-black/20 bg-white p-2 shadow dark:border-white/10 dark:bg-white/10 dark:shadow-dark">
             <div className="flex w-full flex-row-reverse rounded bg-blue-200/60 p-2 py-2 dark:bg-blue-600/60">
