01b1383bd68896f9872a4470e0d946edaf94a73f
Haz <hazdiego@gmail.com>
2023-10-10 17:26:12 +0200

    Update dialog-next-router icon

--- website/app/(examples)/previews/dialog-next-router/site-icon.tsx
+++ website/app/(examples)/previews/dialog-next-router/site-icon.tsx
@@ -10,7 +10,7 @@ export default function Icon() {
       </foreignObject>
       <foreignObject width={128} height={128}>
         <div className="flex h-full flex-col items-center overflow-hidden bg-gray-150/40 p-4 px-2 dark:bg-gray-850/30">
-          <div className="-ml-10 -mt-10 flex w-full flex-col gap-2 rounded-lg border border-black/20 bg-white p-2 shadow dark:border-white/10 dark:bg-gray-650 dark:shadow-dark">
+          <div className="-ml-12 -mt-12 flex w-full flex-col gap-2 rounded-lg border border-black/20 bg-white p-2 shadow dark:border-white/10 dark:bg-gray-650 dark:shadow-dark">
             <div className="h-3 w-14 rounded-sm bg-black/60 dark:bg-white/60" />
             <div className="flex flex-col gap-2 pb-2 pt-2">
               <div className="h-2 w-full rounded-sm bg-black/40 dark:bg-white/40" />
