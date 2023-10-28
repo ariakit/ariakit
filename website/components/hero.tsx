6bbe912b7ef70dbbf023851a3ec1ecab6d815ef2
Haz <hazdiego@gmail.com>
2023-10-29 01:46:28 +0200

    Update hero

--- website/components/hero.tsx
+++ website/components/hero.tsx
@@ -1,17 +1,25 @@
 import { ArrowRight } from "icons/arrow-right.js";
 import Link from "next/link.js";
+import { InlineLink } from "./inline-link.jsx";
 
 export function Hero() {
   return (
     <div className="flex flex-col items-center gap-8 p-5 px-3 sm:px-4 md:px-8">
-      <div className="mt-4 flex w-full max-w-5xl flex-col items-center gap-8 sm:mt-10 md:mt-20">
+      <div className="mt-4 flex w-full max-w-4xl flex-col items-center gap-8 sm:mt-10 md:mt-20">
         <h1 className="bg-gradient-to-br from-[hsl(175_100%_35%)] to-[hsl(204_100%_35%)] bg-clip-text text-center text-5xl font-extrabold tracking-[-0.035em] text-transparent dark:from-[hsl(175_100%_60%)] dark:to-[hsl(204_100%_58%)] dark:font-bold dark:tracking-[-0.015em] sm:text-6xl md:text-7xl">
           Build accessible web apps with React
         </h1>
-        <p className="max-w-3xl text-center text-lg text-black/70 dark:text-white/70">
-          Ariakit is an open source library that provides lower-level React
-          components and hooks for building accessible web apps, design systems,
-          and component libraries.
+        <p className="max-w-[720px] text-center text-lg text-black/70 dark:text-white/70">
+          Ariakit is an open-source library that provides{" "}
+          <InlineLink render={<Link href="/guide/styling" />}>
+            unstyled
+          </InlineLink>
+          ,{" "}
+          <InlineLink render={<Link href="/guide/composition" />}>
+            primitive
+          </InlineLink>{" "}
+          components for building accessible web apps, design systems, and
+          component libraries.
         </p>
       </div>
       <div className="mb-20 flex flex-col items-center gap-4 sm:flex-row">
--- website/components/inline-link.tsx
+++ website/components/inline-link.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { forwardRef } from "react";
 import { Role } from "@ariakit/react";
 import type { RoleProps } from "@ariakit/react";
