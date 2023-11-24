a4ae2624c47035aebb9cd523da70c6a162341972
Renato <renatoribroman@gmail.com>
2023-11-24 14:35:56 -0300

    update composite test check data-active-item

--- examples/composite/test.ts
+++ examples/composite/test.ts
@@ -2,19 +2,34 @@ import { press, q } from "@ariakit/test";
 
 test("navigate through items with keyboard", async () => {
   expect(q.button("🍎 Apple")).not.toHaveFocus();
+  expect(q.button("🍎 Apple")).toHaveAttribute("data-active-item");
+
   await press.Tab();
   expect(q.button("🍎 Apple")).toHaveFocus();
+  expect(q.button("🍎 Apple")).toHaveAttribute("data-active-item");
+
   await press.ArrowDown();
   expect(q.button("🍇 Grape")).toHaveFocus();
+  expect(q.button("🍇 Grape")).toHaveAttribute("data-active-item");
+
   await press.ArrowDown();
   expect(q.button("🍊 Orange")).toHaveFocus();
+  expect(q.button("🍊 Orange")).toHaveAttribute("data-active-item");
+
   await press.ArrowUp();
   expect(q.button("🍇 Grape")).toHaveFocus();
+  expect(q.button("🍇 Grape")).toHaveAttribute("data-active-item");
+
   await press.ArrowRight();
   expect(q.button("🍊 Orange")).toHaveFocus();
+  expect(q.button("🍊 Orange")).toHaveAttribute("data-active-item");
+
   await press.ArrowLeft();
   expect(q.button("🍇 Grape")).toHaveFocus();
+  expect(q.button("🍇 Grape")).toHaveAttribute("data-active-item");
+
   await press.ArrowDown();
-  await press.ArrowDown(); // do not loop
+  await press.ArrowDown(); // should not loop
   expect(q.button("🍊 Orange")).toHaveFocus();
+  expect(q.button("🍊 Orange")).toHaveAttribute("data-active-item");
 });
