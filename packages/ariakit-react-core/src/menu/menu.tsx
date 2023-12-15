5f1d750774b30eb942d28d2ab0f20c21a8afa1c2
Haz <hazdiego@gmail.com>
2023-12-15 07:41:00 +0100

    Fix `preserveTabOrderAnchor` on nested menus (#3208)

--- packages/ariakit-react-core/src/menu/menu.tsx
+++ packages/ariakit-react-core/src/menu/menu.tsx
@@ -1,5 +1,5 @@
 import type { MutableRefObject, RefObject } from "react";
-import { createRef, useEffect, useRef, useState } from "react";
+import { createRef, useEffect, useMemo, useRef, useState } from "react";
 import { fireEvent } from "@ariakit/core/utils/events";
 import { hasFocusWithin } from "@ariakit/core/utils/focus";
 import { invariant, isFalsyBooleanCallback } from "@ariakit/core/utils/misc";
@@ -7,6 +7,7 @@ import { createDialogComponent } from "../dialog/dialog.js";
 import type { HovercardOptions } from "../hovercard/hovercard.jsx";
 import { useHovercard } from "../hovercard/hovercard.jsx";
 import { useMergeRefs } from "../utils/hooks.js";
+import { useStoreState } from "../utils/store.js";
 import {
   createComponent,
   createElement,
@@ -127,6 +128,40 @@ export const useMenu = createHook<MenuOptions>(
     // focus on the dialog container when no initialFocusRef is set.
     const canAutoFocusOnShow =
       !!initialFocusRef || !!props.initialFocus || !!modal;
+
+    const contentElement = useStoreState(
+      store.combobox || store,
+      "contentElement",
+    );
+    const parentContentElement = useStoreState(
+      parentMenu?.combobox || parentMenu,
+      "contentElement",
+    );
+
+    // This ensures that, when the menu is nested in a non-menu wrapper, the tab
+    // order will be preserved from the parent content element rather than from
+    // the anchor element. This is because the preserveTabOrderAnchor feature
+    // will use aria-owns to fix the tab order on screen readers. The parent
+    // content element may have a role that does not allow this menu's role as a
+    // child. By passing the parent content element as the tab order anchor, the
+    // aria-owns element will be added as a sibling instead. TODO: Test this.
+    const preserveTabOrderAnchor = useMemo(() => {
+      if (!parentContentElement) return;
+      if (!contentElement) return;
+      const role = contentElement.getAttribute("role");
+      const parentRole = parentContentElement.getAttribute("role");
+      const parentIsMenuOrMenubar =
+        parentRole === "menu" || parentRole === "menubar";
+      if (parentIsMenuOrMenubar && role === "menu") return;
+      return parentContentElement;
+    }, [contentElement, parentContentElement]);
+
+    if (preserveTabOrderAnchor !== undefined) {
+      props = {
+        preserveTabOrderAnchor,
+        ...props,
+      };
+    }
 
     props = useHovercard({
       store,
