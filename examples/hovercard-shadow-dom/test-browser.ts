93e9572032d101df20ad9d93ef47c8db10a07649
George Karan <georgekaran12@gmail.com>
2023-12-24 13:54:37 -0300

    Fix Hovercard when using shadow DOM (#3258)
    Closes #2983

---
+++ examples/hovercard-shadow-dom/test-browser.ts
@@ -0,0 +1,28 @@
+import type { Page } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+
+const getAnchor = (page: Page) =>
+  page.getByRole("link", { name: "@ariakitjs" });
+const getHovercard = (page: Page) =>
+  page.getByRole("dialog", { name: "Ariakit" });
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/previews/hovercard-shadow-dom", {
+    waitUntil: "networkidle",
+  });
+});
+
+test("https://github.com/ariakit/ariakit/issues/2983", async ({ page }) => {
+  await getAnchor(page).hover();
+  await expect(getHovercard(page)).toBeVisible();
+  await expect(page.getByText("Opened 1 times")).toBeVisible();
+  await getAnchor(page).hover({ position: { y: 10, x: 10 } });
+  await expect(page.getByText("Opened 1 times")).toBeVisible();
+  await getAnchor(page).hover({ position: { y: 10, x: 20 } });
+  await expect(page.getByText("Opened 1 times")).toBeVisible();
+  await page.mouse.move(0, 0);
+  await expect(getHovercard(page)).not.toBeVisible();
+  await getAnchor(page).hover();
+  await expect(getHovercard(page)).toBeVisible();
+  await expect(page.getByText("Opened 2 times")).toBeVisible();
+});
