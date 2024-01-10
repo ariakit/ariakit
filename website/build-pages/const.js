5b83fd90e22c0f3f639ac0425ab6edf266da09a8
Haz <hazdiego@gmail.com>
2024-01-10 06:05:51 +0100

    Update PAGE_FILE_REGEX

--- website/build-pages/const.js
+++ website/build-pages/const.js
@@ -1,6 +1,6 @@
 export const PAGE_INDEX_FILE_REGEX =
   /\/[^\/]+\/((index|page|layout)\.[tj]sx?|readme\.md)/i;
 export const PAGE_FILE_REGEX = new RegExp(
-  `(${PAGE_INDEX_FILE_REGEX.source}|\.md)$`,
+  `(${PAGE_INDEX_FILE_REGEX.source}|\/((?!.*license\.md$).*\.md))$`,
   "i",
 );
