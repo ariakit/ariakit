5b46cb35142e4779bd7057a3be1082c27fa728b6
Haz <hazdiego@gmail.com>
2024-01-26 02:43:49 +0100

    Refactor site icon component

--- examples/dialog-combobox-command-menu/site-icon.tsx
+++ examples/dialog-combobox-command-menu/site-icon.tsx
@@ -3,29 +3,38 @@ export default function Icon() {
     <svg viewBox="0 0 128 128" width={128} height={128}>
       <foreignObject width={128} height={128}>
         <div className="h-full overflow-hidden">
-          <div className="-mx-1 -mt-5">
-            <div className="flex w-full flex-col gap-2 rounded-md border border-black/20 bg-white p-2 shadow-md dark:border-white/10 dark:bg-white/10 dark:shadow-md-dark">
-              <div className="ml-2 h-2 w-10 rounded-sm bg-black/70 dark:bg-white/70" />
-              <div className="h-10 w-full rounded border border-black/30 bg-white px-2 py-1 dark:border-white/30 dark:bg-black">
+          <div className="mx-2 mt-7">
+            <div className="flex w-full flex-col overflow-hidden rounded-lg border border-black/40 bg-white shadow-md dark:border-white/20 dark:bg-white/10 dark:shadow-md-dark">
+              <div className="flex w-full items-center justify-between border-b border-inherit bg-white p-2 dark:bg-black">
                 <div className="flex items-center gap-1">
                   <div className="h-2 w-6 rounded-sm bg-black/60 dark:bg-white/60" />
                   <div className="h-4 w-px bg-black dark:bg-white" />
                 </div>
+                <svg
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1pt"
+                  viewBox="0 0 16 16"
+                  className="size-4 fill-none stroke-black/60 dark:stroke-white/60"
+                >
+                  <line x1="4" y1="4" x2="12" y2="12" />
+                  <line x1="4" y1="12" x2="12" y2="4" />
+                </svg>
               </div>
-              <div className="h-4 w-full rounded bg-blue-600" />
-            </div>
-          </div>
-        </div>
-      </foreignObject>
-      <foreignObject width={128} height={128}>
-        <div className="h-full overflow-hidden">
-          <div className="h-full w-full bg-gray-150/70 p-5 pt-14 dark:bg-gray-850/70">
-            <div className="flex w-full flex-col gap-3 rounded-md border border-black/20 bg-white p-2 shadow-md dark:border-white/10 dark:bg-gray-600 dark:shadow-md-dark">
-              <div className="flex flex-col gap-1.5">
-                <div className="h-1.5 w-full rounded-sm bg-black/40 dark:bg-white/40" />
-                <div className="h-1.5 w-10 rounded-sm bg-black/40 dark:bg-white/40" />
+              <div className="flex flex-col gap-1 p-1">
+                <div className="flex h-4 w-full items-center rounded p-1">
+                  <div className="h-2 w-3/4 rounded-sm bg-blue-600 dark:bg-blue-500" />
+                </div>
+                <div className="flex h-4 w-full items-center rounded p-1">
+                  <div className="h-2 w-1/2 rounded-sm bg-black/60 dark:bg-white/60" />
+                </div>
+                <div className="flex h-4 w-full items-center rounded p-1">
+                  <div className="h-2 w-2/3 rounded-sm bg-black/60 dark:bg-white/60" />
+                </div>
+                <div className="flex h-4 w-full items-center rounded p-1">
+                  <div className="h-2 w-1/3 rounded-sm bg-black/60 dark:bg-white/60" />
+                </div>
               </div>
-              <div className="h-3 w-full rounded-sm bg-blue-600 dark:bg-blue-500" />
             </div>
           </div>
         </div>
