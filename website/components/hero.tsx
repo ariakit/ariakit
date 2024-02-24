b2aebae714aca0fd6b1e38b87600dad79e1f007e
Haz <hazdiego@gmail.com>
2024-02-24 05:09:15 +0100

    Update hero copy

--- website/components/hero.tsx
+++ website/components/hero.tsx
@@ -1,31 +1,41 @@
 import { ArrowRight } from "icons/arrow-right.js";
 import Link from "next/link.js";
-import { InlineLink } from "./inline-link.jsx";
+import { twJoin } from "tailwind-merge";
 
 export function Hero() {
+  const linkClassName = twJoin(
+    "relative inline-block",
+    "before:transition-none before:animate-in before:zoom-in-0 before:duration-700 before:origin-left",
+    "before:absolute before:top-full before:inset-x-0 before:[translate:0_-0.1em] before:h-[0.125em] before:rounded-sm",
+    "before:bg-gradient-to-r before:from-blue-500 before:to-pink-600 before:dark:from-blue-400 before:dark:to-pink-500",
+    "hover:before:h-[0.250em]",
+  );
   return (
-    <div className="flex flex-col items-center gap-8 p-5 px-3 sm:px-4 md:px-8">
-      <div className="mt-4 flex w-full max-w-4xl flex-col items-center gap-8 sm:mt-10 md:mt-20">
-        <h1 className="bg-gradient-to-br from-[hsl(175_100%_35%)] to-[hsl(204_100%_35%)] bg-clip-text text-center text-5xl font-extrabold tracking-[-0.035em] text-transparent dark:from-[hsl(175_100%_60%)] dark:to-[hsl(204_100%_58%)] dark:font-bold dark:tracking-[-0.015em] sm:text-6xl md:text-7xl">
+    <div className="flex flex-col items-center gap-10 p-5 px-3 sm:px-4 md:px-8">
+      <div className="mt-4 flex w-full max-w-4xl flex-col items-center gap-[inherit] sm:mt-10 md:mt-20">
+        <h1 className="text-pretty bg-gradient-to-br from-[hsl(175,100%,35%)] to-[hsl(204,100%,35%)] bg-clip-text text-center text-5xl font-extrabold tracking-[-0.035em] text-transparent dark:from-[hsl(175,100%,60%)] dark:to-[hsl(204,100%,58%)] dark:font-bold dark:tracking-[-0.015em] sm:text-6xl md:text-7xl">
           Build accessible web apps with React
         </h1>
-        <p className="max-w-[720px] text-center text-lg text-black/70 dark:text-white/70">
-          Ariakit is an open-source library that provides{" "}
-          <InlineLink render={<Link href="/guide/styling" />}>
+        <p className="max-w-[720px] text-pretty text-center text-lg text-black/80 dark:text-white/80 sm:text-xl sm:leading-8 md:text-2xl md:leading-9">
+          Open-source library with{" "}
+          <Link href="/guide/styling" className={linkClassName}>
             unstyled
-          </InlineLink>
+          </Link>
           ,{" "}
-          <InlineLink render={<Link href="/guide/composition" />}>
+          <Link href="/guide/composition" className={linkClassName}>
             primitive
-          </InlineLink>{" "}
-          components for building accessible web apps, design systems, and
-          component libraries.
+          </Link>{" "}
+          components, along with a collection of{" "}
+          <Link href="/examples" className={linkClassName}>
+            styled examples
+          </Link>{" "}
+          that you can copy and paste into your apps.
         </p>
       </div>
       <div className="mb-20 flex flex-col items-center gap-4 sm:flex-row">
         <Link
           href="/guide/getting-started"
-          className="flex h-12 items-center justify-center gap-2 whitespace-nowrap rounded-lg bg-blue-600 px-8 text-lg text-white shadow-xl hover:bg-blue-800 focus-visible:ariakit-outline"
+          className="flex h-12 items-center justify-center gap-2 whitespace-nowrap rounded-lg bg-blue-600 px-8 text-lg font-medium text-white shadow-xl hover:bg-blue-800 focus-visible:ariakit-outline"
         >
           Get started
         </Link>
@@ -35,8 +45,8 @@ export function Hero() {
         >
           Explore components{" "}
           <ArrowRight
-            strokeWidth={3}
-            className="h-4 w-4 transition-transform group-hover:translate-x-1"
+            strokeWidth={2}
+            className="size-4 transition-transform group-hover:translate-x-1"
           />
         </Link>
       </div>
