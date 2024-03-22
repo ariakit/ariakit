e78a1ef0d2fbd9f77cfe55c6499480341e4a51db
Haz <hazdiego@gmail.com>
2024-03-22 04:12:47 +0100

    Add TagRemove icon

--- packages/ariakit-react-core/src/tag/tag-remove.tsx
+++ packages/ariakit-react-core/src/tag/tag-remove.tsx
@@ -13,15 +13,6 @@ const TagName = "button" satisfies ElementType;
 type TagName = typeof TagName;
 type HTMLType = HTMLElementTagNameMap[TagName];
 
-const icon = (
-  <span
-    aria-hidden
-    style={{ fontFamily: "system-ui, sans-serif", userSelect: "none" }}
-  >
-    ⌫
-  </span>
-);
-
 /**
  * Returns props to create a `TagRemove` component.
  * @see https://ariakit.org/components/tag
@@ -65,8 +56,26 @@ export const useTagRemove = createHook<TagName, TagRemoveOptions>(
       inputElement?.focus();
     });
 
+    const children = (
+      <svg
+        display="block"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        height="1em"
+        width="1em"
+        aria-hidden
+      >
+        <path d="M7.28782 5.46243C7.80963 4.92308 8.07054 4.65341 8.37751 4.46038C8.64963 4.28926 8.94749 4.16299 9.25969 4.0864C9.61186 4 9.98709 4 10.7375 4H16.8C17.9201 4 18.4802 4 18.908 4.21799C19.2843 4.40973 19.5903 4.71569 19.782 5.09202C20 5.51984 20 6.0799 20 7.2V16.8C20 17.9201 20 18.4802 19.782 18.908C19.5903 19.2843 19.2843 19.5903 18.908 19.782C18.4802 20 17.9201 20 16.8 20H10.7512C9.99584 20 9.61818 20 9.264 19.9126C8.95004 19.8351 8.65067 19.7073 8.37748 19.5342C8.06931 19.339 7.80802 19.0663 7.28544 18.521L2.59879 13.63C2.03714 13.0439 1.75631 12.7508 1.65115 12.4158C1.55859 12.121 1.55935 11.8048 1.65332 11.5104C1.7601 11.1759 2.04233 10.8842 2.60679 10.3008L7.28782 5.46243Z" />
+        <path d="M10.052 9L16 15M15.948 9L10 15" />
+      </svg>
+    );
+
     props = {
-      children: icon,
+      children,
       ...props,
       render: withinTag ? <Role.span render={props.render} /> : props.render,
       onClick,
