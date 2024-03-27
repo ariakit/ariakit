08da55f21dea73ea77096858fe48cc3a0753e488
Haz <hazdiego@gmail.com>
2024-03-27 18:23:05 +0100

    Add more tests

--- examples/tag-combobox/test.ts
+++ examples/tag-combobox/test.ts
@@ -21,6 +21,46 @@ function tags() {
     .map((el) => el.textContent);
 }
 
+test("initial state", async () => {
+  expect(tags()).toEqual(["Abigail Patterson"]);
+});
+
+test("reset the value on selecting a tag from the combobox suggestions", async () => {
+  await press.Tab();
+  expect(q.combobox("Invitees")).toHaveFocus();
+  await type("bella");
+  expect(q.combobox()).toHaveValue("bellawebb86@email.com");
+  await press.Enter();
+  expect(tags()).toEqual(["Abigail Patterson", "Bella Webb"]);
+  expect(q.combobox()).toHaveValue("");
+});
+
+test("the value should not reset when removing a tag from the tag list", async () => {
+  await press.Tab();
+  expect(q.combobox("Invitees")).toHaveFocus();
+  await type("a");
+  expect(q.combobox()).toHaveValue("a");
+  await press.ArrowLeft();
+  await press.Backspace();
+  expect(tags()).toEqual([]);
+  expect(q.combobox()).toHaveValue("a");
+});
+
+test("suggestions reset after selecting a tag", async () => {
+  await press.Tab();
+  expect(q.combobox("Invitees")).toHaveFocus();
+  await type("dan");
+  await press.Enter();
+  expect(tags()).toEqual(["Abigail Patterson", "Daniel Green"]);
+  await press.ArrowDown();
+  await press.ArrowDown();
+  expect(q.option(/Abigail Patterson /)).toHaveAttribute("data-active-item");
+  expect(q.option(/Abigail Patterson /)).toHaveAttribute(
+    "aria-selected",
+    "true",
+  );
+});
+
 test("deselecting a tag should not highlight the input text if it is not the first combobox item", async () => {
   await press.Tab();
   expect(q.combobox("Invitees")).toHaveFocus();
