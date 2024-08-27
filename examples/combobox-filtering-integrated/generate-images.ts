60dc438ae70774a3d27d3d582616c313d9060084
Ben Rodri <95430804+benrodrs@users.noreply.github.com>
2024-08-27 17:37:14 +0200

    Generate images for combobox examples (#4078)

---
+++ examples/combobox-filtering-integrated/generate-images.ts
@@ -0,0 +1,39 @@
+import { query } from "@ariakit/test/playwright";
+import { test } from "@playwright/test";
+import { screenshot } from "../screenshot.ts";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/previews/combobox-filtering-integrated", {
+    waitUntil: "networkidle",
+  });
+});
+
+test("generate images", async ({ page }) => {
+  const q = query(page);
+  await q.combobox().click();
+  await q.combobox().fill("a");
+
+  await screenshot({
+    page,
+    name: "small",
+    elements: [q.combobox()],
+    padding: 24,
+    width: 160,
+    height: "auto",
+  });
+
+  await screenshot({
+    page,
+    name: "medium",
+    elements: [q.combobox()],
+    padding: 24,
+    paddingTop: 44,
+    height: "auto",
+  });
+
+  await screenshot({
+    page,
+    name: "large",
+    elements: [q.combobox(), q.listbox()],
+  });
+});
