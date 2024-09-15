d0359644d7c423e247c5f72c3088dca49de0694a
Dani Guardiola <hi@daniguardio.la>
2024-09-15 07:10:10 +0200

    The order was not correct!

--- packages/ariakit-solid-core/src/visually-hidden/visually-hidden.tsx
+++ packages/ariakit-solid-core/src/visually-hidden/visually-hidden.tsx
@@ -21,20 +21,22 @@ type TagName = typeof TagName;
  */
 export const useVisuallyHidden = createHook<TagName, VisuallyHiddenOptions>(
   function useVisuallyHidden(props) {
-    // TODO: is the order correct?
-    props = combineProps(props, {
-      style: {
-        border: 0,
-        clip: "rect(0 0 0 0)",
-        height: "1px",
-        margin: "-1px",
-        overflow: "hidden",
-        padding: 0,
-        position: "absolute",
-        whiteSpace: "nowrap",
-        width: "1px",
+    props = combineProps(
+      {
+        style: {
+          border: 0,
+          clip: "rect(0 0 0 0)",
+          height: "1px",
+          margin: "-1px",
+          overflow: "hidden",
+          padding: 0,
+          position: "absolute",
+          whiteSpace: "nowrap",
+          width: "1px",
+        },
       },
-    });
+      props,
+    );
     return props;
   },
 );
