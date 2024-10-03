efb52da190ae0751badc246b3e19173af0f2ed87
Haz <hazdiego@gmail.com>
2024-10-03 06:45:45 +0200

    Fix getScrollingElement to accept `overflow: clip`

--- packages/ariakit-core/src/utils/dom.ts
+++ packages/ariakit-core/src/utils/dom.ts
@@ -255,14 +255,17 @@ export function getScrollingElement(
   element?: Element | null,
 ): HTMLElement | Element | null {
   if (!element) return null;
+  const isScrollableOverflow = (overflow: string) => {
+    if (overflow === "auto") return true;
+    if (overflow === "scroll") return true;
+    return false;
+  };
   if (element.clientHeight && element.scrollHeight > element.clientHeight) {
     const { overflowY } = getComputedStyle(element);
-    const isScrollable = overflowY !== "visible" && overflowY !== "hidden";
-    if (isScrollable) return element;
+    if (isScrollableOverflow(overflowY)) return element;
   } else if (element.clientWidth && element.scrollWidth > element.clientWidth) {
     const { overflowX } = getComputedStyle(element);
-    const isScrollable = overflowX !== "visible" && overflowX !== "hidden";
-    if (isScrollable) return element;
+    if (isScrollableOverflow(overflowX)) return element;
   }
   return (
     getScrollingElement(element.parentElement) ||
