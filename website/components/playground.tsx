e2e4e9d763ed5fc0c81795fcf847e43601b0e5b6
Haz <hazdiego@gmail.com>
2024-10-06 09:16:58 +0200

    Update other imports

--- website/components/playground.tsx
+++ website/components/playground.tsx
@@ -1,6 +1,6 @@
 // import type { IVSCodeTheme } from "monaco-vscode-textmate-theme-converter";
 // import { convertTheme } from "monaco-vscode-textmate-theme-converter";
-// import darkPlus from "shiki/themes/dark-plus.json";
+// import darkPlus from "shiki/themes/dark-plus.json" assert { type: "json" };
 import { defer } from "@/lib/defer.ts";
 import { tsToJsFilename } from "@/lib/ts-to-js-filename.ts";
 import { tsToJs } from "@/lib/ts-to-js.ts";
