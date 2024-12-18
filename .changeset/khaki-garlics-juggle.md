7c716b0d84e9e75b37910eb366dcae11dcc930e8
Shrey Dhyani <shreydhyani@Shreys-MacBook-Air.local>
2024-12-18 11:22:46 +0530

    feat: Testing exclude

---
+++ .changeset/khaki-garlics-juggle.md
@@ -0,0 +1,5 @@
+---
+"@sample-test/react-core": minor
+---
+
+Test
--- scripts/build/build.js
+++ scripts/build/build.js
@@ -73,7 +73,7 @@ await Promise.all(
       // dts: true,
       // tsconfig: "tsconfig.build.json",
       splitting: true,
-      external: ["@tanstack/table-core"],
+      external: ["@tanstack/react-table"],
       esbuildOptions(options) {
         options.chunkNames = "__chunks/[hash]";
         if (format === "esm") {
