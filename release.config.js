748b2db49496835c5a48f32bd84f4bbf2078588b
ShreyDhyani <shrey@betalectic.com>
2025-01-08 15:22:44 +0530

    ci: added dev packages for sementic versioning

---
+++ release.config.js
@@ -0,0 +1,18 @@
+const config = {
+  branches: ["main", "next"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/*.js", "dist/*.js.map"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
+
+module.exports = config;
