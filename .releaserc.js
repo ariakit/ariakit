50e9237cf8465617e646e140d86e897e275a4ad7
Rajiv Seelam <rajiv@betalectic.com>
2025-01-08 14:49:59 +0530

    feat: Configure semantic release for monorepo management

---
+++ .releaserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git'
+  ]
+}
