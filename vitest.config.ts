c9e2e48b2d425489a4b8934d04a30296492fef17
Dani Guardiola <hi@daniguardio.la>
2025-02-01 01:57:06 +0100

    set up solid tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { version } from "react";
+import solid from "vite-plugin-solid";
 import { configDefaults, defineConfig } from "vitest/config";
 
 const excludeFromReact17 = [
@@ -20,6 +21,8 @@ const includeWithStyles = [
 const isReact17 = version.startsWith("17");
 
 export default defineConfig({
+  // TODO: instead of this, we should just make it compatible with config
+  plugins: process.env.ARIAKIT_TEST_LOADER === "solid" ? [solid()] : [],
   test: {
     globals: true,
     watch: false,
