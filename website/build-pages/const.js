551569275142d7a5e31e23c6025daf9b26745ad5
Dani Guardiola <hi@daniguardio.la>
2025-02-07 00:12:09 +0100

    make loader suffix required

--- website/build-pages/const.js
+++ website/build-pages/const.js
@@ -1,5 +1,5 @@
 export const PAGE_INDEX_FILE_REGEX =
-  /\/[^\/]+\/((index(\.(react|solid))?|page|layout)\.[tj]sx?|readme\.md)/i;
+  /\/[^\/]+\/((index(\.(react|solid))|page|layout)\.[tj]sx?|readme\.md)/i;
 export const PAGE_FILE_REGEX = new RegExp(
   `(${PAGE_INDEX_FILE_REGEX.source}|\/((?!.*license\.md$).*\.md))$`,
   "i",
