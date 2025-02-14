2bab8d11fbd1aa8719bee2385e1dd85e664f4852
Dani Guardiola <hi@daniguardio.la>
2025-02-14 01:36:07 +0100

    unblocked group example

---
+++ examples/group/index.solid.tsx
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import { Button, Group } from "@ariakit/solid";
+import "./style.css";
+
+export default function Example() {
+  return (
+    <Group class="group">
+      <Button class="button">Bold</Button>
+      <Button class="button">Italic</Button>
+      <Button class="button">Underline</Button>
+    </Group>
+  );
+}
