e75365b57fb6f90e1eaa179d06b23306c3e6f1b7
Wayne Van Son <waynevanson@gmail.com>
2025-02-19 21:07:33 +1100

    chore: add nix configuration

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        # todo: playwright support
+        nativeBuildInputs = with pkgs; [
+          direnv
+          nodejs
+        ];
+      in {
+        devShells.default =
+          pkgs.mkShell {
+            inherit system nativeBuildInputs;
+          };
+      }
+    );
+}
