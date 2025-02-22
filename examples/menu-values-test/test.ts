aa6f4224a1ec41d99769662102fac0609d300fe6
Wayne Van Son <waynevanson@gmail.com>
2025-02-23 10:58:03 +1100

    test: add inline parallel config to menu-values-test

--- examples/menu-values-test/test.ts
+++ examples/menu-values-test/test.ts
@@ -1,7 +1,10 @@
 import { click, press, q, type } from "@ariakit/test";
 
-const spyOnLog = () => vi.spyOn(console, "log").mockImplementation(() => {});
+// sets up the console.log spy before the component renders,
+// so we can see the spy being called as it renders.
+vi.setConfig({ sequence: { hooks: 'parallel' } })
 
+const spyOnLog = () => vi.spyOn(console, "log").mockImplementation(() => {});
 let log = spyOnLog();
 
 beforeEach(() => {
@@ -43,8 +46,14 @@ test("default values", async () => {
     radioParent: "Orange",
     radioUncontrolled: "Banana",
   });
-
-  expect(log.mock.lastCall?.at(0)).toBeUndefined()
+  expect(log.mock.lastCall?.at(0)).toEqual({
+    checkboxControlled: ["Banana"],
+    checkboxParent: ["Orange"],
+    checkboxUncontrolled: ["Banana", "Grape"],
+    radioControlled: "Banana",
+    radioParent: "Orange",
+    radioUncontrolled: "Banana",
+  })
 });
 
 test("interact with checkboxControlled items", async () => {
