9249a30b0b6283a07944af3d97e57666043d45bb
Haz <hazdiego@gmail.com>
2025-02-27 17:57:19 +0100

    Add multi-framework examples to Astro site

--- site/src/examples/separator/index.solid.tsx
+++ site/src/examples/separator/index.solid.tsx
@@ -1,5 +1,5 @@
 import { Separator } from "@ariakit/solid";
 
 export default function Example() {
-  return <Separator orientation="horizontal" class="separator" />;
+  return <Separator orientation="horizontal" class="ak-layer-current" />;
 }
---
+++ site/src/types.ts
@@ -0,0 +1,5 @@
+declare namespace App {
+  interface Locals {
+    framework?: string;
+  }
+}
