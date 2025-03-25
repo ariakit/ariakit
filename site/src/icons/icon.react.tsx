7cf017a8cc0a59b121197de900d3a54e343df3e0
Haz <hazdiego@gmail.com>
2025-03-25 05:40:49 +0100

    Remove unused className utility from Icon component
    - Eliminated the `cn` utility from the Icon component to simplify the code.
    - Adjusted the rendering logic to maintain functionality without the removed className.

--- site/src/icons/icon.react.tsx
+++ site/src/icons/icon.react.tsx
@@ -1,5 +1,4 @@
 import type { ComponentProps } from "react";
-import { cn } from "../lib/cn.ts";
 import * as icons from "./icons.ts";
 
 export interface IconProps extends ComponentProps<"svg"> {
@@ -20,7 +19,6 @@ export function Icon({ name, ...props }: IconProps) {
       fill={fill}
       {...props}
       dangerouslySetInnerHTML={{ __html: html }}
-      className={cn("base:stroke-[1.5] base:ak-dark:stroke-1", props.className)}
     />
   );
 }
