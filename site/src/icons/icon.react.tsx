acd96c8732e22de56e1e40d6c4b8f0f99a5cbef4
Haz <hazdiego@gmail.com>
2025-03-27 17:15:54 +0100

    Enhance Icon component with dynamic ID replacement
    - Updated the Icon component to support dynamic ID replacement by introducing a `replaceId` property.
    - Modified the rendering logic to replace placeholder IDs with unique identifiers, improving accessibility and flexibility.
    - Ensured consistent handling of icon content in both Astro and React implementations.

--- site/src/icons/icon.react.tsx
+++ site/src/icons/icon.react.tsx
@@ -1,4 +1,6 @@
 import type { ComponentProps } from "react";
+import { useId } from "react";
+import { cn } from "../lib/cn.ts";
 import * as icons from "./icons.ts";
 
 export interface IconProps extends ComponentProps<"svg"> {
@@ -6,9 +8,17 @@ export interface IconProps extends ComponentProps<"svg"> {
 }
 
 export function Icon({ name, ...props }: IconProps) {
+  const id = useId();
   const hasLabel =
     props["aria-label"] !== undefined || props["aria-labelledby"] !== undefined;
-  const { html, stroke, fill, viewBox = "0 0 24 24" } = icons[name];
+  const { html, stroke, replaceId, fill, viewBox = "0 0 24 24" } = icons[name];
+
+  let content = html;
+
+  if (replaceId) {
+    content = content.replace(/<replace-id-(\d+)>/g, `${id}$1`);
+  }
+
   return (
     <svg
       viewBox={viewBox}
@@ -18,7 +28,8 @@ export function Icon({ name, ...props }: IconProps) {
       stroke={stroke}
       fill={fill}
       {...props}
-      dangerouslySetInnerHTML={{ __html: html }}
+      dangerouslySetInnerHTML={{ __html: content }}
+      className={cn("base:stroke-[1.5] base:ak-dark:stroke-1", props.className)}
     />
   );
 }
