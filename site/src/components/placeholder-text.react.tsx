e0ec638853281a396369124f30d3019296bf93c3
Haz <hazdiego@gmail.com>
2025-04-02 17:46:46 +0200

    Enhance PlaceholderText component with size prop for improved text styling
    - Added a `size` prop to the `PlaceholderText` component, allowing for flexible text sizing options: "sm", "md", "lg", and "xl".
    - Introduced a `sizeMap` to map the size prop to corresponding CSS classes, enhancing the visual adaptability of the component.
    - Set a default value for the `size` prop to "md" for consistent rendering.

--- site/src/components/placeholder-text.react.tsx
+++ site/src/components/placeholder-text.react.tsx
@@ -4,6 +4,7 @@ export interface PlaceholderTextProps {
   children: string;
   layer?: string;
   weight?: "light" | "normal" | "medium" | "bold";
+  size?: "sm" | "md" | "lg" | "xl";
   className?: string;
 }
 
@@ -11,6 +12,7 @@ export function PlaceholderText({
   children,
   layer,
   weight = "normal",
+  size = "md",
   className,
 }: PlaceholderTextProps) {
   const weightMap = {
@@ -19,19 +21,26 @@ export function PlaceholderText({
     medium: "ak-layer-pop-4",
     bold: "ak-layer-pop-6",
   };
+  const sizeMap = {
+    sm: "text-sm",
+    md: "text-base",
+    lg: "text-lg",
+    xl: "text-xl",
+  };
   return (
     <div
       aria-hidden
       className={clsx(
         layer ?? weightMap[weight],
-        "!text-transparent !bg-transparent select-none px-2 ak-frame leading-tight",
+        sizeMap[size],
+        "!text-transparent !bg-transparent select-none px-2 ak-frame",
         className,
       )}
     >
       {children.split("").map((char, index) => (
         <span
           key={index}
-          className="bg-(--ak-layer) rounded-[inherit] px-2 -mx-2"
+          className="bg-(--ak-layer) rounded-[inherit] px-2 -mx-2 text-[0.7em] tracking-[0.17em]"
         >
           {char}
         </span>
