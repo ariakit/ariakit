0684521720c7a90ffa209fe8d02080cc821ad474
Haz <hazdiego@gmail.com>
2025-05-03 05:55:05 +0200

    Add info icon to icons.ts
    - Introduced a new `info` icon with customizable size, stroke, and fill properties.
    - The icon features an SVG path for rendering, enhancing the icon library.

--- site/src/icons/icons.ts
+++ site/src/icons/icons.ts
@@ -21,6 +21,13 @@ export const check: Icon = {
   html: `<polyline stroke-linecap="round" stroke-linejoin="round" points="4,8 7,12 12,4" />`,
 };
 
+export const info: Icon = {
+  size: 24,
+  stroke: "currentColor",
+  fill: "none",
+  html: `<path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />`,
+};
+
 export const react: Icon = {
   size: 32,
   stroke: "none",
