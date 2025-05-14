4ba274dd3e61945f542b8d79ae1d40286f1d5a5d
Haz <hazdiego@gmail.com>
2025-05-14 02:13:51 +0200

    Add icon support to InlineLink component

--- site/src/components/inline-link.react.tsx
+++ site/src/components/inline-link.react.tsx
@@ -1,15 +1,19 @@
 import clsx from "clsx";
-import type { ComponentProps } from "react";
+import type { ComponentProps, ReactNode } from "react";
 import { Icon } from "#app/icons/icon.react.tsx";
 
 interface InlineLinkProps extends ComponentProps<"a"> {
   newWindow?: boolean;
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
 }
 
 export function InlineLink({
   newWindow,
   children,
   className,
+  iconLeft,
+  iconRight,
   ...props
 }: InlineLinkProps) {
   return (
@@ -19,11 +23,16 @@ export function InlineLink({
       target={newWindow ? "_blank" : undefined}
       rel={newWindow ? "noopener noreferrer nofollow" : undefined}
     >
+      {iconLeft && (
+        <span className="whitespace-nowrap me-[0.25em]">
+          {iconLeft}&#x2060;
+        </span>
+      )}
       {children}
-      {newWindow && (
+      {(newWindow || iconRight) && (
         <span className="whitespace-nowrap ms-[0.25em]">
           &#x2060;
-          <Icon name="newWindow" />
+          {iconRight || <Icon name="newWindow" />}
         </span>
       )}
     </a>
