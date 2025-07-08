9765926b3de4680029cf5cb126b7bdd4ba7e71a4
Haz <hazdiego@gmail.com>
2025-07-08 20:04:57 +0400

    Add getRandomId function to string utility for generating unique IDs

--- site/src/lib/string.ts
+++ site/src/lib/string.ts
@@ -1,3 +1,7 @@
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function getRandomId(prefix = "id-") {
+  return `${prefix}${Math.random().toString(36).substring(2, 15)}`;
+}
