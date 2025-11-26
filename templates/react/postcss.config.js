7d236b1d4acb433d38707737bd7a3495e7f165de
Haz <hazdiego@gmail.com>
2025-11-26 09:33:48 +0400

    Update stylelint configuration to ignore 'templates/**', enhance package-lock.json with new template dependencies, and add React template files including configuration and initial components.

---
+++ templates/react/postcss.config.js
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright 2025-present Ariakit FZ-LLC. All Rights Reserved.
+ *
+ * This software is proprietary. See the license.md file in the root of this
+ * package for licensing terms.
+ *
+ * SPDX-License-Identifier: UNLICENSED
+ */
+export default {};
