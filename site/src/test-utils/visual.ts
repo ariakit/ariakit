0eadb475af4837f4271876d1af3d482b56f123a1
Haz <hazdiego@gmail.com>
2025-11-28 11:49:27 +0400

    Update example tests (#5144)
    Co-authored-by: ariakit-bot <ariakit-bot@users.noreply.github.com>

--- site/src/test-utils/visual.ts
+++ site/src/test-utils/visual.ts
@@ -1,4 +1,5 @@
 import { invariant } from "@ariakit/core/utils/misc";
+import { query } from "@ariakit/test/playwright";
 import type { Locator, Page, TestInfo } from "@playwright/test";
 import { expect, test } from "@playwright/test";
 import { slugify } from "#app/lib/string.ts";
@@ -230,7 +231,7 @@ async function withViewport(
 
 export async function visual(page: Page, options: ScreenshotOptions = {}) {
   expect(
-    test.info().title,
+    test.info().tags,
     "When running visual tests, the test title should contain @visual",
   ).toContain("@visual");
 
@@ -278,8 +279,15 @@ export async function visual(page: Page, options: ScreenshotOptions = {}) {
   }
 }
 
-export async function reduceMotionBeforeEach() {
-  test.beforeEach(async ({ page }) => {
+export const visualTest = test.extend<{
+  visual: (options?: ScreenshotOptions) => Promise<void>;
+  q: ReturnType<typeof query>;
+}>({
+  visual: async ({ page }, use) => {
     await page.emulateMedia({ reducedMotion: "reduce" });
-  });
-}
+    await use((options) => visual(page, options));
+  },
+  q: async ({ page }, use) => {
+    await use(query(page));
+  },
+});
