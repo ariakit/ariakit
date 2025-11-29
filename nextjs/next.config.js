5d45d529f1a8f5ba239eed5c96946d7dc579a584
Haz <hazdiego@gmail.com>
2025-11-29 18:40:07 +0400

    Add Next.js configuration file
    - Introduced a new next.config.js file to define configuration options for the Next.js application.
    - Enabled the React compiler for improved development experience.

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import("next").NextConfig} */
+const nextConfig = {
   /* config options here */
   reactCompiler: true,
   // cacheComponents: true,
