31b323a5096a2c49a68140d059a312a068b18747
Haz <hazdiego@gmail.com>
2026-02-02 08:22:26 +0400

    Add hover styles for enhanced UI interaction
    - Introduced a new `hover.ts` file to define various hover variants, including faint, light, medium, and vivid options.
    - Enhanced user experience by providing a comprehensive set of hover background color options for different states, such as primary, secondary, success, warning, and danger.

---
+++ packages/ariakit-ui/src/styles/hover.ts
@@ -0,0 +1,31 @@
+import { cv } from "clava";
+
+export const hover = cv({
+  variants: {
+    /**
+     * Sets the element's background color.
+     */
+    $hover: {
+      // hover
+      faint: "ui-hover:ak-layer-hover-0.25",
+      light: "ui-hover:ak-layer-hover-0.5",
+      true: "ui-hover:ak-layer-hover",
+      medium: "ui-hover:ak-layer-hover-1.5",
+      bold: "ui-hover:ak-layer-hover-2",
+      intense: "ui-hover:ak-layer-hover-3",
+      // hover vivid
+      vividFaint: "ui-hover:ak-layer-hover-vivid-0.25",
+      vividLight: "ui-hover:ak-layer-hover-vivid-0.5",
+      vivid: "ui-hover:ak-layer-hover-vivid",
+      vividMedium: "ui-hover:ak-layer-hover-vivid-1.5",
+      vividBold: "ui-hover:ak-layer-hover-vivid-2",
+      vividIntense: "ui-hover:ak-layer-hover-vivid-3",
+      // colors
+      primary: "ui-hover:ak-layer-hover-primary",
+      secondary: "ui-hover:ak-layer-hover-secondary",
+      success: "ui-hover:ak-layer-hover-success",
+      warning: "ui-hover:ak-layer-hover-warning",
+      danger: "ui-hover:ak-layer-hover-danger",
+    },
+  },
+});
