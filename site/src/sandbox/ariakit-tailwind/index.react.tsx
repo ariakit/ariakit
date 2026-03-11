2d68fbf88528524a96dcb9feb91c82f11bfc936d
Haz <hazdiego@gmail.com>
2026-03-11 20:25:51 +0400

    Update dependencies and enhance layer component functionality
    - Upgraded `clava` to version 0.1.18 and `csstype` to version 3.2.3 in `package.json` and `package-lock.json`.
    - Modified `.vscode/settings.json` to include `cv` in the `tailwindCSS.classFunctions` array.
    - Refactored `index.react.tsx` to implement a more dynamic layer rendering approach using the `cv` function from `clava`, allowing for variant-based styling.
    - Simplified the `Layers` and `LayersState` components to utilize variant props for layer and state management, improving code maintainability and readability.

--- site/src/sandbox/ariakit-tailwind/index.react.tsx
+++ site/src/sandbox/ariakit-tailwind/index.react.tsx
@@ -1,13 +1,99 @@
-import { cv } from "clava";
+import type { VariantProps } from "clava";
+import { cv, splitProps } from "clava";
 import clsx from "clsx";
 import type { ComponentProps } from "react";
 import { useId } from "react";
 
-const test = cv();
-console.log(test);
+const layer = cv({
+  class: "ak-layer",
+  variants: {
+    $layer: {
+      0: "ak-layer-0",
+      10: "ak-layer-10",
+      20: "ak-layer-20",
+      30: "ak-layer-30",
+      40: "ak-layer-40",
+      50: "ak-layer-50",
+      60: "ak-layer-60",
+      70: "ak-layer-70",
+      80: "ak-layer-80",
+      90: "ak-layer-90",
+      100: "ak-layer-100",
+    },
+    $state: {
+      0: "ak-state-0",
+      10: "ak-state-10",
+      20: "ak-state-20",
+      30: "ak-state-30",
+      40: "ak-state-40",
+      50: "ak-state-50",
+      60: "ak-state-60",
+      70: "ak-state-70",
+      80: "ak-state-80",
+      90: "ak-state-90",
+      100: "ak-state-100",
+    },
+    $contrast: {
+      0: "ak-layer-contrast-0",
+      10: "ak-layer-contrast-10",
+      20: "ak-layer-contrast-20",
+      30: "ak-layer-contrast-30",
+      40: "ak-layer-contrast-40",
+      50: "ak-layer-contrast-50",
+      60: "ak-layer-contrast-60",
+      70: "ak-layer-contrast-70",
+      80: "ak-layer-contrast-80",
+      90: "ak-layer-contrast-90",
+      100: "ak-layer-contrast-100",
+    },
+    $mix: {
+      0: "ak-layer-mix-0",
+      10: "ak-layer-mix-10",
+      20: "ak-layer-mix-20",
+      30: "ak-layer-mix-30",
+      40: "ak-layer-mix-40",
+      50: "ak-layer-mix-50",
+      60: "ak-layer-mix-60",
+      70: "ak-layer-mix-70",
+      80: "ak-layer-mix-80",
+      90: "ak-layer-mix-90",
+      100: "ak-layer-mix-100",
+    },
+    $mixColor: {
+      red: "ak-layer-mix-red-500",
+    },
+    $lighten: {
+      0: "ak-layer-lighten-0",
+      10: "ak-layer-lighten-10",
+      20: "ak-layer-lighten-20",
+      30: "ak-layer-lighten-30",
+      40: "ak-layer-lighten-40",
+      50: "ak-layer-lighten-50",
+      60: "ak-layer-lighten-60",
+      70: "ak-layer-lighten-70",
+      80: "ak-layer-lighten-80",
+      90: "ak-layer-lighten-90",
+      100: "ak-layer-lighten-100",
+    },
+    $darken: {
+      0: "ak-layer-darken-0",
+      10: "ak-layer-darken-10",
+      20: "ak-layer-darken-20",
+      30: "ak-layer-darken-30",
+      40: "ak-layer-darken-40",
+      50: "ak-layer-darken-50",
+      60: "ak-layer-darken-60",
+      70: "ak-layer-darken-70",
+      80: "ak-layer-darken-80",
+      90: "ak-layer-darken-90",
+      100: "ak-layer-darken-100",
+    },
+  },
+});
 
-interface LayerProps extends ComponentProps<"section"> {
-  title: string;
+interface LayerProps
+  extends ComponentProps<"section">,
+    VariantProps<typeof layer> {
   orientation?: "vertical" | "horizontal";
 }
 
@@ -18,15 +104,19 @@ function Layer({
   ...props
 }: LayerProps) {
   const id = useId();
+  const [layerProps, rest] = splitProps(props, layer);
+  title ??= Object.values(layerProps)
+    .find((value) => typeof value === "number")
+    ?.toString();
   return (
     <section
       aria-labelledby={id}
-      {...props}
-      className={clsx(
-        "ak-layer ak-frame ak-frame-2xl/1 ak-edge-20 ak-frame-bordering font-mono flex gap-1 flex-col",
-        // "[content-visibility:auto] [contain-intrinsic-size:2ch_1lh]",
-        props.className,
-      )}
+      {...rest}
+      {...layer.jsx({
+        ...layerProps,
+        class:
+          "ak-frame ak-frame-2xl/1 ak-edge-20 ak-frame-bordering font-mono flex gap-1 flex-col",
+      })}
     >
       <div id={id}>{title}</div>
       {children && (
@@ -43,418 +133,114 @@ function Layer({
   );
 }
 
-interface LayersProps extends Omit<LayerProps, "title"> {
+interface LayersProps extends LayerProps {
   maxLevel?: number;
 }
 
 function Layers({ maxLevel = 100, ...props }: LayersProps) {
   return (
     <>
-      <Layer
-        {...props}
-        title="0"
-        className={clsx("ak-layer-0", props.className)}
-      />
-      <Layer
-        {...props}
-        title="10"
-        className={clsx("ak-layer-10", props.className)}
-      />
-      <Layer
-        {...props}
-        title="20"
-        className={clsx("ak-layer-20", props.className)}
-      />
-      <Layer
-        {...props}
-        title="30"
-        className={clsx("ak-layer-30", props.className)}
-      />
-      <Layer
-        {...props}
-        title="40"
-        className={clsx("ak-layer-40", props.className)}
-      />
-      <Layer
-        {...props}
-        title="50"
-        className={clsx("ak-layer-50", props.className)}
-      />
-      {maxLevel >= 60 && (
-        <Layer
-          {...props}
-          title="60"
-          className={clsx("ak-layer-60", props.className)}
-        />
-      )}
-      {maxLevel >= 70 && (
-        <Layer
-          {...props}
-          title="70"
-          className={clsx("ak-layer-70", props.className)}
-        />
-      )}
-      {maxLevel >= 80 && (
-        <Layer
-          {...props}
-          title="80"
-          className={clsx("ak-layer-80", props.className)}
-        />
-      )}
-      {maxLevel >= 90 && (
-        <Layer
-          {...props}
-          title="90"
-          className={clsx("ak-layer-90", props.className)}
-        />
-      )}
-      {maxLevel >= 100 && (
-        <Layer
-          {...props}
-          title="100"
-          className={clsx("ak-layer-100", props.className)}
-        />
-      )}
+      <Layer {...props} $layer={0} />
+      <Layer {...props} $layer={10} />
+      <Layer {...props} $layer={20} />
+      <Layer {...props} $layer={30} />
+      <Layer {...props} $layer={40} />
+      <Layer {...props} $layer={50} />
+      {maxLevel >= 60 && <Layer {...props} $layer={60} />}
+      {maxLevel >= 70 && <Layer {...props} $layer={70} />}
+      {maxLevel >= 80 && <Layer {...props} $layer={80} />}
+      {maxLevel >= 90 && <Layer {...props} $layer={90} />}
+      {maxLevel >= 100 && <Layer {...props} $layer={100} />}
     </>
   );
 }
 
-function LayersState(props: Omit<LayerProps, "title">) {
+function LayersState(props: LayerProps) {
   return (
     <>
-      <Layer
-        {...props}
-        title="0"
-        className={clsx("ak-state-0", props.className)}
-      />
-      <Layer
-        {...props}
-        title="10"
-        className={clsx("ak-state-10", props.className)}
-      />
-      <Layer
-        {...props}
-        title="20"
-        className={clsx("ak-state-20", props.className)}
-      />
-      <Layer
-        {...props}
-        title="30"
-        className={clsx("ak-state-30", props.className)}
-      />
-      <Layer
-        {...props}
-        title="40"
-        className={clsx("ak-state-40", props.className)}
-      />
-      <Layer
-        {...props}
-        title="50"
-        className={clsx("ak-state-50", props.className)}
-      />
-      <Layer
-        {...props}
-        title="60"
-        className={clsx("ak-state-60", props.className)}
-      />
-      <Layer
-        {...props}
-        title="70"
-        className={clsx("ak-state-70", props.className)}
-      />
-      <Layer
-        {...props}
-        title="80"
-        className={clsx("ak-state-80", props.className)}
-      />
-      <Layer
-        {...props}
-        title="90"
-        className={clsx("ak-state-90", props.className)}
-      />
-      <Layer
-        {...props}
-        title="100"
-        className={clsx("ak-state-100", props.className)}
-      />
+      <Layer {...props} $state={0} />
+      <Layer {...props} $state={10} />
+      <Layer {...props} $state={20} />
+      <Layer {...props} $state={30} />
+      <Layer {...props} $state={40} />
+      <Layer {...props} $state={50} />
+      <Layer {...props} $state={60} />
+      <Layer {...props} $state={70} />
+      <Layer {...props} $state={80} />
+      <Layer {...props} $state={90} />
+      <Layer {...props} $state={100} />
     </>
   );
 }
 
-function LayersContrast(props: Omit<LayerProps, "title">) {
+function LayersContrast(props: LayerProps) {
   return (
     <>
-      <Layer
-        {...props}
-        title="0"
-        className={clsx("ak-layer-contrast-0", props.className)}
-      />
-      <Layer
-        {...props}
-        title="10"
-        className={clsx("ak-layer-contrast-10", props.className)}
-      />
-      <Layer
-        {...props}
-        title="20"
-        className={clsx("ak-layer-contrast-20", props.className)}
-      />
-      <Layer
-        {...props}
-        title="30"
-        className={clsx("ak-layer-contrast-30", props.className)}
-      />
-      <Layer
-        {...props}
-        title="40"
-        className={clsx("ak-layer-contrast-40", props.className)}
-      />
-      <Layer
-        {...props}
-        title="50"
-        className={clsx("ak-layer-contrast-50", props.className)}
-      />
-      <Layer
-        {...props}
-        title="60"
-        className={clsx("ak-layer-contrast-60", props.className)}
-      />
-      <Layer
-        {...props}
-        title="70"
-        className={clsx("ak-layer-contrast-70", props.className)}
-      />
-      <Layer
-        {...props}
-        title="80"
-        className={clsx("ak-layer-contrast-80", props.className)}
-      />
-      <Layer
-        {...props}
-        title="90"
-        className={clsx("ak-layer-contrast-90", props.className)}
-      />
-      <Layer
-        {...props}
-        title="100"
-        className={clsx("ak-layer-contrast-100", props.className)}
-      />
+      <Layer {...props} $contrast={0} />
+      <Layer {...props} $contrast={10} />
+      <Layer {...props} $contrast={20} />
+      <Layer {...props} $contrast={30} />
+      <Layer {...props} $contrast={40} />
+      <Layer {...props} $contrast={50} />
+      <Layer {...props} $contrast={60} />
+      <Layer {...props} $contrast={70} />
+      <Layer {...props} $contrast={80} />
+      <Layer {...props} $contrast={90} />
+      <Layer {...props} $contrast={100} />
     </>
   );
 }
 
-function LayersMix(props: Omit<LayerProps, "title">) {
+function LayersMix(props: LayerProps) {
   return (
     <>
-      <Layer
-        {...props}
-        title="0"
-        className={clsx("ak-layer-mix-red-500 ak-layer-mix-0", props.className)}
-      />
-      <Layer
-        {...props}
-        title="10"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-10",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="20"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-20",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="30"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-30",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="40"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-40",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="50"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-50",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="60"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-60",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="70"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-70",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="80"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-80",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="90"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-90",
-          props.className,
-        )}
-      />
-      <Layer
-        {...props}
-        title="100"
-        className={clsx(
-          "ak-layer-mix-red-500 ak-layer-mix-100",
-          props.className,
-        )}
-      />
+      <Layer {...props} $mixColor="red" $mix={0} />
+      <Layer {...props} $mixColor="red" $mix={10} />
+      <Layer {...props} $mixColor="red" $mix={20} />
+      <Layer {...props} $mixColor="red" $mix={30} />
+      <Layer {...props} $mixColor="red" $mix={40} />
+      <Layer {...props} $mixColor="red" $mix={50} />
+      <Layer {...props} $mixColor="red" $mix={60} />
+      <Layer {...props} $mixColor="red" $mix={70} />
+      <Layer {...props} $mixColor="red" $mix={80} />
+      <Layer {...props} $mixColor="red" $mix={90} />
+      <Layer {...props} $mixColor="red" $mix={100} />
     </>
   );
 }
 
-function LayersLighten(props: Omit<LayerProps, "title">) {
+function LayersLighten(props: LayerProps) {
   return (
     <>
-      <Layer
-        {...props}
-        title="0"
-        className={clsx("ak-layer-lighten-0", props.className)}
-      />
-      <Layer
-        {...props}
-        title="10"
-        className={clsx("ak-layer-lighten-10", props.className)}
-      />
-      <Layer
-        {...props}
-        title="20"
-        className={clsx("ak-layer-lighten-20", props.className)}
-      />
-      <Layer
-        {...props}
-        title="30"
-        className={clsx("ak-layer-lighten-30", props.className)}
-      />
-      <Layer
-        {...props}
-        title="40"
-        className={clsx("ak-layer-lighten-40", props.className)}
-      />
-      <Layer
-        {...props}
-        title="50"
-        className={clsx("ak-layer-lighten-50", props.className)}
-      />
-      <Layer
-        {...props}
-        title="60"
-        className={clsx("ak-layer-lighten-60", props.className)}
-      />
-      <Layer
-        {...props}
-        title="70"
-        className={clsx("ak-layer-lighten-70", props.className)}
-      />
-      <Layer
-        {...props}
-        title="80"
-        className={clsx("ak-layer-lighten-80", props.className)}
-      />
-      <Layer
-        {...props}
-        title="90"
-        className={clsx("ak-layer-lighten-90", props.className)}
-      />
-      <Layer
-        {...props}
-        title="100"
-        className={clsx("ak-layer-lighten-100", props.className)}
-      />
+      <Layer {...props} $lighten={0} />
+      <Layer {...props} $lighten={10} />
+      <Layer {...props} $lighten={20} />
+      <Layer {...props} $lighten={30} />
+      <Layer {...props} $lighten={40} />
+      <Layer {...props} $lighten={50} />
+      <Layer {...props} $lighten={60} />
+      <Layer {...props} $lighten={70} />
+      <Layer {...props} $lighten={80} />
+      <Layer {...props} $lighten={90} />
+      <Layer {...props} $lighten={100} />
     </>
   );
 }
 
-function LayersDarken(props: Omit<LayerProps, "title">) {
+function LayersDarken(props: LayerProps) {
   return (
     <>
-      <Layer
-        {...props}
-        title="0"
-        className={clsx("ak-layer-darken-0", props.className)}
-      />
-      <Layer
-        {...props}
-        title="10"
-        className={clsx("ak-layer-darken-10", props.className)}
-      />
-      <Layer
-        {...props}
-        title="20"
-        className={clsx("ak-layer-darken-20", props.className)}
-      />
-      <Layer
-        {...props}
-        title="30"
-        className={clsx("ak-layer-darken-30", props.className)}
-      />
-      <Layer
-        {...props}
-        title="40"
-        className={clsx("ak-layer-darken-40", props.className)}
-      />
-      <Layer
-        {...props}
-        title="50"
-        className={clsx("ak-layer-darken-50", props.className)}
-      />
-      <Layer
-        {...props}
-        title="60"
-        className={clsx("ak-layer-darken-60", props.className)}
-      />
-      <Layer
-        {...props}
-        title="70"
-        className={clsx("ak-layer-darken-70", props.className)}
-      />
-      <Layer
-        {...props}
-        title="80"
-        className={clsx("ak-layer-darken-80", props.className)}
-      />
-      <Layer
-        {...props}
-        title="90"
-        className={clsx("ak-layer-darken-90", props.className)}
-      />
-      <Layer
-        {...props}
-        title="100"
-        className={clsx("ak-layer-darken-100", props.className)}
-      />
+      <Layer {...props} $darken={0} />
+      <Layer {...props} $darken={10} />
+      <Layer {...props} $darken={20} />
+      <Layer {...props} $darken={30} />
+      <Layer {...props} $darken={40} />
+      <Layer {...props} $darken={50} />
+      <Layer {...props} $darken={60} />
+      <Layer {...props} $darken={70} />
+      <Layer {...props} $darken={80} />
+      <Layer {...props} $darken={90} />
+      <Layer {...props} $darken={100} />
     </>
   );
 }
