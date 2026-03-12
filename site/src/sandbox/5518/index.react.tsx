515a6f401537264240c4c1b4fc9b875bbf7de9a7
Haz <hazdiego@gmail.com>
2026-03-12 05:56:26 +0400

    Add issue 5518 workaround example

--- site/src/sandbox/5518/index.react.tsx
+++ site/src/sandbox/5518/index.react.tsx
@@ -1,11 +1,23 @@
 import * as ak from "@ariakit/react";
 
 export default function Example() {
+  const combobox = ak.useComboboxStore({ defaultOpen: true });
+
   return (
-    <ak.ComboboxProvider defaultOpen>
+    <ak.ComboboxProvider store={combobox}>
       <ak.ComboboxLabel>Your favorite fruit</ak.ComboboxLabel>
       <div>
-        <ak.Combobox autoFocus placeholder="e.g., Apple" />
+        <ak.Combobox
+          autoFocus
+          focusable
+          placeholder="e.g., Apple"
+          onKeyDown={(event) => {
+            if (event.key !== "Escape") return;
+            // TODO: Remove when https://github.com/ariakit/ariakit/issues/5518
+            // is fixed.
+            combobox.hide();
+          }}
+        />
         <ak.ComboboxDisclosure />
       </div>
       <ak.ComboboxPopover gutter={4} sameWidth>
