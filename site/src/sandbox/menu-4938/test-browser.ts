33e67a2c72cb29837a7cdfa42df7ae1f7a518aa0
Haz <hazdiego@gmail.com>
2026-04-07 00:32:46 +0200

    Add menu scroll focus repro

---
+++ site/src/sandbox/menu-4938/test-browser.ts
@@ -0,0 +1,35 @@
+import { withFramework } from "#app/test-utils/preview.ts";
+
+withFramework(import.meta.dirname, async ({ test }) => {
+  test("clicking a menu item scrolls the menu button into view", async ({
+    page,
+    q,
+  }) => {
+    const menuButton = q.button("Actions");
+
+    await test.expect(menuButton).toBeVisible();
+
+    const { x, y, scrollY } = await menuButton.evaluate((element) => {
+      const rect = element.getBoundingClientRect();
+      const scrollY = window.scrollY + rect.bottom - 10;
+      window.scrollTo(0, scrollY);
+      const scrolledRect = element.getBoundingClientRect();
+      return {
+        x: scrolledRect.left + scrolledRect.width / 2,
+        y: scrolledRect.bottom - 5,
+        scrollY,
+      };
+    });
+
+    await page.mouse.click(x, y);
+    await test.expect(q.menu()).toBeVisible();
+
+    await q.text("CLICK THIS").click();
+    await test.expect(q.menu()).not.toBeVisible();
+    await test.expect(menuButton).toBeFocused();
+
+    await test.expect
+      .poll(() => page.evaluate(() => window.scrollY))
+      .toBeLessThan(scrollY);
+  });
+});
