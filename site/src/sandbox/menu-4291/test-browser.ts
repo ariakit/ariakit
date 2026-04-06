ad6165caa6a7c2c43fbf81370032e474136f543c
Haz <hazdiego@gmail.com>
2026-04-07 00:25:58 +0200

    Add menu persistent disclosure focus repro

---
+++ site/src/sandbox/menu-4291/test-browser.ts
@@ -0,0 +1,17 @@
+import { withFramework } from "#app/test-utils/preview.ts";
+
+withFramework(import.meta.dirname, async ({ test }) => {
+  test("shift tab moves focus from modal menu to persistent disclosure", async ({
+    page,
+    q,
+  }) => {
+    const menuButton = q.button("Actions");
+
+    await menuButton.click();
+    await test.expect(q.menu()).toBeVisible();
+
+    await page.keyboard.press("Shift+Tab");
+
+    await test.expect(menuButton).toBeFocused();
+  });
+});
