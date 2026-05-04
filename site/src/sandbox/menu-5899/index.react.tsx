7da3e3ed37b2959679ba9381b229368a0fc9bfef
Haz <hazdiego@gmail.com>
2026-05-04 20:27:40 +0200

    Add menu auto focus workaround

--- site/src/sandbox/menu-5899/index.react.tsx
+++ site/src/sandbox/menu-5899/index.react.tsx
@@ -1,4 +1,9 @@
 import * as Ariakit from "@ariakit/react";
+import type { KeyboardEvent } from "react";
+
+interface MenuExampleProps {
+  name: string;
+}
 
 function MenuItems() {
   return (
@@ -12,21 +17,45 @@ function MenuItems() {
   );
 }
 
+function MenuExample({ name }: MenuExampleProps) {
+  const store = Ariakit.useMenuStore();
+
+  const focusItem = (id: string | null | undefined) => {
+    if (!id) return;
+    requestAnimationFrame(() => store.move(id));
+  };
+
+  const onKeyDown = (event: KeyboardEvent<HTMLButtonElement>) => {
+    const { open } = store.getState();
+    if (!open) return;
+
+    if (event.key === "ArrowDown") {
+      event.preventDefault();
+      focusItem(store.first());
+      return;
+    }
+
+    if (event.key === "ArrowUp") {
+      event.preventDefault();
+      focusItem(store.last());
+    }
+  };
+
+  return (
+    <Ariakit.MenuProvider store={store}>
+      <Ariakit.MenuButton onKeyDown={onKeyDown}>{name}</Ariakit.MenuButton>
+      <Ariakit.Menu autoFocusOnShow={() => false} gutter={8}>
+        <MenuItems />
+      </Ariakit.Menu>
+    </Ariakit.MenuProvider>
+  );
+}
+
 export default function Example() {
   return (
     <>
-      <Ariakit.MenuProvider>
-        <Ariakit.MenuButton>Boolean</Ariakit.MenuButton>
-        <Ariakit.Menu autoFocusOnShow={false} gutter={8}>
-          <MenuItems />
-        </Ariakit.Menu>
-      </Ariakit.MenuProvider>
-      <Ariakit.MenuProvider>
-        <Ariakit.MenuButton>Callback</Ariakit.MenuButton>
-        <Ariakit.Menu autoFocusOnShow={() => false} gutter={8}>
-          <MenuItems />
-        </Ariakit.Menu>
-      </Ariakit.MenuProvider>
+      <MenuExample name="Boolean" />
+      <MenuExample name="Callback" />
     </>
   );
 }
