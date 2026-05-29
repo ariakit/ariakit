5a89a53b483ff67b7bac8a3bad4a0e6bf13f4c3a
Haz <hazdiego@gmail.com>
2026-05-29 08:48:54 +0200

    Add Hovercard hover workaround

--- app/src/sandbox/hovercard-6075/test.ts
+++ app/src/sandbox/hovercard-6075/test.ts
@@ -86,8 +86,8 @@ afterAll(async () => {
 });
 
 // See https://github.com/ariakit/ariakit/issues/6075
-// This must use Testing Library userEvent.hover with built package output.
-test("tracks Testing Library hover as mouse movement in the package build", async () => {
+// TODO: Switch back to userEvent.hover after this issue is fixed.
+test("tracks Testing Library pointer coordinates as mouse movement in the package build", async () => {
   const user = userEvent.setup();
   let isMouseMoving = () => false;
 
@@ -99,7 +99,13 @@ test("tracks Testing Library hover as mouse movement in the package build", asyn
   render(createElement(Test));
   expect(isMouseMoving()).toBe(false);
 
-  await user.hover(q.button.ensure("Hover"));
+  await user.pointer({
+    target: q.button.ensure("Hover"),
+    coords: {
+      screenX: 1,
+      screenY: 1,
+    },
+  });
 
   expect(isMouseMoving()).toBe(true);
 });
