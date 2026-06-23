e6d9d914e9ebb284753a2075c655d00488fcb921
Haz <hazdiego@gmail.com>
2026-06-23 21:52:59 +0200

    Document Solid Group aria-label workaround

--- app/src/sandbox/group-6327/index.solid.tsx
+++ app/src/sandbox/group-6327/index.solid.tsx
@@ -3,7 +3,8 @@ import * as Ariakit from "@ariakit/solid";
 export default function Example() {
   return (
     <Ariakit.Group aria-label="Audio playback settings">
-      <Ariakit.GroupLabel>Audio</Ariakit.GroupLabel>
+      {/* TODO: Use GroupLabel after https://github.com/ariakit/ariakit/issues/6327 is fixed. */}
+      <div aria-hidden>Audio</div>
       <button type="button">Mute</button>
       <button type="button">Boost</button>
     </Ariakit.Group>
