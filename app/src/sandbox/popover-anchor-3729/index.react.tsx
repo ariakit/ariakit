cd4073a998adbd92338dbd5d3ee4ad247a16ab44
Haz <hazdiego@gmail.com>
2026-07-22 17:44:41 +0200

    Demonstrate Popover anchor workaround

--- app/src/sandbox/popover-anchor-3729/index.react.tsx
+++ app/src/sandbox/popover-anchor-3729/index.react.tsx
@@ -1,6 +1,9 @@
 import * as Ariakit from "@ariakit/react";
+import { useRef } from "react";
 
 export default function Example() {
+  const anchorRef = useRef<HTMLDivElement>(null);
+
   return (
     <div
       style={{
@@ -12,8 +15,17 @@ export default function Example() {
     >
       <Ariakit.PopoverProvider placement="right">
         <Ariakit.PopoverDisclosure>Accept invite</Ariakit.PopoverDisclosure>
-        <Ariakit.PopoverAnchor>Different anchor</Ariakit.PopoverAnchor>
-        <Ariakit.Popover flip={false} slide={false} gutter={16}>
+        <Ariakit.PopoverAnchor ref={anchorRef}>
+          Different anchor
+        </Ariakit.PopoverAnchor>
+        <Ariakit.Popover
+          flip={false}
+          slide={false}
+          gutter={16}
+          getAnchorRect={() =>
+            anchorRef.current?.getBoundingClientRect() ?? null
+          }
+        >
           <Ariakit.PopoverHeading>Invitation details</Ariakit.PopoverHeading>
           <p>You have been invited to join the project.</p>
         </Ariakit.Popover>
