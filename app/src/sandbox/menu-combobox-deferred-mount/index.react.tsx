042e584fdc9047f9af5ce322305686cce92b4346
Haz <hazdiego@gmail.com>
2026-08-02 11:07:05 +0200

    Add deferred select focus workaround

--- app/src/sandbox/menu-combobox-deferred-mount/index.react.tsx
+++ app/src/sandbox/menu-combobox-deferred-mount/index.react.tsx
@@ -5,6 +5,9 @@ import { useDeferredValue, useEffect, useState } from "react";
 
 export default function Example() {
   const [open, setOpen] = useState(false);
+  const [inputElement, setInputElement] = useState<HTMLInputElement | null>(
+    null,
+  );
   const deferredOpen = useDeferredValue(open);
   const combobox = Ariakit.useComboboxStore({
     includesBaseElement: false,
@@ -45,7 +48,11 @@ export default function Example() {
     <Ariakit.Role {...props}>
       {deferredOpen && (
         <>
-          <Ariakit.Combobox store={combobox} placeholder="Search pages" />
+          <Ariakit.Combobox
+            ref={setInputElement}
+            store={combobox}
+            placeholder="Search pages"
+          />
           <Ariakit.ComboboxList store={combobox}>
             <Ariakit.ComboboxItem store={combobox} value="Components" />
             <Ariakit.ComboboxItem store={combobox} value="Examples" />
@@ -65,6 +72,7 @@ export default function Example() {
         typeahead={false}
         composite={false}
         unmountOnHide
+        initialFocus={inputElement}
         render={
           <Ariakit.MenuList
             store={menu}
