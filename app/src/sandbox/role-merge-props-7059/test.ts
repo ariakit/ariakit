0beb71cf9fc05af051679fe0b714e4afdf5d0beb
Haz <hazdiego@gmail.com>
2026-08-03 21:25:40 +0200

    Prevent mergeProps prototype replacement (#7061)

---
+++ app/src/sandbox/role-merge-props-7059/test.ts
@@ -0,0 +1,12 @@
+import { click, q, type } from "@ariakit/test";
+import { expect, test } from "vitest";
+
+// https://github.com/ariakit/ariakit/issues/7059
+test("an own __proto__ render prop does not remount the element", async () => {
+  await type("Haz", q.textbox.ensure("Profile name"));
+  expect(q.textbox("Profile name")).toHaveValue("Haz");
+
+  await click(q.button("Refresh payload"));
+
+  expect(q.textbox("Profile name")).toHaveValue("Haz");
+});
