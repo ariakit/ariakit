5856b2aab95b05521c3535cd49465b5604b596a8
Haz <hazdiego@gmail.com>
2026-09-04 23:01:14 +0200

    Demonstrate the menubar control arrow-key workaround

--- app/src/sandbox/menubar-interactions/controls/index.react.tsx
+++ app/src/sandbox/menubar-interactions/controls/index.react.tsx
@@ -1,6 +1,13 @@
 import * as Ariakit from "@ariakit/react";
+import type { KeyboardEvent } from "react";
 import { useState } from "react";
 import "./style.css";
+
+function stopArrowKeys(event: KeyboardEvent) {
+  if (event.key.startsWith("Arrow")) {
+    event.stopPropagation();
+  }
+}
 
 function FileMenu() {
   return (
@@ -8,18 +15,20 @@ function FileMenu() {
       <Ariakit.MenuItem render={<Ariakit.MenuButton />}>File</Ariakit.MenuItem>
       <Ariakit.Menu className="menu" gutter={4}>
         <Ariakit.MenuItem className="menu-item">New Tab</Ariakit.MenuItem>
-        <input aria-label="Search" defaultValue="abc" className="input" />
-        <Ariakit.MenuItem
-          aria-label="Rename"
-          hideOnClick={false}
-          render={<input defaultValue="abc" className="input" />}
-        />
-        <a href="#help" tabIndex={0}>
-          Help
-        </a>
-        <button type="button" tabIndex={0}>
-          Action
-        </button>
+        <div onKeyDown={stopArrowKeys} style={{ display: "contents" }}>
+          <input aria-label="Search" defaultValue="abc" className="input" />
+          <Ariakit.MenuItem
+            aria-label="Rename"
+            hideOnClick={false}
+            render={<input defaultValue="abc" className="input" />}
+          />
+          <a href="#help" tabIndex={0}>
+            Help
+          </a>
+          <button type="button" tabIndex={0}>
+            Action
+          </button>
+        </div>
       </Ariakit.Menu>
     </Ariakit.MenuProvider>
   );
@@ -56,7 +65,12 @@ export default function Example() {
               Insert
             </Ariakit.MenuItem>
             <Ariakit.Menu className="menu" gutter={4}>
-              <Ariakit.Combobox aria-label="Search blocks" className="input" />
+              <div onKeyDown={stopArrowKeys}>
+                <Ariakit.Combobox
+                  aria-label="Search blocks"
+                  className="input"
+                />
+              </div>
               <Ariakit.ComboboxList>
                 <Ariakit.ComboboxItem value="Paragraph" className="menu-item" />
               </Ariakit.ComboboxList>
