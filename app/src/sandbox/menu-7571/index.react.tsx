670d343f087e2ab007b22d3d153e77579b9b1107
Haz <hazdiego@gmail.com>
2026-09-21 18:45:08 -0300

    Demonstrate a submenu store lifecycle workaround
    Co-authored-by: Ben Lowery <14350+blowery@users.noreply.github.com>

--- app/src/sandbox/menu-7571/index.react.tsx
+++ app/src/sandbox/menu-7571/index.react.tsx
@@ -7,26 +7,31 @@ const menuStyle = {
   padding: 8,
 };
 
-export default function Example() {
-  // The submenu store survives each mount of the parent menu's content.
+function BlogsSubmenu() {
+  // Recreate the store with the parent menu so earlier moves are not inherited.
   const submenu = Ariakit.useMenuStore();
+  return (
+    <Ariakit.MenuProvider store={submenu} placement="right-start">
+      <Ariakit.MenuButton render={<Ariakit.MenuItem />}>
+        Blogs
+      </Ariakit.MenuButton>
+      <Ariakit.Menu unmountOnHide portal style={menuStyle}>
+        <Ariakit.MenuItem>Alpha</Ariakit.MenuItem>
+        <Ariakit.MenuItem>Bravo</Ariakit.MenuItem>
+        <Ariakit.MenuItem>Charlie</Ariakit.MenuItem>
+      </Ariakit.Menu>
+    </Ariakit.MenuProvider>
+  );
+}
 
+export default function Example() {
   return (
     <main>
       <div style={{ height: 3000 }}>Scroll down to the menu.</div>
       <Ariakit.MenuProvider>
         <Ariakit.MenuButton>Menu</Ariakit.MenuButton>
         <Ariakit.Menu unmountOnHide style={menuStyle}>
-          <Ariakit.MenuProvider store={submenu} placement="right-start">
-            <Ariakit.MenuButton render={<Ariakit.MenuItem />}>
-              Blogs
-            </Ariakit.MenuButton>
-            <Ariakit.Menu unmountOnHide portal style={menuStyle}>
-              <Ariakit.MenuItem>Alpha</Ariakit.MenuItem>
-              <Ariakit.MenuItem>Bravo</Ariakit.MenuItem>
-              <Ariakit.MenuItem>Charlie</Ariakit.MenuItem>
-            </Ariakit.Menu>
-          </Ariakit.MenuProvider>
+          <BlogsSubmenu />
           <Ariakit.MenuItem>Settings</Ariakit.MenuItem>
         </Ariakit.Menu>
       </Ariakit.MenuProvider>
